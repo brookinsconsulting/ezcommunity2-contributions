6a35047a64c066ca7f8119c06fab9cb9af7f42a8
Bård Farstad <bf@ez.no>
2000-07-10 12:45:37 +0000

    Telefon h�ndtering.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/classes/ezphone.php
@@ -0,0 +1,81 @@
+<?
+
+class eZPhone
+{
+    /*
+
+    */
+    function eZPhone()
+    {
+
+    }
+
+    /*
+      Lagrer et telefonnummer link i databasen.      
+    */
+    function store()
+    {
+        $this->dbInit();
+        
+        query( "INSERT INTO Phone set Number='$this->Number', Type='$this->Type' " );
+        return mysql_insert_id();
+    }
+
+    /*
+      Henter ut telefonnummer med ID == $id
+    */  
+    function get( $id )
+    {
+        $this->dbInit();    
+        if ( $id != "" )
+        {
+            array_query( $phone_array, "SELECT * FROM Phone WHERE ID='$id'" );
+            if ( count( $phone_array ) > 1 )
+            {
+                die( "Feil: Flere telefonnummer med samme ID funnet i database, dette skal ikke være mulig. " );
+            }
+            else if ( count( $phone_array ) == 1 )
+            {
+                $this->ID = $phone_array[ 0 ][ "ID" ];
+                $this->Number = $phone_array[ 0 ][ "Number" ];
+                $this->Type = $phone_array[ 0 ][ "Type" ];
+            }
+        }
+    }    
+
+    function setNumber( $value )
+    {
+        $this->Number = $value;
+    }
+
+    function setType( $value )
+    {
+        $this->Type = $value;
+    }
+
+    function number( )
+    {
+        return $this->Number;
+    }
+
+    function type( )
+    {
+        return $this->Type;
+    }
+    
+    /*
+      Privat: Initiering av database. 
+    */
+    function dbInit()
+    {
+        require "ezcontact/dbsettings.php";
+        mysql_pconnect( $SERVER, $USER, $PWD ) or die( "Kunne ikke kople til database" );
+        mysql_select_db( $DATABASE ) or die( "Kunne ikke velge database" );
+    }
+
+    var $ID;
+    var $Number;
+    var $Type;
+}
+
+?>
