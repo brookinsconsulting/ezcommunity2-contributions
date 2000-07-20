005ed63c138153a41f94d0a9537c367b8f9f329f
eZ Systems <info@ez.no>
2000-07-20 15:33:05 +0000

    Initial revision

---
+++ contribution/versions/ezpublish2/trunk/projects/php/ezlink/gotolink.php
@@ -0,0 +1,33 @@
+<?
+/*
+  Legger inn hits
+*/
+
+
+include "template.inc";
+require "ezlink/dbsettings.php";
+require "ezphputils.php";
+
+require $DOCUMENTROOT . "classes/ezlinkgroup.php";
+require $DOCUMENTROOT . "classes/ezlink.php";
+require $DOCUMENTROOT . "classes/ezhit.php";
+
+
+print( $HTTP_REFERER );
+
+if ( $Action == "addhit" )
+{
+    $hit = new eZHit();
+    $hit->setLink( $LID );
+    $hit->store();
+
+}
+die();
+
+printRedirect( "http://" . $Url );
+
+
+
+
+
+?>
