d24b6fcfef730a4e53e20f12be87b9adb2f6662a
eZ Systems <info@ez.no>
2000-07-21 07:49:10 +0000

    lagt til: -bedre suggestion

--- contribution/versions/ezpublish2/trunk/projects/php/ezlink/suggestlink.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezlink/suggestlink.php
@@ -14,22 +14,71 @@ require $DOCUMENTROOT . "classes/ezhit.php";
 
 $t = new Template();
 $t->set_file( array(
-    "suggestlink" => $DOCUMENTROOT . "templates/suggestlink.tpl"
+    "suggestlink" => $DOCUMENTROOT . "templates/suggestlink.tpl",
+    "suggest_group_select" => $DOCUMENTROOT . "templates/suggestgroupselect.tpl"
     ));
+
+$groupselect = new eZLinkGroup();
+$grouplink_array = $groupselect->getAll( );
+
 
 if ( $Action == "suggest" )
 {
     $newlink = new eZLink();
 
-    $newlink->setTitle( $title );
-    $newlink->setDescription( $description );
-    $newlink->setKeyWords( $keywords );
-    $newlink->setLinkGroup( $linkgroup );
-    $newlink->setAccepted( "N" );
-    print ( "tittel: " . $title );
+    if ( ( $title == "" ) || ( $url = "" ) || ( $keywords == "" ) || ( $description == "" )  ) 
+    {
+        $terror_msg = "Legg til alle feltene..."; 
+
+        $ttitle = $title;
+        $turl = $url;
+        $tkeywords = $keywords;
+        $tdescription = $description; 
+    }
+    else
+    {
+        print ( "Lagt inn i database" );
+        $newlink->setTitle( $title );
+        $newlink->setDescription( $description );
+        $newlink->setKeyWords( $keywords );
+        $newlink->setLinkGroup( $linkgroup );
+        $newlink->setAccepted( "N" );
+   
+        $newlink->store();
+
+        printRedirect( "../index.php?page=" . $DOCUMENTROOT . "linklist.php" );
+    }
+   
     
-   $newlink->store();
 }
+
+// Selecter
+$group_select_dict = "";
+for ( $i=0; $i<count( $grouplink_array ); $i++ )
+{
+    $t->set_var( "grouplink_id", $grouplink_array[ $i ][ "ID" ] );
+    $t->set_var( "grouplink_title", $grouplink_array[ $i ][ "Title" ] );
+
+    if ( $grouplink_array[ $i ][ "ID" ] == $LGID )
+    {
+        $t->set_var( "is_selected", "selected" );
+    }
+    else
+    {
+        $t->set_var( "is_selected", "" );
+    }
+
+    $group_select_dict[ $grouplink_array[$i][ "ID" ] ] = $i;
+
+    $t->parse( "group_category", "suggest_group_select", true );
+}
+
+$t->set_var( "error_msg", $terror_msg );
+
+$t->set_var( "title", $ttitle );
+$t->set_var( "url", $turl );
+$t->set_var( "keywords", $tkeywords );
+$t->set_var( "description", $tdescription ); 
 
 $t->set_var( "document_root", $DOCUMENTROOT );
 
