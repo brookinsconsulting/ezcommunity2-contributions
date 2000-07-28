1fd33dcb01af19346cafba75f64db87acef7dfb2
eZ Systems <info@ez.no>
2000-07-28 10:05:51 +0000

    ezforum admin

--- contribution/versions/ezpublish2/trunk/projects/php/ezforum/admin/category.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezforum/admin/category.php
@@ -1,6 +1,6 @@
 <?
 /*!
-    $Id: category.php,v 1.6 2000/07/26 17:03:13 lw-cvs Exp $
+    $Id: category.php,v 1.7 2000/07/28 10:05:51 lw-cvs Exp $
 
     Author: Lars Wilhelmsen <lw@ez.no>
     
@@ -8,6 +8,8 @@
     
     Copyright (C) 2000 eZ systems. All rights reserved.
 */
+// zez/admin/
+// zez/ezforum/
 include( "ezforum/dbsettings.php" );
 include_once( "template.inc" );
 include_once( "$DOCROOT/classes/ezdb.php" );
