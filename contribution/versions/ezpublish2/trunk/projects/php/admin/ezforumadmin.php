99748248b2fe121126e3c8de2b6c2b7fa533e8cf
Bård Farstad <bf@ez.no>
2000-08-22 09:34:51 +0000

    Fikset bilder.

--- contribution/versions/ezpublish2/trunk/projects/php/admin/ezforumadmin.php
+++ contribution/versions/ezpublish2/trunk/projects/php/admin/ezforumadmin.php
@@ -7,7 +7,6 @@
                     <tr bgcolor="#C8B4B4"> 
                       <td>
                         <a class="small" href="index.php?page=../ezforum/admin/categorylist.php">Kategorioversikt</a><br>
-                        <a class="small" href="index.php?page=../ezforum/admin/users.php">Brukeroversikt</a><br>
                         <a class="small" href="index.php?page=../ezforum/admin/category.php">Ny katergori</a><br>
                         <a class="small" href="index.php?page=../ezforum/admin/users.php">Ny forumbruker</a>
                       </td>
