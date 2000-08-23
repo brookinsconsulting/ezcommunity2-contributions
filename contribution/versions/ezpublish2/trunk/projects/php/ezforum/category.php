957aec22b611132d4af8436413e3399a236345b8
Bård Farstad <bf@ez.no>
2000-08-23 14:46:52 +0000

    FIkset en path.

--- contribution/versions/ezpublish2/trunk/projects/php/ezforum/category.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezforum/category.php
@@ -1,6 +1,6 @@
 <?
 /*!
-    $Id: category.php,v 1.17 2000/08/22 09:35:02 bf-cvs Exp $
+    $Id: category.php,v 1.18 2000/08/23 14:46:52 bf-cvs Exp $
 
     Author: Lars Wilhelmsen <lw@ez.no>
     
@@ -8,21 +8,27 @@
     
     Copyright (C) 2000 eZ systems. All rights reserved.
 */
-include( "ezforum/dbsettings.php" );
+
+//include( "ezforum/dbsettings.php" );
+
+$ini = new INIFile( "site.ini" ); // get language settings
+$DOC_ROOT = $ini->read_var( "eZForumMain", "DocumentRoot" );
+
+
 include_once( "ezphputils.php" );
 include_once( "template.inc" );
-include_once( "$DOCROOT/classes/ezforumforum.php" );
-include_once( "$DOCROOT/classes/ezforummessage.php" );
-include_once( "$DOCROOT/classes/ezsession.php" );
-include_once( "$DOCROOT/classes/ezuser.php" );
-include_once( "$DOCROOT/classes/eztemplate.php" );
+include_once( $DOC_ROOT . "classes/ezforumforum.php" );
+include_once( $DOC_ROOT . "classes/ezforummessage.php" );
+include_once( $DOC_ROOT . "classes/ezsession.php" );
+include_once( $DOC_ROOT . "classes/ezuser.php" );
+include_once( "classes/eztemplate.php" );
 
 $session = new eZSession;
 
-$ini = new INIFile( "ezforum.ini" ); // get language settings
+$ini = new INIFile( "site.ini" ); // get language settings
 $Language = $ini->read_var( "MAIN", "Language" );
 
-$t = new eZTemplate( "$DOCROOT/templates", "$DOCROOT/intl", $Language, "category.php" );
+$t = new eZTemplate( "$DOC_ROOT/templates", "$DOC_ROOT/intl", $Language, "category.php" );
 $t->setAllStrings();
 
 $t->set_file( array("category" => "category.tpl",
@@ -35,7 +41,7 @@ $t->set_file( array("category" => "category.tpl",
                     )
               );
 
-$t->set_var( "docroot", $DOCROOT);
+$t->set_var( "docroot", $DOC_ROOT);
 $t->set_var( "category_id", $category_id );
 
 if ( $session->get( $AuthenticatedSession ) == 0 )
