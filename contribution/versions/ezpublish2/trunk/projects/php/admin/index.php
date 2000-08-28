1f33d2dd3367541cfc06df2425c87c7d1c2abd04
Bård Farstad <bf@ez.no>
2000-08-28 17:25:07 +0000

    Fikset ezpublish til � bruke site.ini.

--- contribution/versions/ezpublish2/trunk/projects/php/admin/index.php
+++ contribution/versions/ezpublish2/trunk/projects/php/admin/index.php
@@ -18,7 +18,7 @@ include_once( "template.inc" );
 include_once( "../classes/ezuser.php" );
 
 
-include_once( "../ezpublish/settings/dbconnect.php" );
+//include_once( "../ezpublish/settings/dbconnect.php" );
 //include_once( "../dbsettings.php" );
 
 include_once( "../classes/ezsession.php" );
