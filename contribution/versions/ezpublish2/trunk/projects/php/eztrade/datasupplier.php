5abb162e51e285b4d892afc035e06ae32d6990ce
Bård Farstad <bf@ez.no>
2000-09-14 15:51:00 +0000

    Ny fil.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/eztrade/datasupplier.php
@@ -0,0 +1,19 @@
+<?
+//print $REQUEST_URI;
+
+$url_array = explode( "/", $REQUEST_URI );
+
+switch ( $url_array[2] )
+{
+    case "product" :
+        print( "<h1>Product</h1>" );
+        break;
+    case "search" :
+        print( "<h1>Product search</h1>" );        
+        break;
+    default :
+        print( "<h1>Sorry, Your PRODUCT page could not be found. </h1>" );
+        break;
+}
+
+?>
