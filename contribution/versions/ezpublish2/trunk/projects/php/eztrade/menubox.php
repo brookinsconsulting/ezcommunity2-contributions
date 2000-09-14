cd98ef8a5a5ba22c764e3db100e0672ea783c06e
Bård Farstad <bf@ez.no>
2000-09-14 15:46:47 +0000

    Ny Fil.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/eztrade/menubox.php
@@ -0,0 +1,30 @@
+<?
+// Add templates
+?>
+
+<tr bgcolor="#C85028"> 
+	<td>
+        <p class="smallhead">
+	Shopping
+        </p>
+        </td>
+</tr>
+<tr>
+	<td>
+	<a class="small" href="/trade/product/">
+	<img src="/images/prikk.gif" width="8" heigth="8" border="0">&nbsp;
+    Shop main test 
+	</a>
+	</td>
+</tr>
+<tr>
+	<td>
+	<a class="small" href="/trade/search/">
+	<img src="/images/prikk.gif" width="8" heigth="8" border="0">&nbsp;
+    Search
+	</a>
+	</td>
+</tr>
+<tr> 
+	<td width="100%"><img src="/images/1x1.gif" width="120" height="4"></td>
+</tr>
