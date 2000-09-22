a7668c49c33c5b85ea69caa87474bdec642695e9
eZ Systems <info@ez.no>
2000-09-22 09:31:42 +0000

    Inital commit.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/eznews/admin/intl/no_NO/menubox.php
@@ -0,0 +1,21 @@
+<table width="100%" border="0" cellspacing="0" cellpadding="4">
+<tr bgcolor="#500A0A"> 
+<td> 
+<p class="smallhead">Nyheter</p>
+</td>
+</tr>
+<tr bgcolor="#C8B4B4"> 
+<td>
+<a class="small" href="/news/testbench/">Test benk</a><br>
+<br>
+<a class="small" href="/news/category/browse">Kategorier</a><br>
+<a class="small" href="/news/category/create">Ny kategori</a><br>
+<br>s
+<a class="small" href="/news/article/browse">List artikler</a><br>
+<a class="small" href="/news/article/create">Ny artikkel</a><br>
+
+</td>
+</tr>
+</table>
+
+<img src="images/1x1.gif" width="120" height="4" border="0"><br>
