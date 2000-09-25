24112f79722716a7f4324740d8016e6d6a4290f0
eZ Systems <info@ez.no>
2000-09-25 09:12:57 +0000

    Aaarrrgh, forgot the .ini on this file. Had to move it.

--- contribution/versions/ezpublish2/trunk/projects/php/eznews/admin/intl/no_NO/eznewsflowereditor.php
+++
@@ -1,7 +0,0 @@
-[strings]
-create_page_title=Skriv en artikkel
-create_title=Skriv tittelen på artikkelen
-create_body=Artikkel tekst
-create_select_category=Velg kategori artikkelen skal inn i
-create_price=Artikkel pris
-create_picture=Artikkel bilde (valgfritt)
