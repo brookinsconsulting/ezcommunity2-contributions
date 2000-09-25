cce8b5f55239f1a242f22a901e5689a24c3bcdd4
eZ Systems <info@ez.no>
2000-09-25 09:06:05 +0000

    Initial release.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/eznews/admin/intl/no_NO/eznewsflowereditor.php
@@ -0,0 +1,7 @@
+[strings]
+create_page_title=Skriv en artikkel
+create_title=Skriv tittelen på artikkelen
+create_body=Artikkel tekst
+create_select_category=Velg kategori artikkelen skal inn i
+create_price=Artikkel pris
+create_picture=Artikkel bilde (valgfritt)
---
+++ contribution/versions/ezpublish2/trunk/projects/php/eznews/classes/eznewsflower.php
@@ -0,0 +1,7 @@
+<?
+
+class eZNewsArticleProduct extends eZNewsArticle
+{
+}
+
+?>
