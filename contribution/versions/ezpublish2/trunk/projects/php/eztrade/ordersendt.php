d2d8288882cc5b63076fe9cc461adb17a17a6c8a
Bård Farstad <bf@ez.no>
2000-10-08 13:07:23 +0000

    Ny Fil.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/eztrade/ordersendt.php
@@ -0,0 +1,28 @@
+<?php
+// 
+// $Id: ordersendt.php,v 1.1 2000/10/08 13:07:23 bf-cvs Exp $
+//
+// 
+//
+// Bård Farstad <bf@ez.no>
+// Created on: <06-Oct-2000 14:04:17 bf>
+//
+// Copyright (C) 1999-2000 eZ Systems.  All rights reserved.
+//
+// IMPORTANT NOTE: You may NOT copy this file or any part of it into
+// your own programs or libraries.
+//
+
+$t = new eZTemplate( "eztrade/" . $ini->read_var( "eZTradeMain", "TemplateDir" ) . "/ordersendt/",
+                     "eztrade/intl/", $Language, "ordersendt.php" );
+
+$t->setAllStrings();
+
+$t->set_file( array(
+    "order_sendt_tpl" => "ordersendt.tpl"
+    ) );
+
+
+$t->pparse( "output", "order_sendt_tpl" );
+
+?>
