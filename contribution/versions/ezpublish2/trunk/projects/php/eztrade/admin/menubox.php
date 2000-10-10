87c57e8bb56c6fe774429af16f06dd50c722048a
eZ Systems <info@ez.no>
2000-10-10 12:00:05 +0000

    lagt til et bilde

--- contribution/versions/ezpublish2/trunk/projects/php/eztrade/admin/menubox.php
+++ contribution/versions/ezpublish2/trunk/projects/php/eztrade/admin/menubox.php
@@ -1,5 +1,5 @@
 <tr>
-    <td background="/images/<? echo $SiteStyle; ?>/menu-t.gif" valign="top"><img src="/images/<? echo $SiteStyle; ?>/1x1.gif" width="15" height="1"><img src="/images/<? echo $SiteStyle; ?>/menu-forum.gif" width="120" height="50"></td>
+    <td background="/images/<? echo $SiteStyle; ?>/menu-t.gif" valign="top"><img src="/images/<? echo $SiteStyle; ?>/1x1.gif" width="15" height="1"><img src="/images/<? echo $SiteStyle; ?>/menu-trade.gif" width="120" height="50"></td>
 </tr>
 </tr>
 <tr> 
