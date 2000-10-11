a8a95a2c906b589e74b81ec249c16352069fc89c
Bård Farstad <bf@ez.no>
2000-10-11 10:05:48 +0000

    Ryddet opp i templates.

--- contribution/versions/ezpublish2/trunk/projects/php/ezforum/category.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezforum/category.php
@@ -1,6 +1,6 @@
 <?
 /*!
-    $Id: category.php,v 1.27 2000/10/03 16:47:19 bf-cvs Exp $
+    $Id: category.php,v 1.28 2000/10/11 10:05:48 bf-cvs Exp $
 
     Author: Lars Wilhelmsen <lw@ez.no>
     
@@ -12,41 +12,26 @@
 include_once( "classes/INIFile.php" );
 
 $ini = new INIFile( "site.ini" ); // get language settings
-$DOC_ROOT = $ini->read_var( "eZForumMain", "DocumentRoot" );
 
 include_once( "common/ezphputils.php" );
 include_once( "classes/template.inc" );
-include_once( $DOC_ROOT . "classes/ezforumforum.php" );
-include_once( $DOC_ROOT . "classes/ezforummessage.php" );
-include_once( $DOC_ROOT . "classes/ezforumcategory.php" );
-//  include_once( "classes/ezsession.php" );
-//  include_once( "classes/ezuser.php" );
-//  include_once( "classes/eztemplate.php" );
+include_once( "ezforum/classes/ezforumforum.php" );
+include_once( "ezforum/classes/ezforummessage.php" );
+include_once( "ezforum/classes/ezforumcategory.php" );
 
 $session = new eZSession;
 
 
 $Language = $ini->read_var( "eZForumMain", "Language" );
 
-$t = new eZTemplate( $DOC_ROOT . "/templates", $DOC_ROOT. "/intl", $Language, "category.php" );
+$t = new eZTemplate( "ezforum/templates", "ezforum/intl", $Language, "category.php" );
 $t->setAllStrings();
 
-$t->set_file( array("category" => "category.tpl",
-                    "elements" => "category-elements.tpl",
-                    "navigation" => "navigation.tpl",
-                    "navigation-bottom" => "navigation-bottom.tpl",
-                    "no-forums" => "noforums.tpl",
-                    "login" => "login.tpl",
-                    "logout" => "logout.tpl"
-                    )
-              );
+$t->set_file( "category_tpl", "category.tpl" );
 
-$t->set_var( "docroot", $DOC_ROOT);
+$t->set_block( "category_tpl", "forum_tpl", "forum" );
+
 $t->set_var( "category_id", $category_id );
-
-//  	<a href="index.php?page=ezforum/category.php">
-
-//  	</a>
 
 $category = new eZForumCategory( );
 $category->get( $category_id );
@@ -57,14 +42,15 @@ if ( $session->get( $AuthenticatedSession ) == 0 )
 {
     $user = new eZUser();
 //      $t->set_var( "user", $user->resolveUser( $session->UserID() ) );
-    $t->parse( "logout-message", "logout", true );
+//      $t->parse( "logout-message", "logout", true );
 }
 else
 {
-    $t->set_var( "user", "Anonym" );
-    $t->parse( "logout-message", "login", true);
+//      $t->set_var( "user", "Anonym" );
+//      $t->parse( "logout-message", "login", true);
 }
-$t->parse( "navigation-bar", "navigation", true);
+
+//  $t->parse( "navigation-bar", "navigation", true);
 
 $forum = new eZForumForum();
 $forums = $forum->getAllForums( $category_id );
@@ -82,17 +68,17 @@ for ($i = 0; $i < count( $forums ); $i++)
     $t->set_var( "messages", $message->countMessages( $t->get_var( "forum_id" ) ) );
     $t->set_var( "color", switchColor( $i, "#f0f0f0", "#dcdcdc" ) );
 
-    $t->parse("forums","elements",true);
+    $t->parse( "forum", "forum_tpl", true );
 }
 
-if ( count( $forums ) == 0 )
-    $t->set_var( "forums", "noforums", true);
+//  if ( count( $forums ) == 0 )
+//      $t->set_var( "forums", "noforums", true);
 
 $t->set_var( "link1-url", "main.php");
 $t->set_var( "link2-url", "search.php");
 
 $t->set_var( "back-url", "main.php");
-$t->parse( "navigation-bar-bottom", "navigation-bottom", true);
+//  $t->parse( "navigation-bar-bottom", "navigation-bottom", true);
 
-$t->pparse( "output", "category" );
+$t->pparse( "output", "category_tpl" );
 ?>
