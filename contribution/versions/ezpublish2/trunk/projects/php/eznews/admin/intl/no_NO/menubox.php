5dabf7413f9714b798e54876d86abcefdd5cf519
eZ Systems <info@ez.no>
2000-10-12 13:58:12 +0000

    Added new menues.

--- contribution/versions/ezpublish2/trunk/projects/php/eznews/admin/intl/no_NO/menubox.php
+++ contribution/versions/ezpublish2/trunk/projects/php/eznews/admin/intl/no_NO/menubox.php
@@ -1,20 +1,21 @@
 <tr>
-    <td><img src="/images/<? echo $SiteStyle; ?>/menu-t.gif" width="150" height="50"></td>
+    <td background="/images/<? echo $SiteStyle; ?>/menu-t.gif" valign="top"><img src="/images/<? echo $SiteStyle; ?>/1x1.gif" width="15" height="1"><img src="/images/<? echo $SiteStyle; ?>/menu-news.gif" width="120" height="50"></td>
 </tr>
 <tr> 
-<td background="/images/<? echo $SiteStyle; ?>/menu-m.gif">
-    <img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="small" href="/news/testbench/">Test benk</a><br>
-<br>
-    <img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="small" href="/news/category/">Kategorier</a><br>
-    <img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="small" href="/news/category/create">Ny kategori</a><br>
-<br>
-    <img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="small" href="/news/article/">List artikler</a><br>
-    <img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="small" href="/news/article/create">Ny artikkel</a><br>
-</td>
+    <td background="/images/<? echo $SiteStyle; ?>/menu-m.gif">
+
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/Begravelse/">Begravelse</a><br>
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/Blomster/">Blomster</a><br>
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/Buketter/">Buketter</a><br>
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/Euro3plast/">Euro3plast</a><br>
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/Hageartikler/">Hageartikler</a><br>
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/Hagesenter/">Hagesenter</a><br>
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/Hundehus/">Hundehus</a><br>
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/Planter/">Planter</a><br>
+    <br>
+	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/news/2?create+child">Legg til artikkel</a><br>
+	</td>
 </tr>
 <tr>
-    <td><img src="/images/<? echo $SiteStyle; ?>/menu-b.gif" width="150" height="30"></td>
-</tr>
-<tr>
-	<td><img src="/images/<? echo $SiteStyle; ?>/1x1.gif" width="10" height="10"></td>
+    <td><img src="/images/<? echo $SiteStyle; ?>/menu-b.gif" width="150" height="40"></td>
 </tr>
