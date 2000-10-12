411f8b6fe5a82df55b2ba7feb9ff2f1b3211b6ab
eZ Systems <info@ez.no>
2000-10-12 11:01:39 +0000

    Moved this file.

--- contribution/versions/ezpublish2/trunk/projects/php/eznews/classes/eznewsflower.php
+++
@@ -1,9 +0,0 @@
-<?
-
-include_once( "eznews/classes/eznewsarticle.php" );
-
-class eZNewsFlower extends eZNewsArticle
-{
-};
-
-?>
---
+++ contribution/versions/ezpublish2/trunk/projects/php/eznews/classes/eznewsflowerarticle.php
@@ -0,0 +1,24 @@
+<?
+// 
+// $Id: eznewsflowerarticle.php,v 1.1 2000/10/12 11:01:39 pkej-cvs Exp $
+//
+// Definition of eZNewsFlowerArticle class
+//
+// Paul K Egell-Johnsen <pkej@ez.no>
+// Created on: <20-Sep-2000 13:03:00 pkej>
+//
+// Copyright (C) 1999-2000 eZ Systems.  All rights reserved.
+//
+// IMPORTANT NOTE: You may NOT copy this file or any part of it into
+// your own programs or libraries.
+//
+/*!
+    A dummy class needed for polymorphic operation...
+ */
+include_once( "eznews/classes/eznewsarticle.php" );
+
+class eZNewsFlowerArticle extends eZNewsArticle
+{
+};
+
+?>
