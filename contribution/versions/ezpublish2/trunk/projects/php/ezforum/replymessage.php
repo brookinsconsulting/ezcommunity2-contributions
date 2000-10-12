0f70b58a73852629e2e7202a05872a95925792ec
Bård Farstad <bf@ez.no>
2000-10-12 10:03:53 +0000

    Fikset i templates og fjernet gammel kode.

--- contribution/versions/ezpublish2/trunk/projects/php/ezforum/replymessage.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezforum/replymessage.php
@@ -1,51 +1,43 @@
 <?
-/*!
-    $Id: replymessage.php,v 1.8 2000/10/11 15:45:26 ce-cvs Exp $
-
-    Author: Lars Wilhelmsen <lw@ez.no>
-    
-    Created on: <14-Jul-2000 12:49:25 lw>
-    
-    Copyright (C) 2000 eZ systems. All rights reserved.
-*/
+// 
+// $Id: replymessage.php,v 1.9 2000/10/12 10:03:53 bf-cvs Exp $
+//
+// 
+//
+// Bård Farstad <bf@ez.no>
+// Created on: <24-Sep-2000 12:20:32 bf>
+//
+// Copyright (C) 1999-2000 eZ Systems.  All rights reserved.
+//
+// IMPORTANT NOTE: You may NOT copy this file or any part of it into
+// your own programs or libraries.
+//
 
 include_once( "classes/INIFile.php" );
 
 $ini = new INIFile( "site.ini" ); // get language settings
-$DOC_ROOT = $ini->read_var( "eZForumMain", "DocumentRoot" );
 
 
 include_once( "common/ezphputils.php" );
 include_once( "classes/template.inc" );
 include_once( "classes/ezdb.php" );
-include_once( $DOC_ROOT . "/classes/ezforummessage.php");
-include_once( $DOC_ROOT . "/classes/ezforumcategory.php");
-include_once( "classes/ezsession.php" );
-include_once( "classes/ezuser.php" );
 
-$msg = new eZforumMessage;
+include_once( "ezforum/classes/ezforummessage.php");
+include_once( "ezforum/classes/ezforumcategory.php");
+
+$msg = new eZforumMessage();
 $t = new Template(".");
 
-$t->set_file("replymessage","$DOC_ROOT/templates/replymessage.tpl");
+$t->set_file("replymessage","ezforum/templates/replymessage.tpl");
 
-$t->set_var( "docroot", $DOC_ROOT);
 $t->set_var( "category_id", $category_id);
 
 $msg->get( $reply_id );
-    
-if ($AuthenticatedSession)
-{
-    $session = new eZSession();
-    $session->get( $AuthenticatedSession );
-    $UserID = $session->UserID();
-}
-else
-{
-    $UserId = 0;
-}
 
 $category = new eZForumCategory();
-$info = $category->categoryForumInfo($forum_id);
+
+//  $info = $category->categoryForumInfo($forum_id);
+
 $infoString = $info["CategoryName"] . "::" . $info["ForumName"];
 
 $user = new eZUser();
@@ -54,9 +46,11 @@ $t->set_var("forum_id", $forum_id );
 $t->set_var("msg_id", $msg->id() );
 $t->set_var("info",  $infoString);
 $t->set_var("topic", ("SV: " . stripslashes( $msg->topic() ) ) );
-$t->set_var("user", $user->resolveUser( $msg->userId() ) );
+
+//  $t->set_var("user", $user->resolveUser( $msg->userId() ) );
+
 $t->set_var("body", nl2br( stripslashes( $msg->body() ) ) );
-$t->set_var("replier", $user->resolveUser( $UserID ) );
+//  $t->set_var("replier", $user->resolveUser( $UserID ) );
 
 $t->pparse("output", "replymessage");
 ?>
