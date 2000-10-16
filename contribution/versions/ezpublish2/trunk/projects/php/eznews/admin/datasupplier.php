6337c3d891b7e50e89cf187e5b2748d5f9b11d36
eZ Systems <info@ez.no>
2000-10-16 13:47:21 +0000

    More test

--- contribution/versions/ezpublish2/trunk/projects/php/eznews/admin/datasupplier.php
+++ contribution/versions/ezpublish2/trunk/projects/php/eznews/admin/datasupplier.php
@@ -1,5 +1,16 @@
 <?
+// 	function utime ()
+// 	{
+// 		$time = explode( " ", microtime());
+// 		$usec = (double)$time[0];
+// 		$sec = (double)$time[1];
+// 		return $sec + $usec;
+//     }
+    
+    #echo "Time start: " . utime() . "<br />\n";
     include_once("eznews/admin/eznewsadmin.php");
     $item=new eZNewsAdmin( "site.ini" );
     $item->doActions();
+    #echo "Time stop: " . utime() . "<br />\n";
+    
 ?> 
