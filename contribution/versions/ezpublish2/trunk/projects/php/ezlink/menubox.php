7d17035414c062e5cc4214d0b89b53df2b449482
Bård Farstad <bf@ez.no>
2000-10-17 10:51:12 +0000

    Added GPL license to the project.

--- contribution/versions/ezpublish2/trunk/projects/php/ezlink/menubox.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezlink/menubox.php
@@ -1,16 +1,28 @@
 <?
 // 
-// $Id: menubox.php,v 1.2 2000/10/17 10:27:29 bf-cvs Exp $
+// $Id: menubox.php,v 1.3 2000/10/17 10:51:12 bf-cvs Exp $
 //
 // 
 //
 // Bård Farstad <bf@ez.no>
 // Created on: <17-Oct-2000 12:16:07 bf>
 //
-// Copyright (C) 1999-2000 eZ Systems.  All rights reserved.
+// This source file is part of eZ publish, publishing software.
+// Copyright (C) 1999-2000 eZ systems as
 //
-// IMPORTANT NOTE: You may NOT copy this file or any part of it into
-// your own programs or libraries.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
 //
 
 include_once( "classes/INIFile.php" );
