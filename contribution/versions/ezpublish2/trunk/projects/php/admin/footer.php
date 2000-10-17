3faf340621e8cb216f644794072de832426d9457
eZ Systems <info@ez.no>
2000-10-17 08:21:58 +0000

    Lagt til logo p� bunnen av admininterface

--- contribution/versions/ezpublish2/trunk/projects/php/admin/footer.php
+++ contribution/versions/ezpublish2/trunk/projects/php/admin/footer.php
@@ -6,6 +6,8 @@
     <td width="98%" background="/images/<? echo $SiteStyle; ?>/main-b.gif"><img src="/images/<? echo $SiteStyle; ?>/1x1.gif" width="392" height="1"></td>
     <td width="1%"><img src="/images/<? echo $SiteStyle; ?>/main-br.gif" width="26" height="44"></td>
 </tr>
+<tr>
+	<td colspan="3" align="right"><a href="http://www.ez.no"><img src="/images/<? echo $SiteStyle; ?>/bottom-ez-logo.gif" width="87" height="25" border="0"></a><img src="/images/<? echo $SiteStyle; ?>/1x1.gif" width="4" height="1" border="0"></td>
 </table>
 
 	</td>
