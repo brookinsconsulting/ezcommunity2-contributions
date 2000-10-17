fc7c916007932a893ee0235789574df9a15224ef
Bård Farstad <bf@ez.no>
2000-10-17 14:54:43 +0000

    Flyttet filer.

--- contribution/versions/ezpublish2/trunk/projects/php/ezforum/sql/ezforum.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/ezforum/sql/ezforum.sql
@@ -1,100 +1,66 @@
 # MySQL dump 7.1
 #
-# Host: localhost    Database: ezforum
+# Host: localhost    Database: zez
 #--------------------------------------------------------
 # Server version	3.22.32
 
 #
-# Table structure for table 'AclTable'
+# Table structure for table 'eZForum_Category'
 #
-CREATE TABLE AclTable (
-  Id int(11) DEFAULT '0' NOT NULL auto_increment,
-  UserId int(11) DEFAULT '0' NOT NULL,
-  ForumId int(11) DEFAULT '0' NOT NULL,
-  CategoryId int(11) DEFAULT '0' NOT NULL,
-  Rights set('READ','WRITE','MODIFY','DELETE'),
-  PRIMARY KEY (Id)
-);
-
-#
-# Dumping data for table 'AclTable'
-#
-
-
-#
-# Table structure for table 'CategoryTable'
-#
-CREATE TABLE CategoryTable (
-  Id int(11) DEFAULT '0' NOT NULL auto_increment,
+DROP TABLE IF EXISTS eZForum_Category;
+CREATE TABLE eZForum_Category (
   Name varchar(20),
   Description varchar(40),
   Private enum('Y','N') DEFAULT 'N',
-  PRIMARY KEY (Id)
+  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  PRIMARY KEY (ID)
 );
 
 #
-# Table structure for table 'ForumTable'
+# Dumping data for table 'eZForum_Category'
 #
-CREATE TABLE ForumTable (
-  Id int(11) DEFAULT '0' NOT NULL auto_increment,
-  CategoryId int(11) DEFAULT '0' NOT NULL,
+
+
+#
+# Table structure for table 'eZForum_Message'
+#
+DROP TABLE IF EXISTS eZForum_Message;
+CREATE TABLE eZForum_Message (
+  ForumID int(11) DEFAULT '0' NOT NULL,
+  Topic varchar(60),
+  Body text,
+  UserID int(11),
+  Parent int(11),
+  EmailNotice enum('N','Y') DEFAULT 'N',
+  PostingTime timestamp(14),
+  TreeID int(11),
+  ThreadID int(11),
+  Depth int(11),
+  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  PRIMARY KEY (ID)
+);
+
+#
+# Dumping data for table 'eZForum_Message'
+#
+
+
+#
+# Table structure for table 'eZForum_Forum'
+#
+DROP TABLE IF EXISTS eZForum_Forum;
+CREATE TABLE eZForum_Forum (
+  CategoryID int(11) DEFAULT '0' NOT NULL,
   Name varchar(20) DEFAULT '' NOT NULL,
   Description varchar(40),
   Moderated enum('Y','N') DEFAULT 'N',
   Private enum('Y','N') DEFAULT 'N',
-  PRIMARY KEY (Id)
+  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  PRIMARY KEY (ID)
 );
 
 #
-# Table structure for table 'MessageTable'
-#
-CREATE TABLE MessageTable (
-  Id int(11) DEFAULT '0' NOT NULL auto_increment,
-  ForumId int(11) DEFAULT '0' NOT NULL,
-  Topic varchar(60),
-  Body text,
-  UserId int(11),
-  PostingTime timestamp(12),
-  Parent int(11),
-  PRIMARY KEY (Id)
-);
-
-#
-# Dumping data for table 'MessageTable'
+# Dumping data for table 'eZForum_Forum'
 #
 
-#
-# Table structure for table 'UserTable'
-#
-CREATE TABLE UserTable (
-  id int(11) DEFAULT '0' NOT NULL auto_increment,
-  group_id int(11) DEFAULT '0' NOT NULL,
-  first_name varchar(30),
-  last_name varchar(35),
-  nick_name varchar(15),
-  email varchar(50),
-  passwd varchar(16),
-  auth_hash varchar(32),
-  state enum('E','D') DEFAULT 'E',
-  company varchar(30),
-  phone_number varchar(14),
-  mobile_number varchar(14),
-  fax_number varchar(14),
-  Address_one varchar(60),
-  Address_two varchar(60),
-  zip_code varchar(6),
-  city varchar(25),
-  country varchar(20),
-  region_info char(2),
-  PRIMARY KEY (id)
-);
 
-#
-# Table structure for table 'session'
-#
-CREATE TABLE session (
-  id int(11) DEFAULT '0' NOT NULL auto_increment,
-  sid char(32) DEFAULT '' NOT NULL,
-  usr int(11) DEFAULT '0' NOT NULL,
-  PRIMARY KEY (id)
-);
