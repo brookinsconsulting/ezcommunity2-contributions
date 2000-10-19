830ad3bbff965aa4569821a48b5516cfa2fd7d0d
Bård Farstad <bf@ez.no>
2000-10-19 11:14:21 +0000

    Nye filer.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/user/articlelist.php
@@ -0,0 +1,133 @@
+<?
+// 
+// $Id: articlelist.php,v 1.1 2000/10/19 11:14:21 bf-cvs Exp $
+//
+// 
+//
+// Bård Farstad <bf@ez.no>
+// Created on: <18-Oct-2000 14:41:37 bf>
+//
+// Copyright (C) 1999-2000 eZ Systems.  All rights reserved.
+//
+// IMPORTANT NOTE: You may NOT copy this file or any part of it into
+// your own programs or libraries.
+//
+
+include_once( "classes/INIFile.php" );
+include_once( "classes/eztemplate.php" );
+include_once( "classes/ezlocale.php" );
+
+include_once( "ezarticle/classes/ezarticlecategory.php" );
+include_once( "ezarticle/classes/ezarticle.php" );
+
+$ini = new INIFIle( "site.ini" );
+
+$Language = $ini->read_var( "eZArticleMain", "Language" );
+
+$t = new eZTemplate( "ezarticle/user/" . $ini->read_var( "eZArticleMain", "TemplateDir" ),
+                     "ezarticle/user/intl/", $Language, "articlelist.php" );
+
+$t->setAllStrings();
+
+$t->set_file( array(
+    "article_list_page_tpl" => "articlelist.tpl"
+    ) );
+
+// path
+$t->set_block( "article_list_page_tpl", "path_item_tpl", "path_item" );
+
+// article
+$t->set_block( "article_list_page_tpl", "category_list_tpl", "category_list" );
+$t->set_block( "category_list_tpl", "category_item_tpl", "category_item" );
+
+// product
+$t->set_block( "article_list_page_tpl", "article_list_tpl", "article_list" );
+$t->set_block( "article_list_tpl", "article_item_tpl", "article_item" );
+
+$category = new eZArticleCategory( $CategoryID );
+
+// path
+$pathArray = $category->path();
+
+$t->set_var( "path_item", "" );
+foreach ( $pathArray as $path )
+{
+    $t->set_var( "category_id", $path[0] );
+
+    $t->set_var( "category_name", $path[1] );
+    
+    $t->parse( "path_item", "path_item_tpl", true );
+}
+
+$categoryList = $category->getByParent( $category );
+
+
+// categories
+$i=0;
+$t->set_var( "category_list", "" );
+foreach ( $categoryList as $categoryItem )
+{
+    $t->set_var( "category_id", $categoryItem->id() );
+
+    $t->set_var( "category_name", $categoryItem->name() );
+
+    $parent = $categoryItem->parent();
+    
+
+    if ( ( $i % 2 ) == 0 )
+    {
+        $t->set_var( "td_class", "bglight" );
+    }
+    else
+    {
+        $t->set_var( "td_class", "bgdark" );
+    }
+    
+    $t->set_var( "category_description", $categoryItem->description() );
+
+    $t->parse( "category_item", "category_item_tpl", true );
+    $i++;
+}
+
+if ( count( $categoryList ) > 0 )    
+    $t->parse( "category_list", "category_list_tpl" );
+else
+    $t->set_var( "category_list", "" );
+
+
+// articles
+$articleList = $category->articles();
+
+$locale = new eZLocale( $Language );
+$i=0;
+$t->set_var( "article_list", "" );
+foreach ( $articleList as $article )
+{
+    $t->set_var( "article_name", $article->name() );
+
+    $t->set_var( "article_id", $article->id() );
+
+    if ( ( $i % 2 ) == 0 )
+    {
+        $t->set_var( "td_class", "bglight" );
+    }
+    else
+    {
+        $t->set_var( "td_class", "bgdark" );
+    }
+
+    $t->parse( "article_item", "article_item_tpl", true );
+    $i++;
+}
+
+if ( count( $articleList ) > 0 )    
+    $t->parse( "article_list", "article_list_tpl" );
+else
+    $t->set_var( "article_list", "" );
+
+
+$t->pparse( "output", "article_list_page_tpl" );
+
+
+?>
+
---
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/user/templates/standard/articleview.tpl
@@ -0,0 +1,8 @@
+<h2>{article_name}</h2><br />
+{intl-article_author}: {author_text} <br />
+
+<p>
+{article_body}
+</p>
+
+
