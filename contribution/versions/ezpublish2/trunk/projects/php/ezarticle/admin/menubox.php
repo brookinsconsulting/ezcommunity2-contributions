34326568100898d5043d81aeda5a0239fd383cea
eZ Systems <info@ez.no>
2000-10-19 15:28:20 +0000

    Layout

--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/admin/menubox.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/admin/menubox.php
@@ -2,7 +2,7 @@
     <td background="/images/<? echo $SiteStyle; ?>/menu-t.gif" valign="top"><img src="/images/<? echo $SiteStyle; ?>/1x1.gif" width="15" height="1"><img src="/images/<? echo $SiteStyle; ?>/menu-news.gif" width="120" height="50"></td>
 </tr>
 <tr> 
-    <td background="/images/<? echo $SiteStyle; ?>/menu-m.gif">
+    <td class="repeaty" background="/images/<? echo $SiteStyle; ?>/menu-m.gif">
 	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/article/archive/">Arkiv</a><br>
 	<img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/article/categoryedit/new/">Ny kategori</a><br>
     <img src="/images/<? echo $SiteStyle; ?>/menu-arrow.gif" width="20" height="10"><a class="menu" href="/article/articleedit/new/">Ny artikkel</a><br>
