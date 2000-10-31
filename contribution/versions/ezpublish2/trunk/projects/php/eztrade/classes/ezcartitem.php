3a552caa4d0b97d07c0d7b49f4223ff862d9b329
Bård Farstad <bf@ez.no>
2000-10-31 22:15:40 +0000

    Fikset GLP i koden.

--- contribution/versions/ezpublish2/trunk/projects/php/eztrade/classes/ezcartitem.php
+++ contribution/versions/ezpublish2/trunk/projects/php/eztrade/classes/ezcartitem.php
@@ -1,6 +1,6 @@
 <?php
 // 
-// $Id: ezcartitem.php,v 1.4 2000/10/25 19:21:41 bf-cvs Exp $
+// $Id: ezcartitem.php,v 1.5 2000/10/31 22:15:40 bf-cvs Exp $
 //
 // Definition of eZCompany class
 //
@@ -9,8 +9,22 @@
 //
 // Copyright (C) 1999-2000 eZ Systems.  All rights reserved.
 //
-// IMPORTANT NOTE: You may NOT copy this file or any part of it into
-// your own programs or libraries.
+// This source file is part of eZ publish, publishing software.
+// Copyright (C) 1999-2000 eZ systems as
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
 //
 
 //!! eZTrade
