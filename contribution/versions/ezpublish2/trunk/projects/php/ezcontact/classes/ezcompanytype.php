0046cfe6bec810f4b26bb8d3bedf0bae9ad92589
eZ Systems <info@ez.no>
2000-11-14 18:31:32 +0000

    Updated.

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/classes/ezcompanytype.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/classes/ezcompanytype.php
@@ -1,15 +1,45 @@
 <?
 
-//!! eZContact
-//!
-/*!
+// 
+// $Id: ezcompanytype.php,v 1.13 2000/11/14 18:31:32 pkej-cvs Exp $
+//
+// Definition of eZCompanyType class
+//
+// <real-name><<email-name>>
+// Created on: <09-Nov-2000 14:52:40 ce>
+//
+// This source file is part of eZ publish, publishing software.
+// Copyright (C) 1999-2000 eZ systems as
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
 
+//!! eZContact
+//! eZCompanyType handles the company types.
+/*!
+    This class handles the company types in the database. A company type can be used to
+    describe different hiearchical information about a company. For example geographical
+    area, business area, etc.
 */
+
+include_once( "classes/ezdb.php" );
 
 class eZCompanyType
 {
     /*!
-      Constructor.
+      Constructor of the eZCompanyType.
     */
     function eZCompanyType( $id="-1", $fetch=true )
     {
@@ -34,9 +64,9 @@ class eZCompanyType
             $this->State_ = "New";
         }
     }
-
+    
     /*!
-      Lagrer informasjonen til databasen.
+      Stores the object in the database.
     */
     function store()
     {
@@ -49,6 +79,10 @@ class eZCompanyType
             $this->Database->query( "INSERT INTO eZContact_CompanyType set Name='$this->Name', Description='$this->Description'" );
 
             $this->ID = mysql_insert_id();
+            
+            $this->Database->query( "INSERT INTO eZContact_CompanyTypeHiearchy set CompanyTypeID='$this->ID', ParentID='$this->ParentID'" );
+
+            $this->ParentID = mysql_insert_id();
 
             $this->State_ = "Coherent";
             $ret = true;
@@ -56,6 +90,7 @@ class eZCompanyType
         else
         {
             $this->Database->query( "UPDATE eZContact_CompanyType set Name='$this->Name', Description='$this->Description' WHERE ID='$this->ID'" );
+            $this->Database->query( "UPDATE eZContact_CompanyTypeHiearchy set ParentID='$this->ParentID' WHERE CompanyTypeID='$this->ID'" );
 
             $this->State_ = "Coherent";
             $ret = true;
@@ -63,39 +98,53 @@ class eZCompanyType
         return $ret;
     }
 
+
     /*
-      Sletter adressetypen fra databasen.
+      Delets the company type from the database.
      */
     function delete()
     {
         $this->dbInit();
         $this->Database->query( "DELETE FROM eZContact_CompanyType WHERE ID='$this->ID'" );
+        $this->Database->query( "DELETE FROM eZContact_CompanyTypeHiearchy WHERE CompanyTypeID='$this->ID'" );
     }
 
     /*
-      Henter ut en firmatype med ID == $id
+        Fetches a company type with the  ID == $id
     */  
     function get( $id )
     {
         $this->dbInit();    
         if ( $id != "" )
         {
-            array_query( $company_type_array, "SELECT * FROM eZContact_CompanyType WHERE ID='$id'" );
+            $this->Database->array_query( $company_type_array, "SELECT * FROM eZContact_CompanyType WHERE ID='$id'" );
+            $this->Database->array_query( $company_type_hiearchy_array, "SELECT ParentID FROM eZContact_CompanyTypeHiearchy WHERE CompanyTypeID='$this->ID'" );
+            
+            
             if ( count( $company_type_array ) > 1 )
             {
-                die( "Feil: Flere companytype med samme ID funnet i database, dette skal ikke være mulig. " );
+                die( "Error: More than one company type with the same ID found. Major problem, clean up the table eZContact_CompanyType. " );
             }
-            else if ( count( $company_type_array ) == 1 )
+            elseif( count( $company_type_array ) == 1 )
             {
                 $this->ID = $company_type_array[ 0 ][ "ID" ];
                 $this->Name = $company_type_array[ 0 ][ "Name" ];
                 $this->Description = $company_type_array[ 0 ][ "Description" ];
             }
+            
+            if( count( $company_type_hiearchy_array ) > 1 )
+            {
+                die( "Error: A company type is only allowed one parent at the moment. " );
+            }
+            elseif( count( $company_type_hiearchy_array ) == 1 )
+            {
+                $this->ParentID = $company_type_hiearchy_array[ 0 ][ "ParentID" ];
+            }
         }
     }
     
     /*!
-
+        Fetches all the company types in the db and return them as an array of objects.
      */
     function getAll( )
     {
@@ -116,23 +165,47 @@ class eZCompanyType
   
   
     /*!
-      Setter navn.
+      Set the name.
     */
     function setName( $value )
     {
+        if ( $this->State_ == "Dirty" )
+            $this->get( $this->ID );
+
         $this->Name = $value;
     }
     /*!
-      Setter navn.
+      Set the description.
     */
     function setDescription( $value )
     {
+        if ( $this->State_ == "Dirty" )
+            $this->get( $this->ID );
+
         $this->Description = $value;
+    }
+    /*!
+      Set parent
+    */
+    function setParent( $value )
+    {
+        if ( $this->State_ == "Dirty" )
+            $this->get( $this->ID );
+
+        $this->ParentID = $value;
     }
 
   
     /*!
-      Returnerer navnet.
+      Returns the ID of the company type.
+    */
+    function id()
+    {
+        return $this->ID;
+    }
+  
+    /*!
+      Returns the name.
     */
     function name( )
     {
@@ -140,12 +213,28 @@ class eZCompanyType
     }
   
     /*!
-      Returnerer kommentaren.
+      Returns the description.
     */
     function description( )
     {
+        if ( $this->State_ == "Dirty" )
+            $this->get( $this->ID );
+
         return $this->Description;
     }
+    
+    /*!
+      Returns the parent.
+    */
+    function parent( )
+    {
+        if ( $this->State_ == "Dirty" )
+            $this->get( $this->ID );
+
+        return $this->ParentID;
+    }
+    
+    
     
     /*!
       \private
@@ -161,6 +250,7 @@ class eZCompanyType
     }
 
     var $ID;
+    var $ParentID;
     var $Name;
     var $Description;
 
