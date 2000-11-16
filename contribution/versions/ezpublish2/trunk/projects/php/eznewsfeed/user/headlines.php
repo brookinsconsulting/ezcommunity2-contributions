a302ab53bb3ace5ef161652154f35615bf40e3d9
Bård Farstad <bf@ez.no>
2000-11-16 11:05:04 +0000

    Nye filer.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/eznewsfeed/user/headlines.php
@@ -0,0 +1,67 @@
+<?
+// 
+// $Id: headlines.php,v 1.1 2000/11/16 11:05:04 bf-cvs Exp $
+//
+// Bård Farstad <bf@ez.no>
+// Created on: <16-Nov-2000 10:51:34 bf>
+//
+// This source file is part of eZ publish, publishing software.
+// Copyright (C) 1999-2000 eZ systems as
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
+
+include_once( "classes/INIFile.php" );
+include_once( "classes/eztemplate.php" );
+include_once( "classes/ezlocale.php" );
+
+include_once( "eznewsfeed/classes/eznews.php" );
+
+include_once( "classes/ezdatetime.php" );
+
+$news = new eZNews( );
+
+$ini = new INIFIle( "site.ini" );
+
+$Language = $ini->read_var( "eZNewsFeedMain", "Language" );
+$ImageDir = $ini->read_var( "eZNewsFeedMain", "ImageDir" );
+
+$t = new eZTemplate( "eznewsfeed/user/" . $ini->read_var( "eZNewsFeedMain", "TemplateDir" ),
+                     "eznewsfeed/user/intl/", $Language, "headlines.php" );
+
+$t->setAllStrings();
+
+$t->set_file( array(
+    "headlines_page_tpl" => "headlines.tpl"
+    ) );
+
+$t->set_block( "headlines_page_tpl", "head_line_tpl", "head_line" );
+
+$newsList = $news->newsList();
+
+foreach ( $newsList as $newsItem )
+{
+    $intro = $newsItem->intro();
+    
+    $t->set_var( "head_line", $newsItem->name() );
+    $t->set_var( "head_line_url", $newsItem->url() );
+    
+    $t->pparse( "head_line", "head_line_tpl" );    
+}
+
+
+$t->pparse( "output", "headlines_page_tpl" );
+
+?>
---
+++ contribution/versions/ezpublish2/trunk/projects/php/eznewsfeed/user/templates/standard/headlines.tpl
@@ -0,0 +1,3 @@
+<!-- BEGIN head_line_tpl -->
+<a target="_blank" class="menu" href="{head_line_url}">{head_line}</a><br /><br />
+<!-- END head_line_tpl -->
