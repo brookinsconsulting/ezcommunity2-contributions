e38cb27d55c6f18e1b1cbb766e75b210194089ca
Bård Farstad <bf@ez.no>
2000-11-27 09:38:58 +0000

    Nye filer for news feed.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/eznewsfeed/admin/sourcesiteedit.php
@@ -0,0 +1,183 @@
+<?php
+// 
+// $Id: sourcesiteedit.php,v 1.1 2000/11/27 09:38:58 bf-cvs Exp $
+//
+// Bård Farstad <bf@ez.no>
+// Created on: <26-Nov-2000 17:55:31 bf>
+//
+// This source file is part of eZ publish, publishing software.
+// Copyright (C) 1999-2000 eZ systems as
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
+
+include_once( "classes/INIFile.php" );
+include_once( "classes/eztemplate.php" );
+include_once( "classes/ezlocale.php" );
+
+include_once( "eznewsfeed/classes/ezsourcesite.php" );
+
+include_once( "eznewsfeed/classes/eznewscategory.php" );
+
+if ( $Action == "Insert" )
+{
+    $category = new eZNewsCategory( $CategoryID );
+    
+    $news = new eZNews( );
+
+    $news->setName( $NewsTitle );
+    $news->setIntro( $NewsIntro );
+    
+    if ( $IsPublished == "on" )
+    {
+        $news->setIsPublished( true );
+    }
+    else
+    {
+        $news->setIsPublished( false );
+    }
+
+    $news->setKeywords( $NewsKeywords );
+    $news->setOrigin( $NewsSource );
+    $news->setURL( $NewsURL );
+    $dateTime = new eZDateTime( 2000, 11, 13, 14, 0, 15 );
+    $news->setOriginalPublishingDate( $dateTime );
+
+    $news->store();
+
+    $category->addNews( $news );
+    Header( "Location: /newsfeed/archive/$CategoryID/" );
+    exit();
+}
+
+if ( $Action == "Update" )
+{
+    $category = new eZNewsCategory( $CategoryID );
+    
+    $news = new eZNews( $NewsID );
+
+    $news->setName( $NewsTitle );
+    $news->setIntro( $NewsIntro );
+
+    if ( $IsPublished == "on" )
+    {
+        $news->setIsPublished( true );
+    }
+    else
+    {
+        $news->setIsPublished( false );
+    }
+
+    $news->setKeywords( $NewsKeywords );
+    $news->setOrigin( $NewsSource );
+    $news->setURL( $NewsURL );
+    $dateTime = new eZDateTime( 2000, 11, 13, 14, 0, 15 );
+    $news->setOriginalPublishingDate( $dateTime );
+
+    $news->store();
+
+    $news->removeFromCategories();
+    $category->addNews( $news );
+    Header( "Location: /newsfeed/archive/$CategoryID/" );
+    exit();
+}
+
+
+$ini = new INIFIle( "site.ini" );
+
+$Language = $ini->read_var( "eZNewsFeedMain", "Language" );
+$ImageDir = $ini->read_var( "eZNewsFeedMain", "ImageDir" );
+
+$t = new eZTemplate( "eznewsfeed/admin/" . $ini->read_var( "eZNewsFeedMain", "AdminTemplateDir" ),
+                     "eznewsfeed/admin/intl/", $Language, "sourcesiteedit.php" );
+
+$t->setAllStrings();
+
+$t->set_file( array(
+    "news_edit_page_tpl" => "sourcesiteedit.tpl"
+    ) );
+
+$t->set_block( "news_edit_page_tpl", "value_tpl", "value" );
+
+$t->set_var( "action_value", "Insert" );
+
+$t->set_var( "news_title_value", "" );
+$t->set_var( "news_source_value", "" );
+$t->set_var( "news_date_value", "" );
+$t->set_var( "news_intro_value", "" );
+$t->set_var( "news_url_value", "" );
+$t->set_var( "news_keywords_value", "" );
+$t->set_var( "news_id", "" );
+
+if ( $Action == "Edit" )
+{
+    $news = new eZNews( $NewsID );
+
+    $t->set_var( "news_title_value", $news->name() );
+    $t->set_var( "news_source_value", $news->origin() );
+    $t->set_var( "news_intro_value", $news->intro() );
+    $t->set_var( "news_url_value", $news->url() );
+    $t->set_var( "news_keywords_value", $news->keywords() );
+    $t->set_var( "news_id", $news->id() );
+    $t->set_var( "action_value", "Update" );
+
+    if ( $news->isPublished() == true )
+    {
+        $t->set_var( "news_is_published", "checked" );
+    }
+    else
+    {
+        $t->set_var( "news_is_published", "" );
+    }
+    
+
+    $cats = $news->categories();
+
+    $defCat = $cats[0];
+}
+
+// category select
+$category = new eZNewsCategory();
+$categoryArray = $category->getAll( );
+
+foreach ( $categoryArray as $catItem )
+{
+    if ( $Action == "Edit" )
+    {
+        if ( $defCat->id() == $catItem->id() )
+        {
+            $t->set_var( "selected", "selected" );
+        }
+        else
+        {
+            $t->set_var( "selected", "" );
+        }
+    }
+    else
+    {
+        $t->set_var( "selected", "" );
+    }    
+    
+    $t->set_var( "option_value", $catItem->id() );
+    $t->set_var( "option_name", $catItem->name() );
+
+    $t->parse( "value", "value_tpl", true );    
+}
+
+
+$t->pparse( "output", "news_edit_page_tpl" );
+
+?>
+
---
+++ contribution/versions/ezpublish2/trunk/projects/php/eznewsfeed/admin/templates/standard/sourcesiteedit.tpl
@@ -0,0 +1,46 @@
+<h1>{intl-sourcesite_edit}</h1>
+<form method="post" action="/newsfeed/sourcesite/">
+<hr noshade="noshade" size="4" />
+
+<p class="boxtext">{intl-source_site_title}:</p>
+<input type="text" size="40" name="SourceSiteTitle" value="{source_site_title_value}" />
+
+<p class="boxtext">{intl-source_site_url}:</p>
+<input type="text" size="40" name="SourceSiteURL" value="{source_site_url_value}" />
+
+<p class="boxtext">{intl-source_site_login}:</p>
+<input type="text" size="40" name="SourceSiteLogin" value="{source_site_login_value}" />
+
+<p class="boxtext">{intl-source_site_password}:</p>
+<input type="text" size="40" name="SourceSitePassword" value="{source_site_password_value}" />
+
+<p class="boxtext">{intl-source_site_category}:</p>
+<select name="CategoryID">
+
+<!-- BEGIN value_tpl -->
+<option value="{option_value}" {selected}>{option_name}</option>
+<!-- END value_tpl -->
+
+</select>
+
+<p class="boxtext">{intl-sourcesite_intro}:</p>
+<textarea cols="40" rows="5" wrap="soft" name="SourcesiteIntro">{sourcesite_intro_value}</textarea>
+
+<p class="boxtext">{intl-sourcesite_url}:</p>
+<input type="text" size="40" name="SourcesiteURL" value="{sourcesite_url_value}"/>
+
+<p class="boxtext">{intl-sourcesite_keywords}:</p>
+<input type="text" size="40" name="SourcesiteKeywords" value="{sourcesite_keywords_value}"/>
+
+<br />
+<input type="checkbox" name="IsPublished" {sourcesite_is_published} />
+<span class="boxtext">{intl-sourcesite_is_published}</span><br />
+
+<hr noshade="noshade" size="4" />
+
+<input type="submit" class="okbutton" value="{intl-ok}" />
+<input class="okbutton" type="submit" name="Cancel" value="{intl-cancel}" />
+<input type="hidden" value="{action_value}" name="Action" />
+<input type="hidden" value="{sourcesite_id}" name="SourcesiteID" />
+
+</form>
