a77695fb807b79d419702a2a83102dd71c159aa9
eZ Systems <info@ez.no>
2000-11-29 15:14:12 +0000

    Added checks for e-mail validity and password lenght.

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/personedit.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/personedit.php
@@ -9,7 +9,7 @@ $ini = new INIFIle( "site.ini" );
 $Language = $ini->read_var( "eZContactMain", "Language" );
 
 include_once( "classes/eztemplate.php" );
-
+include_once( "classes/ezmail.php" );
 include_once( "ezcontact/classes/ezperson.php" );
 
 if( $Action == "delete" )
@@ -70,8 +70,8 @@ $t->set_block( "errors_tpl", "error_email_item_tpl", "error_email_item" );
 $t->set_block( "errors_tpl", "error_personno_item_tpl", "error_personno_item" );
 $t->set_block( "errors_tpl", "error_loginname_item_tpl", "error_loginname_item" );
 $t->set_block( "errors_tpl", "error_password_item_tpl", "error_password_item" );
-$t->set_block( "errors_tpl", "error_passwordmatch_item_tpl", "error_passwordmatch_item" );
-$t->set_block( "errors_tpl", "error_passwordrepeat_item_tpl", "error_passwordrepeat_item" );
+$t->set_block( "errors_tpl", "error_password_too_short_item_tpl", "error_password_too_short_item" );
+$t->set_block( "errors_tpl", "error_email_not_valid_item_tpl", "error_email_not_valid_item" );
 $t->set_block( "errors_tpl", "error_address_item_tpl", "error_address_item" );
 
 $t->set_var( "firstname", "" );
@@ -137,7 +137,18 @@ if( $Action == "insert" || $Action == "update" )
         $error = true;
     }
     else
+    {
         $t->set_var( "error_email_item", "" );
+        if( eZMail::validate( $Online[0] ) )
+        {
+            $t->parse( "error_email_not_valid_item", "error_email_not_valid_item_tpl" );
+            $error = true;
+        }
+        else
+        {
+            $t->set_var( "error_email_not_valid_item", "" );
+        }
+    }    
     
     if( empty( $FirstName ) )
     {
@@ -185,8 +196,18 @@ if( $Action == "insert" || $Action == "update" )
         $error = true;
     }
     else
+    {
         $t->set_var( "error_password_item", "" );
-    
+        if( strlen( $VerifyPassword ) > 3 )
+        {
+            $t->parse( "error_password_too_short_item", "error_password_too_short_item_tpl" );
+            $error = true;
+        }
+        else
+        {
+            $t->set_var( "error_password_too_short_item", "" );
+        }
+    }
     if( empty( $PasswordRepeat ) && !empty( $Password ) && empty( $UserID ) )
     {
         $t->parse( "error_passwordrepeat_item", "error_passwordrepeat_item_tpl" );
@@ -239,6 +260,13 @@ if( $Action == "insert" && $error == false && $Add_User == true )
         $user->store();
         $UserID = $user->id();
         $Add_User = false;
+                    
+        // add user to usergroup
+        $AnonymousUserGroup = $ini->read_var( "eZUserMain", "AnonymousUserGroup" );
+        setType( $AnonymousUserGroup, "integer" );
+
+        $group = new eZUserGroup( $AnonymousUserGroup );
+        $group->addUser( $user );
     }
 }
 
