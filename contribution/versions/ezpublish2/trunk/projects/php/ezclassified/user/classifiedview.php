0013992f3eac79de88ac1213c8006b4ea70561bb
eZ Systems <info@ez.no>
2000-12-01 12:59:32 +0000

    lagt til filer

---
+++ contribution/versions/ezpublish2/trunk/projects/php/ezclassified/user/classifiedview.php
@@ -0,0 +1,163 @@
+<?
+/*
+  Editere firma.
+*/
+
+include_once( "classes/INIFile.php" );
+
+$ini = new INIFIle( "site.ini" );
+$Language = $ini->read_var( "eZClassifiedMain", "Language" );
+
+include_once( "ezclassified/classes/ezposition.php" );
+include_once( "ezclassified/classes/ezcategory.php" );
+include_once( "ezcontact/classes/ezcompany.php" );
+include_once( "ezcontact/classes/ezonline.php" );
+include_once( "ezcontact/classes/ezaddress.php" );
+
+if ( isSet ( $Back ) )
+{
+    header( "Location: /contact/companytype/list/" );
+    exit();
+}
+
+$t = new eZTemplate( "ezclassified/user/" . $ini->read_var( "eZClassifiedMain", "TemplateDir" ),
+                     "ezclassified/user/intl", $Language, "classifiedview.php" );
+$t->setAllStrings();
+
+$t->set_file( array(                    
+    "classified_edit" => "classifiedview.tpl"
+    ) );
+
+$t->set_block( "classified_edit", "company_view_tpl", "company_view" );
+$t->set_block( "company_view_tpl", "address_item_tpl", "address_item" );
+$t->set_block( "company_view_tpl", "phone_item_tpl", "phone_item" );
+$t->set_block( "company_view_tpl", "fax_item_tpl", "fax_item" );
+$t->set_block( "company_view_tpl", "web_item_tpl", "web_item" );
+$t->set_block( "company_view_tpl", "email_item_tpl", "email_item" );
+$t->set_block( "company_view_tpl", "image_view_tpl", "image_view" );
+$t->set_block( "company_view_tpl", "logo_view_tpl", "logo_view" );
+$t->set_block( "company_view_tpl", "no_image_tpl", "no_image" );
+$t->set_block( "company_view_tpl", "no_logo_tpl", "no_logo" );
+
+
+$position = new eZPosition( $PositionID );
+
+$t->set_var( "classified_name", $position->name() );
+$t->set_var( "classified_id", $position->id() );
+$t->set_var( "classified_description", $position->description() );
+$t->set_var( "classified_contact_person", $position->contactPerson() );
+$t->set_var( "classified_pay", $position->pay() );
+$t->set_var( "classified_worktime", $position->WorkTime() );
+$t->set_var( "classified_duration", $position->Duration() );
+$t->set_var( "classified_workplace", $position->WorkPlace() );
+
+// Template variabler.
+$Action_value = "update";
+
+$company = $position->company();
+
+$t->set_var( "company_name", $company->name() );
+$t->set_var( "company_description", $company->comment() );
+$t->set_var( "company_companyno", $company->companyNo() );
+
+// View logo.
+$logoImage = $company->logoImage();
+
+if ( ( get_class ( $logoImage ) == "ezimage" ) && ( $logoImage->id() != 0 ) )
+{
+    $variation = $logoImage->requestImageVariation( 150, 150 );
+
+    $t->set_var( "logo_image_src", "/" . $variation->imagePath() );
+    $t->set_var( "logo_name", $logoImage->name() );
+    $t->set_var( "logo_id", $logoImage->id() );
+
+    $t->set_var( "no_logo", "" );
+    $t->parse( "logo_view", "logo_view_tpl" );
+}
+else
+{
+    $t->set_var( "logo_view", "" );
+    $t->parse( "no_logo", "no_logo_tpl" );
+}
+// Telephone list
+$phoneList = $company->phones( $company->id() );
+
+if ( count( $phoneList ) <= 2 )
+{
+    for( $i=0; $i<count ( $phoneList ); $i++ )
+    {
+        if ( $phoneList[$i]->phoneTypeID() == 1 )
+        {
+            $t->set_var( "tele_phone_id", $phoneList[$i]->id() );
+            $t->set_var( "telephone", $phoneList[$i]->number() );
+        }
+        if ( $phoneList[$i]->phoneTypeID() == 2 )
+        {
+            $t->set_var( "fax_phone_id", $phoneList[$i]->id() );
+            $t->set_var( "fax", $phoneList[$i]->number() );
+        }
+
+        $t->parse( "phone_item", "phone_item_tpl" );
+        $t->parse( "fax_item", "fax_item_tpl" );
+    }
+}
+
+// Address list
+$addressList = $company->addresses( $company->id() );
+if ( count ( $addressList ) == 1 )
+{
+    foreach( $addressList as $addressItem )
+        {
+            $t->set_var( "address_id", $addressItem->id() );
+            $t->set_var( "street1", $addressItem->street1() );
+            $t->set_var( "street2", $addressItem->street2() );
+            $t->set_var( "zip", $addressItem->zip() );
+            $t->set_var( "place", $addressItem->place() );
+            
+            $t->set_var( "company_id", $CompanyID );
+            
+            $t->set_var( "script_name", "companyedit.php" );
+
+            $t->parse( "address_item", "address_item_tpl", true );
+            
+        }
+}
+
+// Online list
+$onlineList = $company->onlines( $company->id() );
+
+if ( count ( $onlineList ) <= 2 )
+{
+    for( $i=0; $i<count ( $onlineList ); $i++ )
+    {
+        if ( $onlineList[$i]->onlineTypeID() == 1 )
+        {
+            $t->set_var( "web_online_id", $onlineList[$i]->id() );
+            $t->set_var( "web", $onlineList[$i]->URL() );
+        }
+        if ( $onlineList[$i]->onlineTypeID() == 2 )
+        {
+            $t->set_var( "email_online_id", $onlineList[$i]->id() );
+            $t->set_var( "email", $onlineList[$i]->URL() );
+        }
+            
+    }
+    $t->parse( "web_item", "web_item_tpl" );
+    $t->parse( "email_item", "email_item_tpl" );
+
+}
+$t->set_var( "company_id", $company->id() );
+$t->parse( "company_view", "company_view_tpl" );
+
+
+    
+// Templateoun variabler.
+
+$t->set_var( "error", $error );
+$t->set_var( "errors_item", $error );
+
+$t->set_var( "action_value", $Action_value );
+
+$t->pparse( "output", "classified_edit"  );
+
+?>
