d64b384d346aae19543a1f3615e2928371e2ba1f
Bård Farstad <bf@ez.no>
2000-12-04 16:57:10 +0000

    Clearcache script.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/clearcache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Clearing the cache."
+
+rm -f ezlink/cache/*.cache
+rm -f ezforum/cache/*.cache
+rm -f ezpoll/cache/*.cache
+rm -f eztrade/cache/*.cache
+rm -f ezarticle/cache/*.cache
+
