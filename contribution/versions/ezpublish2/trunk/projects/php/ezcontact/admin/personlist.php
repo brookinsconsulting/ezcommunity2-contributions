0984c63b5231e3b7c3680316fd11b5dd3474c0d2
eZ Systems <info@ez.no>
2000-12-04 10:32:34 +0000

    Added proper functionality.

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/personlist.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/personlist.php
@@ -13,7 +13,8 @@ include_once( "ezcontact/classes/ezperson.php" );
 $t->set_file( array(
     "person_page" => "personlist.tpl"
     ) );    
-
+$t->set_block( "person_page", "person_item_tpl", "person_item" );
+$t->set_var( "person_item", "" );
 $person = new eZPerson();
 $persons = $person->getAll();
 
@@ -28,17 +29,17 @@ for( $i = 0; $i < $count; $i++ )
 {
     if( ( $i % 2 ) == 0 )
     {
-        $t->set_var( "bg_color", "#eeeeee" );
+        $t->set_var( "bg_color", "bglight" );
     }
     else
     {
-        $t->set_var( "bg_color", "#dddddd" );
+        $t->set_var( "bg_color", "bgdark" );
     }
 
     $t->set_var( "person_id", $persons[$i]->id() );
     $t->set_var( "person_firstname", $persons[$i]->firstName() );
     $t->set_var( "person_lastname", $persons[$i]->lastName() );
-    $t->parse( "person_list", "person_item", true );
+    $t->parse( "person_item", "person_item_tpl", true );
     
 }
 
