a2533f0bb0c73f9f8628931a1788d6dbf4ad6e52
eZ Systems <info@ez.no>
2000-12-21 15:17:53 +0000

    When I get tired, I really get tired.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/clearcache.sh
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/clearcache.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 echo "Clearing the cache."
 
-rm -f ezlink/cache/*.cache
-rm -f ezforum/cache/*.cache
-rm -f ezpoll/cache/*.cache
-rm -f eztrade/cache/*.cache
-rm -f ezarticle/cache/*.cache
+dirs="
+ezlink
+ezforum
+ezpoll
+eztrade
+ezarticle
+"
 
+for dir in $dirs
+do
+    if [ -d $dir ]; then
+	    echo "Clearing $dir"
+        rm -f $dir/cache/*.cache
+    else
+        echo "Creating $dir"
+	    mkdir -p $dir
+    fi
+    chmod 777 $dir   
+done
