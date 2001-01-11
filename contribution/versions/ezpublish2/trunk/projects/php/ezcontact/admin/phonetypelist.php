fec60d13faa8e044182b4eb578b6f7bbf45783c2
eZ Systems <info@ez.no>
2001-01-11 21:51:51 +0000

    Reuses typelist.php to make it easier to maintain the list code.

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/phonetypelist.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/phonetypelist.php
@@ -1,91 +1,14 @@
 <?
 
-$ini =& $GlobalSiteIni;
-$Language = $ini->read_var( "eZContactMain", "Language" );
-$DOC_ROOT = $ini->read_var( "eZContactMain", "DocumentRoot" );
-
-include_once( "classes/eztemplate.php" );
-
-include_once( "ezuser/classes/ezuser.php" );
-include_once( "ezuser/classes/ezusergroup.php" );
-include_once( "ezuser/classes/ezmodule.php" );
-include_once( "ezuser/classes/ezpermission.php" );
-
 include_once( "ezcontact/classes/ezphonetype.php" );
 
-require( "ezuser/admin/admincheck.php" );
-
-$t = new eZTemplate( $DOC_ROOT . "/admin/" . $ini->read_var( "eZContactMain", "AdminTemplateDir" ), $DOC_ROOT . "admin/intl", $Language, "phonetype.php" );
-$t->setAllStrings();
-
-$item_type = new eZPhoneType( $PhoneTypeID );
+$language_file = "phonetype.php";
 $page_path = "/contact/phonetype";
-$item_error = true;
-
-if( empty( $HTTP_REFERER ) )
-{
-    if( empty( $BackUrl ) )
-    {
-        $back_command = "$page_path/list";
-    }
-    else
-    {
-        $back_command = $BackUrl;
-    }
-}
-else
-{
-    $back_command = $HTTP_REFERER;
-}
-
-$t->set_file( array(
-    "list_page" =>  "typelist.tpl",
-    ) );
-$t->set_block( "list_page", "list_item_tpl", "list_item" );
-$t->set_block( "list_item_tpl", "line_item_tpl", "line_item" );
-$t->set_block( "list_page", "no_line_item_tpl", "no_line_item" );
-
-$t->set_var( "no_line_item", "" );    
-$t->set_var( "line_item", "" );    
-$t->set_var( "list_item", "" );    
-
-$t->set_var( "item_edit_command", "$page_path/edit" );
-$t->set_var( "item_delete_command", "$page_path/delete" );
-$t->set_var( "item_view_command", "$page_path/view" );
-$t->set_var( "item_list_command", "$page_path/list" );
-$t->set_var( "item_new_command", "$page_path/new" );
-$t->set_var( "item_id", $ItemID );
-$t->set_var( "item_name", $ItemName );
-$t->set_var( "back_url", $back_command );
-$t->set_var( "item_back_command", $back_command );
 
 $item_type = new eZPhoneType();
 $item_type_array = $item_type->getAll();
-$count = count( $item_type_array );
+$move_item = true;
 
-$i = 0;
-foreach( $item_type_array as $item )
-{
-    if ( ( $i %2 ) == 0 )
-        $t->set_var( "bg_color", "bglight" );
-    else
-        $t->set_var( "bg_color", "bgdark" );
-    $i++;
+include( "ezcontact/admin/typelist.php" );
 
-    $t->set_var( "item_id", $item->id() );
-    $t->set_var( "item_name", $item->name() );
-
-    $t->parse( "line_item", "line_item_tpl", true );
-} 
-
-if( $count < 1 )
-{
-    $t->parse( "no_line_item", "no_line_item_tpl" );
-}
-else
-{
-    $t->parse( "list_item", "list_item_tpl" );
-}
-
-$t->pparse( "output", "list_page" );
 ?>
