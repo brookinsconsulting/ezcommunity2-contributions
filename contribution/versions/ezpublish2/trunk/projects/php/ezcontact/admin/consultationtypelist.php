15fa68d077e4a2bd9d68e03f6c16149659a55ab0
eZ Systems <info@ez.no>
2001-01-12 17:44:32 +0000

    Initialized the $SortPage variable to tell the generic lister to turn on hyperlinks on names

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/consultationtypelist.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/consultationtypelist.php
@@ -6,6 +6,7 @@ $language_file = "consultationtype.php";
 $page_path = "/contact/consultationtype";
 $item_type_array = eZConsultationType::findTypes();
 $move_item = true;
+$SortPage = "/contact/consultation/type/list";
 
 include( "ezcontact/admin/typelist.php" );
 
