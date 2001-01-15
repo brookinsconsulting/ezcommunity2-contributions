bd1dfb17ac93e230ce94bc45310f1ee13a9c196a
eZ Systems <info@ez.no>
2001-01-15 14:41:47 +0000

    Added consultations for the users and rewrote the user cases.

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/user/datasupplier.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/user/datasupplier.php
@@ -32,62 +32,56 @@ switch ( $url_array[2] )
         }
         break;
     }
+
     case "person":
     {
-        switch( $url_array[3] )
+        $PersonID = $url_array[4];
+        $Action = $url_array[3];
+        switch( $Action )
         {
-            #case "list":
-            #{
-            #    $PersonID = $url_array[4];
-            #    $Action = "list";
-            #    include( "ezcontact/user/personlist.php" );
-            #    break;
-            #}
+            // intentional fall through
             case "new":
-            {
-                $Action = "new";
-                include( "ezcontact/user/personedit.php" );
-                break;
-            }
+            case "edit":
+            case "update":
+            case "delete":
             case "insert":
             {
-                $PersonID = $url_array[4];
-                $Action = "insert";
-                include( "ezcontact/user/personedit.php" );
+                include( "ezcontact/admin/personedit.php" );
+                break;
+            }
+            case "list":
+            {
+                if ( is_numeric( $url_array[4] ) )
+                    $Index = $url_array[4];
+                include( "ezcontact/admin/personlist.php" );
+                break;
+            }
+            case "search":
+            {
+                if ( is_numeric( $url_array[4] ) )
+                    $Index = $url_array[4];
+                if ( count( $url_array ) >= 5 && !isset( $SearchText ) )
+                {
+                    $SearchText = $url_array[5];
+                    $SearchText = preg_replace( "/[+]/", " ", $SearchText );
+                    $SearchText = preg_replace( "/[%2b]/i", "+", $SearchText );
+                }
+                include( "ezcontact/admin/personlist.php" );
                 break;
             }
             case "view":
             {
-                $PersonID = $url_array[4];
-                $Action = "view";
-                include( "ezcontact/user/personview.php" );
+                include( "ezcontact/admin/personview.php" );
                 break;
             }
-            case "edit":
+            default:
             {
-                $PersonID = $url_array[4];
-                $Action = "edit";
-                include( "ezcontact/user/personedit.php" );
-                break;
-            }
-            case "update":
-            {
-                $PersonID = $url_array[4];
-                $Action = "update";
-                include( "ezcontact/user/personedit.php" );
-                break;
-            }
-            case "delete":
-            {
-                $PersonID = $url_array[4];
-                $Action = "delete";
-                include( "ezcontact/user/persondelete.php" );
+                header( "Location: /contact/error?Type=404&Uri=$REQUEST_URI&Query=$QUERY_STRING&BackUrl=$HTTP_REFERER" );
                 break;
             }
         }
         break;
     }
-    break;
 
     case "company":
     {
@@ -106,6 +100,100 @@ switch ( $url_array[2] )
     break;
 
 
+    case "consultation":
+    {
+        $ConsultationID = $url_array[4];
+        $Action = $url_array[3];
+        switch ( $Action )
+        {
+            // intentional fall through
+            case "new":
+            case "edit":
+            case "update":
+            case "delete":
+            case "insert":
+            {
+                include( "ezcontact/admin/consultationedit.php" );
+                break;
+            }
+            case "list":
+            {
+                include( "ezcontact/admin/consultationlist.php" );
+                break;
+            }
+            case "company":
+            {
+                $SubAction = $url_array[3];
+                $Action = $url_array[4];
+                $CompanyID = $url_array[5];
+//                  $ConsultationID = $url_array[5];
+                switch ( $Action )
+                {
+                    // intentional fall through
+                    case "new":
+                    case "edit":
+                    case "update":
+                    case "delete":
+                    case "insert":
+                    {
+                        include( "ezcontact/admin/consultationedit.php" );
+                        break;
+                    }
+                    case "list":
+                    {
+                        $ConsultationList = true;
+                        include( "ezcontact/admin/consultationlist.php" );
+                        break;
+                    }
+                    case "view":
+                    {
+                        include( "ezcontact/admin/consultationview.php" );
+                        break;
+                    }
+                }
+                break;
+            }
+            case "person":
+            {
+                $SubAction = $url_array[3];
+                $Action = $url_array[4];
+                $PersonID = $url_array[5];
+//                  $ConsultationID = $url_array[5];
+                switch ( $Action )
+                {
+                    // intentional fall through
+                    case "new":
+                    case "edit":
+                    case "update":
+                    case "delete":
+                    case "insert":
+                    {
+                        include( "ezcontact/admin/consultationedit.php" );
+                        break;
+                    }
+                    case "list":
+                    {
+                        $ConsultationList = true;
+                        include( "ezcontact/admin/consultationlist.php" );
+                        break;
+                    }
+                    case "view":
+                    {
+                        include( "ezcontact/admin/consultationview.php" );
+                        break;
+                    }
+                }
+                break;
+            }
+
+            default:
+            {
+                header( "Location: /contact/error?Type=404&Uri=$REQUEST_URI&Query=$QUERY_STRING&BackUrl=$HTTP_REFERER" );
+                break;
+            }
+        }
+        break;
+    }
 
     default :
         print( "<h1>Sorry, This page isn't for you. </h1>" );
