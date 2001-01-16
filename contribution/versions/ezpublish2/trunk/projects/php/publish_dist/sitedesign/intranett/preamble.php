b40ae79db4bb3d1f721a0e7de8aa4d8208c2acb8
eZ Systems <info@ez.no>
2001-01-16 12:14:57 +0000

    Removed files

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/intranett/preamble.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="no" lang="no">
-
-<head>
-<title>eZ publish</title>
-<link rel="stylesheet" type="text/css" href="/sitedesign/standard/style.css">
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
