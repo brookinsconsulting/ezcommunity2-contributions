63dd2b3315bdfbdf4d64d5a72799aa0dbbe4e90e
eZ Systems <info@ez.no>
2001-01-17 15:46:38 +0000

    Fix

--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/modules/prodtjen.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/modules/prodtjen.php
@@ -4,35 +4,35 @@
 
 <table cellpadding="0" cellspacing="0" border="0">
 <tr>
-	<td>
+	<td class="tdmini">
 	<a href="/article/articlestatic/61/">
 	<img src="/images/pt-bgs.gif" width="220" height="84" border="0" alt="" /><br />
 	</a>
 	</td>
 </tr>
 <tr>
-	<td>
+	<td class="tdmini">
 	<a href="/article/articlestatic/62/">
 	<img src="/images/pt-app.gif" width="220" height="84" border="0" alt="" /><br />
 	</a>
 	</td>
 </tr>
 <tr>
-	<td>
+	<td class="tdmini">
 	<a href="/article/articlestatic/63/">
 	<img src="/images/pt-os.gif" width="220" height="84" border="0" alt="" /><br />
 	</a>
 	</td>
 </tr>
 <tr>
-	<td>
+	<td class="tdmini">
 	<a href="/article/articlestatic/64/">
 	<img src="/images/pt-mv.gif" width="220" height="84" border="0" alt="" /><br />
 	</a>
 	</td>
 </tr>
 <tr>
-	<td>
+	<td class="tdmini">
 	<a href="/article/articlestatic/65/">
 	<img src="/images/pt-wh.gif" width="220" height="84" border="0" alt="" /><br />
 	</a>
