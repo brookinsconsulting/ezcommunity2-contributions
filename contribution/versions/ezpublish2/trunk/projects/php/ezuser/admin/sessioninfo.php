87a998613cd83d937e42c2124871216e39d31f91
eZ Systems <info@ez.no>
2001-01-21 18:12:08 +0000

    Change of eZSession usage.

--- contribution/versions/ezpublish2/trunk/projects/php/ezuser/admin/sessioninfo.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezuser/admin/sessioninfo.php
@@ -1,6 +1,6 @@
 <?
 // 
-// $Id: sessioninfo.php,v 1.5 2000/12/23 18:05:36 bf Exp $
+// $Id: sessioninfo.php,v 1.6 2001/01/21 18:12:08 jb Exp $
 //
 // Bård Farstad <bf@ez.no>
 // Created on: <01-Nov-2000 14:34:30 bf>
@@ -37,7 +37,7 @@ require( "ezuser/admin/admincheck.php" );
 
 if ( $Action == "Delete" )
 {
-    $session = new eZSession( $SessionID );
+    $session = eZSession::globalSession( $SessionID );
     $session->delete();
     
     Header( "Location: /user/sessioninfo/" );
