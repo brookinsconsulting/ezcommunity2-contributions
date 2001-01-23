819f7ebbf9941cfc1ce9ef6970b096e454736592
eZ Systems <info@ez.no>
2001-01-23 11:25:52 +0000

    Added support for limiting the listings

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/countrylist.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/countrylist.php
@@ -2,10 +2,17 @@
 
 include_once( "ezcontact/classes/ezcountry.php" );
 
+$ini =& $GlobalSiteIni;
+$Max = $ini->read_var( "eZContactMain", "MaxCountryList" );
+
+include( "ezcontact/admin/typelist_pre.php" );
+
 $language_file = "country.php";
 $page_path = "/contact/country";
-$item_type_array = eZCountry::getAll();
-$SortPage = "/contact/project/type/list";
+
+$item_type_array = eZCountry::getAll( true, $SearchText, $Index, $Max );
+$total_types = eZCountry::getAllCount( $SearchText );
+$Searchable = true;
 
 include( "ezcontact/admin/typelist.php" );
 
