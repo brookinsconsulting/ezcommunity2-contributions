cd712a7ec5e6de5005b3717a9f8bccd4c81bae91
eZ Systems <info@ez.no>
2001-01-23 17:58:18 +0000

    New menubox code.

--- contribution/versions/ezpublish2/trunk/projects/php/eznewsfeed/admin/menubox.php
+++ contribution/versions/ezpublish2/trunk/projects/php/eznewsfeed/admin/menubox.php
@@ -1,8 +1,6 @@
 <?
 // 
-// $Id: menubox.php,v 1.1 2000/11/16 10:30:22 bf-cvs Exp $
-//
-// 
+// $Id: menubox.php,v 1.2 2001/01/23 17:58:18 jb Exp $
 //
 // Bård Farstad <bf@ez.no>
 // Created on: <23-Oct-2000 17:53:46 bf>
@@ -25,25 +23,15 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
 //
 
-include_once( "classes/INIFile.php" );
-$ini = new INIFile( "site.ini" );
+// Supply $menuItems to get a menubox
 
-$Language = $ini->read_var( "eZNewsFeedMain", "Language" );
-
-include_once( "classes/eztemplate.php" );
-
-$t = new eZTemplate( "eznewsfeed/admin/" . $ini->read_var( "eZNewsFeedMain", "AdminTemplateDir" ),
-                     "eznewsfeed/admin/intl", $Language, "menubox.php" );
-
-$t->setAllStrings();
-
-$t->set_file( array(
-    "menu_box_tpl" => "menubox.tpl"
-    ) );
-    
-$t->set_var( "site_style", $SiteStyle );
-
-$t->pparse( "output", "menu_box_tpl" );
-    
+$menuItems = array(
+    array( "/newsfeed/archive/", "{intl-news_archive}" ),
+    array( "/newsfeed/news/new", "{intl-new_news}" ),
+    array( "/newsfeed/category/new", "{intl-new_category}" ),
+    array( "/newsfeed/unpublished/", "{intl-unpublished_news}" ),
+    array( "/newsfeed/importnews/", "{intl-import_news}" ),
+    array( "/newsfeed/sourcesite/new/", "{intl-newsourcesite}" )
+    );
 
 ?>
