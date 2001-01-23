09a6a5d93c623f9f7768f9d38303fffc8c8d8ca6
eZ Systems <info@ez.no>
2001-01-23 11:27:24 +0000

    Uses phonetype limiting.

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/phonetypelist.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/phonetypelist.php
@@ -2,11 +2,13 @@
 
 include_once( "ezcontact/classes/ezphonetype.php" );
 
+include( "ezcontact/admin/typelist_pre.php" );
+
 $language_file = "phonetype.php";
 $page_path = "/contact/phonetype";
 
-$item_type = new eZPhoneType();
-$item_type_array = $item_type->getAll();
+$item_type_array = eZPhoneType::getAll( true, $Index, $Max );
+$total_types = eZPhoneType::getAllCount();
 $move_item = true;
 
 include( "ezcontact/admin/typelist.php" );
