9ee5510526d5881c097478fc7404634920556284
eZ Systems <info@ez.no>
2001-01-25 00:17:47 +0000

    Changed to use the general type edit by extending with custom template.

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/countryedit.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/countryedit.php
@@ -5,6 +5,16 @@ include_once( "ezcontact/classes/ezcountry.php" );
 $language_file = "country.php";
 $item_type = new eZCountry( $CountryID );
 $page_path = "/contact/country";
+$typeedit = "typeedit.tpl";
+$template_array = array( "country_tpl" => "countryedit.tpl" );
+$block_array = array( "extra_type_input" => "country_tpl" );
+
+$func_call = array( "item_id" => "id",
+                    "item_name" => "name",
+                    "item_iso" => "iso" );
+
+$func_call_set = array( "setName" => "ItemName",
+                        "setISO" => "ItemISO" );
 
 include( "ezcontact/admin/typeedit.php" );
 
