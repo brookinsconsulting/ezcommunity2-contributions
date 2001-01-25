8cd20876162ea7b81df0be8e3b198e73e70c3e06
eZ Systems <info@ez.no>
2001-01-25 00:19:52 +0000

    Changed phonetypelist to use function callbacks.

--- contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/phonetypelist.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcontact/admin/phonetypelist.php
@@ -9,6 +9,8 @@ $page_path = "/contact/phonetype";
 
 $item_type_array = eZPhoneType::getAll( true, $Index, $Max );
 $total_types = eZPhoneType::getAllCount();
+$func_call = array( "item_id" => 'id',
+                    "item_name" => 'name' );
 $move_item = true;
 
 include( "ezcontact/admin/typelist.php" );
