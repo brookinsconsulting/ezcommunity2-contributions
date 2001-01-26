060540ebd5847391a727b49b553a5ab2aca22e44
Bård Farstad <bf@ez.no>
2001-01-26 12:32:07 +0000

    Fixed make dist.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/make_dist.sh
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/make_dist.sh
@@ -1,35 +1,4 @@
 #!/bin/bash
 
-cd ezcontact
-rm -f *.php
-rm -rf admin
-rm -rf templates
-rm -rf sql
-rm -rf intl
-rm -rf images
-rm -rf ez.css
-cd ..
-
-file_a=ezaddress.php
-file_b=ezcountry.php
-file_c=ezaddresstype.php
-
-cd ezcontact
-cd classes
-
-for i in *; do
-    if [ "$i" != "$file_a" ]; then
-	if [ "$i" != "$file_b" ]; then
-	    if [ "$i" != "$file_c" ]; then
-		echo "deleting $i"
-		rm -f "$i";
-	    fi
-	fi
-    fi
-done
-
-cd ..
-cd ..
-rm -rf eztrade/
 pwd
 rm make_dist.sh
