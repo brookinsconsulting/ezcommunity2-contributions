3fa66bdd0e254a78299b6fb0a81321a2b6ad2914
Bård Farstad <bf@ez.no>
2001-01-26 09:07:17 +0000

    YABF.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/ezabout/user/datasupplier.php
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/ezabout/user/datasupplier.php
@@ -1,4 +1,4 @@
-<h1>About eZ publish v 1.0.5</h1>
+<h1>About eZ publish v 2.0 beta 1</h1>
 <hr noshade="noshade" size="4" />
 <br />
 
@@ -6,7 +6,6 @@
 eZ publish is an Open Source portal building software, content manager or publishing solution whichever fits your needs. 
 </p><p>
 It is released under the <a href="http://www.gnu.org/copyleft/gpl.html
-">GPL license</a> and can be downloaded from <a href="http://publish.ez.no">publish.ez.no</a>. You can get commercial support from eZ systems at <a href="http://ez.no">ez.no</a> or at <a href="http://sourceprovider.com">sourceprovider.com</a>.<br />
-<br />
-There is a similar project which deals with e-commerce solutions at <a href="http://trade.ez.no">trade.ez.no</a>
+">GPL license</a> and can be downloaded from <a href="http://publish.ez.no">publish.ez.no</a>. You can get commercial support from eZ systems at
+<a href="http://ez.no">ez.no</a> or at <a href="http://sourceprovider.com">sourceprovider.com</a>.<br />
 </p>
