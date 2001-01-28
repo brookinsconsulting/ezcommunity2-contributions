7aa2e5cbcfa3ae5795f5fcacafbdd72eba70d712
Bård Farstad <bf@ez.no>
2001-01-28 14:12:14 +0000

    added new style.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/news/footer.php
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/news/footer.php
@@ -3,26 +3,22 @@
    	<td width="1%" bgcolor="#ffffff"><img src="/images/1x1.gif" width="2" height="1" border="0" alt="0" /></td>
 
 	<td width="1%" bgcolor="#f0f0f0">
-	
-	<!-- Oppslagstavle kommer her! -->
+
+   	<!-- Oppslagstavle kommer her! -->
     
     <?
-        include( "ezuser/user/userbox.php" );
-    ?>
-
-    <?
-        include( "ezpoll/user/votebox.php" );
-    ?>
-
-    <?
-        include( "eztodo/user/todomenulist.php" );
-    ?>
-
-	<?
-	    include( "ezcontact/user/consultationlist.php" );
+          include( "ezuser/user/userbox.php" );
 	?>
-	     
+
+    <?
+          include( "ezpoll/user/votebox.php" );
+    ?>
+        
+	<!-- Oppslagstavle fram til hit! -->
+
+
 	<hr noshade="noshade" size="4" />
+	
 
 	
     <?
@@ -66,35 +62,16 @@ if ( $Design == 2 )
     exit();
 }
 
-if ( $Design == 3 )
-{
-    $session->setVariable( "SiteDesign", "news" );
-    include_once( "classes/ezhttptool.php" );
-
-    $redir = "/";
-    if ( isset( $REQUEST_URI ) && ( $REQUEST_URI != "" ) )
-    {
-        $redir = $REQUEST_URI;
-    }
-        
-    eZHTTPTool::header( "Location: $redir" );
-    exit();
-}
-
 
 ?>
-    <a href="<? print( $REQUEST_URI . "?Design=1"); ?>">Portal site</a><br />
-    <a href="<? print( $REQUEST_URI . "?Design=2"); ?>">E-commerce</a><br />
-    <a href="<? print( $REQUEST_URI . "?Design=3"); ?>">News site</a><br />
-    
-    <!-- Oppslagstavle fram til hit! -->
-	
-	<img src="/images/1x1.gif" width="130" height="1" border="0"><br />
+    <a href="<? print( $REQUEST_URI . "?Design=1"); ?>">Portal site</a> <br />
+    <a href="<? print( $REQUEST_URI . "?Design=2"); ?>">E-commerce</a> <br />
 
+    <img src="/images/1x1.gif" width="130" height="1" border="0"><br />
+	 
 	</td>
   </tr>
 </table>
-
 <div class="credit" align="center" valign="bottom"><br />Powered by <a class="credit" href="http://publish.ez.no">eZ publish</a> made by <img src="/images/logo-mini.gif" width="16" height="16" border="0" alt="0" align="absmiddle" /> <a class="credit" href="http://publish.ez.no">eZ systems</a></div>
 
 </body>
---
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/news/header.php
@@ -0,0 +1,79 @@
+</head>
+
+<body bgcolor="#ffffff" topmargin="6" marginheight="6" leftmargin="6" marginwidth="6"  onLoad="MM_preloadImages('/images/redigerminimrk.gif','/images/slettminimrk.gif','/images/downloadminimrk.gif','/images/addminimrk.gif')">
+
+<table width="100%" cellspacing="0" cellpadding="0" border="0">
+<tr>
+	<td bgcolor="#ffffff">
+    <a href="/">
+    <img src="/sitedesign/news/images/newssitelogo.gif" /></a>
+	</td>
+	<td bgcolor="#ffffff" align="right">
+	<h2>eZ publish v2.0 Beta 2</h2>
+	</td>
+</tr>
+</table>
+
+
+<table width="100%" border="0" cellspacing="0" cellpadding="4">
+<tr valign="top">
+    <td width="1%" bgcolor="#f0f0f0">
+	<!-- Meny start! -->
+
+	<table width="100%" cellspacing="0" cellpadding="2" border="0">
+    <tr>
+		<td colspan="2" class="menuhead">News</td>
+	</tr>
+	<tr>
+		<td width="1%" valign="top"><img src="/images/dot.gif" width="10" height="12"><br /></td>
+		<td width="99%"><a class="menu" href="/article/archive/0/">Latest</a></td>
+	</tr>
+	<tr>
+		<td width="1%" valign="top"><img src="/images/dot.gif" width="10" height="12"><br /></td>
+		<td width="99%"><a class="menu" href="/article/articleheaderlist/0/">Archive</a></td>
+	</tr>
+	<tr>
+		<td colspan="2" class="menuspacer">&nbsp;</td>
+	</tr>
+	</table>
+
+	<?
+	include( "ezarticle/user/menubox.php" );
+	?>
+	
+	<?
+	include( "ezforum/user/menubox.php" );
+	?>
+	
+	<table width="100%" cellspacing="0" cellpadding="2" border="0">
+	<tr>
+		<td colspan="2" class="menuhead">Information</td>
+	</tr>
+	<?
+	     // include the static pages for category 2
+	     $CategoryID = 2;
+	     include( "ezarticle/user/articlelinks.php" );
+	?>
+	<tr>
+		<td colspan="2" class="menuspacer">&nbsp;</td>
+	</tr>
+	</table>
+
+	<!-- Meny end! -->
+	
+	<img src="/images/1x1.gif" width="130" height="1" border="0"><br />
+	</td>
+
+	<td width="1%" bgcolor="#ffffff"><img src="/images/1x1.gif" width="2" height="1" border="0" alt="0" /></td>
+    <td width="96%" bgcolor="#ffffff">
+
+
+    <!-- Banner -->
+
+    <div align="center">
+        <?
+        $CategoryID = 4;
+        $Limit = 1; 
+        include( "ezad/user/adlist.php" );
+        ?>
+    </div><br />
