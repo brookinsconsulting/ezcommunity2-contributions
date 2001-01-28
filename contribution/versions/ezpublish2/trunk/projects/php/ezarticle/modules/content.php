e538fac4cc24628a54511b4379fb1aaf8b01635d
eZ Systems <info@ez.no>
2001-01-28 17:11:56 +0000

    Used heredoc variable for the contents, and added check for article printing to decide whether to print the content box or not.

--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/modules/content.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/modules/content.php
@@ -2,7 +2,7 @@
     global $url_array;
     $page = $url_array[3];
     $viewer = $url_array[2];
-?>
+$str = <<<EOD
 <div style="float: right;">
 <table cellspacing="0" cellpadding="2" border="0">
 <tr>
@@ -12,22 +12,29 @@
 </tr>
 <tr>
     <td bgcolor="#f0f0f0">
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezad">eZ ad</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezaddress">eZ address</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezbug">eZ bug</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezcalendar">eZ calendar</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezcontact">eZ contact</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezfilemanager">eZ filemanager</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezforum">eZ forum</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezimagecatalogue">eZ imagecatalogue</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezlink">eZ link</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#eznewsfeed">eZ newsfeed</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezpoll">eZ poll</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezstats">eZ stats</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#eztodo">eZ todo</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#eztrade">eZ trade</a><br />
-        <a href="/article/<? echo "$viewer/$page"; ?>/3/#ezuser">eZ user</a><br />
+        <a href="/article/$viewer/$page/3/#ezad">eZ ad</a><br />
+        <a href="/article/$viewer/$page/3/#ezaddress">eZ address</a><br />
+        <a href="/article/$viewer/$page/3/#ezbug">eZ bug</a><br />
+        <a href="/article/$viewer/$page/3/#ezcalendar">eZ calendar</a><br />
+        <a href="/article/$viewer/$page/3/#ezcontact">eZ contact</a><br />
+        <a href="/article/$viewer/$page/3/#ezfilemanager">eZ filemanager</a><br />
+        <a href="/article/$viewer/$page/3/#ezforum">eZ forum</a><br />
+        <a href="/article/$viewer/$page/3/#ezimagecatalogue">eZ imagecatalogue</a><br />
+        <a href="/article/$viewer/$page/3/#ezlink">eZ link</a><br />
+        <a href="/article/$viewer/$page/3/#eznewsfeed">eZ newsfeed</a><br />
+        <a href="/article/$viewer/$page/3/#ezpoll">eZ poll</a><br />
+        <a href="/article/$viewer/$page/3/#ezstats">eZ stats</a><br />
+        <a href="/article/$viewer/$page/3/#eztodo">eZ todo</a><br />
+        <a href="/article/$viewer/$page/3/#eztrade">eZ trade</a><br />
+        <a href="/article/$viewer/$page/3/#ezuser">eZ user</a><br />
     </td>
 </tr>
 </table>
 </div>
+EOD;
+
+if( $viewer != "articleprint" )
+{
+    echo $str;
+}
+?>
