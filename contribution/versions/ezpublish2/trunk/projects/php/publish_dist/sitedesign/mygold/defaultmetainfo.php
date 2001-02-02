00c79e5afc790f6cb0ccaea6d3750b3b26c7bc2b
eZ Systems <info@ez.no>
2001-02-02 14:21:45 +0000

    Remocved files

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/mygold/defaultmetainfo.php
+++
@@ -1,4 +0,0 @@
-<meta name="author" content="MyGold.com"/>
-<meta name="copyright" content="MyGold.com &amp;copy; 2001"/>
-<meta name="description" content="Gold, Schmuck und Geschenke zu fairen Preisen"/>
-<meta name="keywords" content="Gold, Schmuck, Webshop, Geschenke, Gutschein, Ohrringe, Halsketten, Ringe, Diamanten"/>
--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/mygold/simplefooter.php
+++
@@ -1,4 +0,0 @@
-<hr size="1" noshade/>
-<div align="center">&copy; 2000 MyGold - Impetex GmbH http://www.mygold.com</div>
-</body>
-</html>
