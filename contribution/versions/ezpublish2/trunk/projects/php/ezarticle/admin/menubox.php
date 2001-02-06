8c56130b0515a3883802dcc5db6c326db6d69be1
eZ Systems <info@ez.no>
2001-02-06 12:10:52 +0000

    Added menu choice, reordered menu.

--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/admin/menubox.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/admin/menubox.php
@@ -1,6 +1,6 @@
 <?
 // 
-// $Id: menubox.php,v 1.9 2001/01/23 17:45:17 jb Exp $
+// $Id: menubox.php,v 1.10 2001/02/06 12:10:52 pkej Exp $
 //
 // Bård Farstad <bf@ez.no>
 // Created on: <23-Oct-2000 17:53:46 bf>
@@ -27,8 +27,9 @@
 
 $menuItems = array(
     array( "/article/archive/", "{intl-archive}" ),
-    array( "/article/categoryedit/new/", "{intl-new_category}" ),
-    array( "/article/articleedit/new/", "{intl-new_article}" )
+    array( "/article/archive/", "{intl-unpublished}" ),
+    array( "/article/articleedit/new/", "{intl-new_article}" ),
+    array( "/article/categoryedit/new/", "{intl-new_category}" )
     );
 
 ?>
