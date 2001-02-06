efa6f77da411bf37d96a73ae2e124568d3295084
eZ Systems <info@ez.no>
2001-02-06 16:44:59 +0000

    Prints out title from the $Title variable.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/standard/preamble.php
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/standard/preamble.php
@@ -2,7 +2,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="no" lang="no">
 
 <head>
-<title>eZ publish</title>
+<title><?
+if ( isset( $Title ) )
+    print( $Title );
+else
+	print( "eZ publish" );
+    ?></title>
 <link rel="stylesheet" type="text/css" href="/sitedesign/standard/style.css">
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 
