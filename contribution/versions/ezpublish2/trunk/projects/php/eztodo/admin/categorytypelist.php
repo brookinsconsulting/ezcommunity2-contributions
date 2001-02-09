13b3cab56aa973bd5da77dd506bf5e7e958ccc3c
eZ Systems <info@ez.no>
2001-02-09 14:55:03 +0000

    Added gpl. Removed extra include. Useing global ini file.

--- contribution/versions/ezpublish2/trunk/projects/php/eztodo/admin/categorytypelist.php
+++ contribution/versions/ezpublish2/trunk/projects/php/eztodo/admin/categorytypelist.php
@@ -1,11 +1,30 @@
 <?
-/*
-  Viser liste over kontakt typer.
-*/
-include_once( "classes/INIFile.php" );
-$ini = new INIFIle( "site.ini" );
-$Language = $ini->read_var( "eZTodoMain", "Language" );
+// 
+// $Id: categorytypelist.php,v 1.4 2001/02/09 14:55:03 pkej Exp $
+//
+// Christoffer A. Elo <ce@ez.no>
+// Created on: <18-Oct-2000 15:04:39 bf>
+//
+// This source file is part of eZ publish, publishing software.
+// Copyright (C) 1999-2001 eZ systems as
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
 
+
+include_once( "classes/INIFile.php" );
 include_once( "classes/eztemplate.php" );
 
 include_once( "ezuser/classes/ezuser.php" );
@@ -15,6 +34,9 @@ include_once( "ezuser/classes/ezpermission.php" );
 
 include_once( "eztodo/classes/eztodo.php" );
 include_once( "eztodo/classes/ezcategory.php" );
+
+$ini =& $GLOBALS["GlobalSiteIni"];
+$Language = $ini->read_var( "eZTodoMain", "Language" );
 
 
 $t = new eZTemplate( "eztodo/admin/" . $ini->read_var( "eZTodoMain", "AdminTemplateDir" ),
