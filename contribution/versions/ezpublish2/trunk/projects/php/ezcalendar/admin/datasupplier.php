8602a58ec5a38957eef4ba94150220ebc2e463a9
Gunnstein Lye <gl@ez.no>
2001-02-19 14:56:34 +0000

    added admin module for eZCalendar, with typeedit/list

---
+++ contribution/versions/ezpublish2/trunk/projects/php/ezcalendar/admin/datasupplier.php
@@ -0,0 +1,36 @@
+<?
+
+switch ( $url_array[2] )
+{
+    case "typelist":
+    {
+        include( "ezcalendar/admin/typelist.php" );
+    }
+    break;
+
+    case "typeedit" :
+    {
+        if ( $url_array[3] == "edit" )
+        {
+            $Action = "Edit";
+            $TypeID = $url_array[4];
+        }
+
+        if ( $url_array[3] == "delete" )
+        {
+            $Action = "Delete";
+            $TypeID = $url_array[4];
+        }
+        
+        include( "ezcalendar/admin/typeedit.php" );
+    }
+    break;
+
+    default :
+    {
+        // go to default module page or show an error message
+        print( "Error: your page request was not found" );
+    }
+}
+
+?>
