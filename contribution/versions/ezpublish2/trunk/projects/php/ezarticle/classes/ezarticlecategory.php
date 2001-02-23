a35db5393bc3468e0adbea665101ba769c8908c7
Frederik Holljen <fh@ez.no>
2001-02-23 10:25:27 +0000

    Fixes...

--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/classes/ezarticlecategory.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/classes/ezarticlecategory.php
@@ -1,6 +1,6 @@
 <?
 // 
-// $Id: ezarticlecategory.php,v 1.34 2001/02/23 10:24:25 fh Exp $
+// $Id: ezarticlecategory.php,v 1.35 2001/02/23 10:25:27 fh Exp $
 //
 // Definition of eZArticleCategory class
 //
@@ -504,7 +504,7 @@ class eZArticleCategory
 
         $database =& eZDB::globalDatabase();
         // check if group
-        $database->query_single( $res, "SELECT OwnerGroupID from eZArticle_ArticleCategory WHERE ID='$categoryID'");
+        $database->query_single( $res, "SELECT OwnerGroupID from eZArticle_Category WHERE ID='$categoryID'");
         $ownerGroupID = $res[ "OwnerGroupID" ];
         $userGroups = $user->groups( true );
         if( in_array( $ownerGroupID, $userGroups ) || $ownerGroupID == 0 )
