c5f73cc140b0946ec2e735cd7f34f25a41fdc46c
eZ Systems <info@ez.no>
2001-03-05 12:54:58 +0000

    Layout

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/trade/header.php
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/trade/header.php
@@ -1,6 +1,6 @@
 </head>
 
-<body bgcolor="#7d7daa" topmargin="6" marginheight="6" leftmargin="6" marginwidth="6"  onLoad="MM_preloadImages('/images/redigerminimrk.gif','/images/slettminimrk.gif','/images/downloadminimrk.gif','/images/addminimrk.gif')">
+<body bgcolor="#8a8ab3" topmargin="6" marginheight="6" leftmargin="6" marginwidth="6"  onLoad="MM_preloadImages('/images/redigerminimrk.gif','/images/slettminimrk.gif','/images/downloadminimrk.gif','/images/addminimrk.gif')">
 
 <img src="/sitedesign/trade/images/ezpublish-trade.gif" height="40" width="610" border="0" alt="" />
 
