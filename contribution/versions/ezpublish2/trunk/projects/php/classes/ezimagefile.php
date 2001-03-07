aa3a684b6b312d1bf39c3f08b236f1b60fbb7a57
eZ Systems <info@ez.no>
2001-03-07 14:53:41 +0000

    Bugfix: Supports animated gifs/pngs. Sets a better file permissions on copied and converted files.

--- contribution/versions/ezpublish2/trunk/projects/php/classes/ezimagefile.php
+++ contribution/versions/ezpublish2/trunk/projects/php/classes/ezimagefile.php
@@ -1,6 +1,6 @@
 <?
 // 
-// $Id: ezimagefile.php,v 1.7 2001/03/05 15:39:37 jb Exp $
+// $Id: ezimagefile.php,v 1.8 2001/03/07 14:53:41 jb Exp $
 //
 // Definition of eZCompany class
 //
@@ -65,9 +65,9 @@ class eZImageFile extends eZFile
     }
 
     /*!
-      Returns true if the uploaded file is a image.
-
-      False is returned if not.
+      Returns true if the uploaded file is an image (As perceived by Apache).
+      Warning!! Do not use this to figure out if a file is an true image,
+      only to see if Apache can serve it directly.
     */
     function isImage()
     {
@@ -83,7 +83,53 @@ class eZImageFile extends eZFile
         return $ret;
     }
 
+    /*!
+      \static
+      Returns a information structure of the image file.
+      The structure is an associative array with the following contents:
+      "suffix" - The suffix of the image file, for instance: png, jpg or gif
+      "dot-suffix" - The suffix of the image file with a dot, is empty if not supported.
+      "image-type" - The image type, for instance: image/jpeg, image/png or image/gif
+      "support" - True if the image type is supported.
+    */
+    function &information( $file, $use_default = false )
+    {
+        $ret = array();
+        $suffix = "";
+        if ( ereg( "\\.([a-zA-Z]+)$", $file, $regs ) )
+        {
+            // We got a suffix, make it lowercase and store it
+            $suffix = strtolower( $regs[1] );
+        }
 
+        // List of supported suffixes
+        $suffix_list = array( "jpg" => array( ".jpg", "image/jpeg" ),
+                              "jpeg" => array( ".jpg", "image/jpeg" ),
+                              "gif" => array( ".gif", "image/gif" ),
+                              "png" => array( ".png", "image/png" ) );
+        $postfix = $suffix_list[$suffix];
+        $ret["suffix"] = $suffix;
+        $ret["dot-suffix"] = $postfix[0];
+        $ret["image-type"] = $postfix[1];
+        $ret["supported"] = is_array( $postfix );
+        if ( !$ret["supported"] and $use_default )
+            return eZImageFile::defaultInformation();
+        return $ret;
+    }
+
+    /*!
+      \static
+      Returns the default image info structure, which uses the jpeg format.
+      This is used for returning a standard info structure if the file format is unsupported.
+    */
+    function &defaultInformation()
+    {
+        $ret = array( "suffix" => "jpg",
+                      "dot-suffix" => ".jpg",
+                      "image-type" => "image/jpeg",
+                      "supported" => false );
+        return $ret;
+    }
     /*!
       Makes a copy of the image if and scales the copy according
       to the parameters $width and $height. If $aspectScale is true
@@ -95,41 +141,55 @@ class eZImageFile extends eZFile
     function scaleCopy( $dest, $width, $height, $aspectScale=true )
     {
         $ret = false;
-        if ( $this->isImage() )
+        $lock_file = $dest . ".lock";
+        if ( file_exists( $lock_file ) )
         {
-            $lock_file = $dest . ".lock";
-            if ( file_exists( $lock_file ) )
+            // If image file is locked we need to wait until it's finished
+            $i = 0;
+            while( file_exists( $lock_file ) and $i < 5*5 ) // Wait max 5 seconds
             {
-                // If image file is locked we need to wait until it's finished
-                $i = 0;
-                while( file_exists( $lock_file ) and $i < 5*5 ) // Wait max 5 seconds
-                {
-                    usleep( 200000 ); // Sleep 1/5 of a second
-                    clearstatcache();
-                    $i++;
-                }
-                return "locked";
+                usleep( 200000 ); // Sleep 1/5 of a second
+                clearstatcache();
+                $i++;
             }
-            touch( $lock_file );
-            $ini =& INIFile::globalINI();
-            $image_prog = "convert";
-            if ( $ini->has_var( "classes", "ImageConversionProgram" ) )
-                $image_prog = $ini->read_var( "classes", "ImageConversionProgram" );
-            $execstr = "$image_prog -geometry \"$width" . "x" . "$height" . ">\" "  . $this->TmpFileName . " " . $dest;
-
-            $err = system( $execstr, $ret_code );
-            unlink( $lock_file );
-
-            if ( $ret_code == 0 )
-            {
-                $ret = true;
-            }
-            else
-            {
-                print( "<br><b>error in scaleCopy: $err</b><br>" );
-                $ret = false;
-            }
+            return "locked";
         }
+        touch( $lock_file );
+        $ini =& INIFile::globalINI();
+        $image_prog = "convert";
+        if ( $ini->has_var( "classes", "ImageConversionProgram" ) )
+            $image_prog = $ini->read_var( "classes", "ImageConversionProgram" );
+        $execstr = "$image_prog -geometry \"$width" . "x" . "$height" . ">\" "  . $this->TmpFileName . " " . $dest;
+        print( "<br><b>$execstr</b><br>" );
+
+        $err = system( $execstr, $ret_code );
+
+        if ( $ret_code == 0 )
+        {
+            chmod( $dest, 0644 );
+            $ret = true;
+        }
+        else
+        {
+            print( "<br><b>error in scaleCopy: $err</b><br>" );
+            $ret = false;
+        }
+
+        // Check for animated gif/png
+        if ( file_exists( "$dest" . ".0" ) )
+        {
+            copy( $this->TmpFileName, $dest );
+            chmod( $dest, 0644 );
+            $i = 0;
+            while( file_exists( "$dest.$i" ) )
+            {
+                unlink( "$dest.$i" );
+                $i++;
+            }
+            $ret = true;
+        }
+
+        unlink( $lock_file );
 
         return $ret;
     }
@@ -141,22 +201,22 @@ class eZImageFile extends eZFile
     function convertCopy( $dest )
     {
         $ret = false;
-        if ( $this->isImage() )
-        {
-            $ini =& INIFile::globalINI();
-            $image_prog = "convert";
-            if ( $ini->has_var( "classes", "ImageConversionProgram" ) )
-                $image_prog = $ini->read_var( "classes", "ImageConversionProgram" );
-            $execstr = "$image_prog -quality 95 " . $this->TmpFileName . " " . $dest;
+        $ini =& INIFile::globalINI();
+        $image_prog = "convert";
+        if ( $ini->has_var( "classes", "ImageConversionProgram" ) )
+            $image_prog = $ini->read_var( "classes", "ImageConversionProgram" );
+        $execstr = "$image_prog -quality 95 " . $this->TmpFileName . " " . $dest;
+        print( "<br><b>$execstr</b><br>" );
 
-            $err = system( $execstr, $ret_code );
-            $ret = true;
-            if ( $ret_code == 0 )
-            {
-                print( "<br><b>error in convertCopy: $err</b><br>" );
-                $ret = false;
-            }
+        $err = system( $execstr, $ret_code );
+        $ret = true;
+        if ( $ret_code != 0 )
+        {
+            print( "<br><b>error in convertCopy: $err</b><br>" );
+            $ret = false;
         }
+        else
+            chmod( $dest, 0644 );
         
         return $ret;
     }
