99efeb1c6090529482c738f8f7e1b8948f3794fe
Frederik Holljen <fh@ez.no>
2001-03-08 20:31:00 +0000

    Fixed showstopper bug that causes permissions for new files to stop working!

--- contribution/versions/ezpublish2/trunk/projects/php/ezfilemanager/user/fileupload.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezfilemanager/user/fileupload.php
@@ -1,6 +1,6 @@
 <?
 // 
-// $Id: fileupload.php,v 1.22 2001/03/08 10:28:34 fh Exp $
+// $Id: fileupload.php,v 1.23 2001/03/08 20:31:00 fh Exp $
 //
 // Bård Farstad <bf@ez.no>
 // Created on: <10-Dec-2000 15:49:57 bf>
@@ -414,7 +414,7 @@ $folderList = $folder->getTree( );
 
 foreach ( $folderList as $folderItem )
 {
-    if( eZObjectPermission::hasPermission( $folderItem[0]->id(), "filemanager_file", 'w' ) )
+    if( eZObjectPermission::hasPermission( $folderItem[0]->id(), "filemanager_folder", 'w' ) )
     {
         $t->set_var( "option_name", $folderItem[0]->name() );
         $t->set_var( "option_value", $folderItem[0]->id() );
