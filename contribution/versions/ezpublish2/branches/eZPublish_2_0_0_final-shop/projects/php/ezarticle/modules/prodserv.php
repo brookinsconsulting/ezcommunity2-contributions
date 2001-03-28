2aeba5f79c679982b4911bea76e66e0143071484
eZ Systems <info@ez.no>
2001-03-28 09:51:17 +0000

    idasndosdhwod eeVS: ----------------------------------------------------------------------

---
+++ contribution/versions/ezpublish2/branches/eZPublish_2_0_0_final-shop/projects/php/ezarticle/modules/prodserv.php
@@ -0,0 +1,39 @@
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+<tr>
+	<td align="center">
+
+<table cellpadding="0" cellspacing="0" border="0">
+<tr>
+	<td class="tdmini">
+	<a href="/article/articlestatic/75/">
+	<img src="/images/ps-ui.gif" width="220" height="84" border="0" alt="" /><br />
+	</a>
+	</td>
+</tr>
+<tr>
+	<td class="tdmini">
+	<a href="/article/articlestatic/76/">
+	<img src="/images/ps-app.gif" width="220" height="84" border="0" alt="" /><br />
+	</a>
+	</td>
+</tr>
+<tr>
+	<td class="tdmini">
+	<a href="/article/articlestatic/78/">
+	<img src="/images/ps-os.gif" width="220" height="84" border="0" alt="" /><br />
+	</a>
+	</td>
+</tr>
+<tr>
+	<td class="tdmini">
+	<a href="/article/articlestatic/80/">
+	<img src="/images/ps-hw.gif" width="220" height="84" border="0" alt="" /><br />
+	</a>
+	</td>
+</tr>
+</table>
+
+	</td>
+</tr>
+</table>
+
