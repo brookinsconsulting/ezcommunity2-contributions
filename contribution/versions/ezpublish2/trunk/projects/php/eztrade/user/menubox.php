868756337a17f648bde3609ac1569d40a66288c3
eZ Systems <info@ez.no>
2001-04-18 10:59:40 +0000

    New menubox

--- contribution/versions/ezpublish2/trunk/projects/php/eztrade/user/menubox.php
+++ contribution/versions/ezpublish2/trunk/projects/php/eztrade/user/menubox.php
@@ -1,46 +1,50 @@
 <?
-// Add templates
-?>
+// 
+// $Id: menubox.php,v 1.2 2001/04/18 10:59:40 th Exp $
+//
+// Thomas Hellstrøm <th@ez.no>
+// Created on: <18-April-2001 13:00:00 th>
+//
+// This source file is part of eZ publish, publishing software.
+// Copyright (C) 1999-2001 eZ systems as
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
 
-<tr bgcolor="#C85028"> 
-	<td>
-        <p class="smallhead">
-	Shopping
-        </p>
-        </td>
-</tr>
-<tr>
-	<td>
-	<a class="menu" href="/trade/productlist/">
-	<img src="/images/prikk.gif" width="8" heigth="8" border="0">&nbsp;
-    Product list
-	</a>
-	</td>
-</tr>
-<tr>
-	<td>
-	<a class="menu" href="/trade/cart/">
-	<img src="/images/prikk.gif" width="8" heigth="8" border="0">&nbsp;
-    Your cart!
-	</a>
-	</td>
-</tr>
-<tr>
-	<td>
-	<a class="menu" href="/trade/wishlist/">
-	<img src="/images/prikk.gif" width="8" heigth="8" border="0">&nbsp;
-    Your wishlist!
-	</a>
-	</td>
-</tr>
-<tr>
-	<td>
-	<a class="menu" href="/trade/search/">
-	<img src="/images/prikk.gif" width="8" heigth="8" border="0">&nbsp;
-    Search
-	</a>
-	</td>
-</tr>
-<tr> 
-	<td width="100%"><img src="/images/1x1.gif" width="120" height="4"></td>
-</tr>
+include_once( "classes/INIFile.php" );
+
+$ini =& $GLOBALS["GlobalSiteIni"];
+
+$Language = $ini->read_var( "eZTradeMain", "Language" );
+
+    
+include_once( "classes/eztemplate.php" );
+include_once( "classes/ezdb.php" );
+include_once( "ezuser/classes/ezobjectpermission.php" );
+
+$t = new eZTemplate( "eztrade/user/" . $ini->read_var( "eZTradeMain", "TemplateDir" ),
+                     "eztrade/user/intl", $Language, "menubox.php" );
+
+$t->setAllStrings();
+
+$t->set_file( array(
+    "menu_box_tpl" => "menubox.tpl"
+    ) );
+
+$t->set_var( "sitedesign", $GlobalSiteDesign );
+
+$t->pparse( "output", "menu_box_tpl" );
+		
+?>
