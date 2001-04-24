afce04b8bfb325ae961d6daef655518b471cfe7d
Frederik Holljen <fh@ez.no>
2001-04-24 12:36:20 +0000

    Lots of changes in the user interface in ezbulkmail... added verification via mail

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b1_to_2_1_b2/2_1_b1_to_2_1_b2.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b1_to_2_1_b2/2_1_b1_to_2_1_b2.sql
@@ -1,3 +1,17 @@
 create table eZURLTranslator_URL( ID int primary key auto_increment, Source char(200), Dest char(200) );
 alter table eZURLTranslator_URL add Created timestamp;
 
+alter table eZBulkMail_SubscriptionAddress add Password varchar(50) default '' not null;
+#
+# Table structure for table 'eZBulkMail_Forgot'
+#
+DROP TABLE IF EXISTS eZBulkMail_Forgot;
+CREATE TABLE eZBulkMail_Forgot (
+  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  Mail varchar(255) DEFAULT '' NOT NULL,
+  Password varchar(50) DEFAULT '' NOT NULL,
+  Hash char(33),
+  Time timestamp(14),
+  PRIMARY KEY (ID)
+);
+
--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b1_to_2_1_b2/2_1_b1_to_2_1_b2.tpl
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b1_to_2_1_b2/2_1_b1_to_2_1_b2.tpl
@@ -4,3 +4,5 @@ have been altered.
 
 Files changed from 2.1 beta 1 to 2.1 beta 2
 -------------------------
+ezbulkmail/user/templates/standard/subscriptionlist.tpl
+ezbulkmail/user/templates/standard/subscriptionlogin.tpl (new)
