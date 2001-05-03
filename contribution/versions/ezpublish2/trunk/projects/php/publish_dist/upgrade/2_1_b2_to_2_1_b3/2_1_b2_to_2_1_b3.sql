505385e68fe69a66a686c4be4c65e4fb18707805
eZ Systems <info@ez.no>
2001-05-03 14:24:05 +0000

    Added linker stuff.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b2_to_2_1_b3/2_1_b2_to_2_1_b3.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b2_to_2_1_b3/2_1_b2_to_2_1_b3.sql
@@ -1,1 +1,7 @@
+ALTER TABLE eZTrade_Link ADD ModuleType int(11) NOT NULL;
+CREATE TABLE eZModule_LinkModuleType
+       (ID int(11) NOT NULL AUTO_INCREMENT,
+        Module varchar(40) NOT NULL,
+        Type varchar(40) NOT NULL,
+        PRIMARY KEY(ID,Module,Type));
 
