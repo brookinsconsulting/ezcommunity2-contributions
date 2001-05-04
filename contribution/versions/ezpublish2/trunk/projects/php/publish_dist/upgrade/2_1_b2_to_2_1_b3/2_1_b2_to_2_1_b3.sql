a8556932009e0d83dcab61be9e79631fd4e2eeb1
Frederik Holljen <fh@ez.no>
2001-05-04 12:00:09 +0000

    Added some stuff into the permissionts tables..

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b2_to_2_1_b3/2_1_b2_to_2_1_b3.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b2_to_2_1_b3/2_1_b2_to_2_1_b3.sql
@@ -5,3 +5,21 @@ CREATE TABLE eZModule_LinkModuleType
         Module varchar(40) NOT NULL,
         Type varchar(40) NOT NULL,
         PRIMARY KEY(ID,Module,Type));
+
+insert into eZUser_Permission SET ModuleID='1', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='2', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='3', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='4', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='5', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='6', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='7', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='8', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='9', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='10', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='11', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='12', Name='ModuleEdit';
+insert into eZUser_Permission SET ModuleID='13', Name='ModuleEdit';
+insert into eZUser_Module set Name='eZStats';
+insert into eZUser_Permission SET ModuleID='14', Name='ModuleEdit';
+insert into eZUser_Module set Name='eZSysInfo';
+insert into eZUser_Permission SET ModuleID='15', Name='ModuleEdit';
