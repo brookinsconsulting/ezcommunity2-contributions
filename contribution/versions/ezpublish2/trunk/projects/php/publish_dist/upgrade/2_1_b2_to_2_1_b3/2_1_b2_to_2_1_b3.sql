f67bf7a2abe301f46b6bcf46e518cb03e335fa9d
Frederik Holljen <fh@ez.no>
2001-05-04 08:19:02 +0000

    eZUser stuff

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b2_to_2_1_b3/2_1_b2_to_2_1_b3.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_1_b2_to_2_1_b3/2_1_b2_to_2_1_b3.sql
@@ -1,7 +1,7 @@
+alter table eZUser_Group add IsRoot int(1) default '0';
 ALTER TABLE eZTrade_Link ADD ModuleType int(11) NOT NULL;
 CREATE TABLE eZModule_LinkModuleType
        (ID int(11) NOT NULL AUTO_INCREMENT,
         Module varchar(40) NOT NULL,
         Type varchar(40) NOT NULL,
         PRIMARY KEY(ID,Module,Type));
-
