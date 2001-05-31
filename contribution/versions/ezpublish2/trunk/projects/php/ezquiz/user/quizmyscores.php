a0814612ff828af27e9974928d4cb4b56bdc2010
eZ Systems <info@ez.no>
2001-05-31 14:45:35 +0000

    Must remember to commit all files when finishing up.

---
+++ contribution/versions/ezpublish2/trunk/projects/php/ezquiz/user/quizmyscores.php
@@ -0,0 +1,190 @@
+<?
+// 
+// $Id: quizmyscores.php,v 1.1 2001/05/31 14:45:35 pkej Exp $
+//
+// Paul K Egell-Johnsen <pkej@ez.no>
+// Created on: <28-May-2001 11:24:41 pkej>
+//
+// This source file is part of eZ publish, publishing software.
+// Copyright (C) 1999-2001 eZ systems as
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
+
+// First check if this game is open
+// Check if there are other open games
+
+include_once( "classes/ezlocale.php" );
+include_once( "classes/INIFile.php" );
+include_once( "classes/ezlist.php" );
+include_once( "classes/eztemplate.php" );
+include_once( "classes/ezlocale.php" );
+include_once( "ezuser/classes/ezuser.php" );
+include_once( "ezquiz/classes/ezquizquestion.php" );
+include_once( "ezquiz/classes/ezquizgame.php" );
+include_once( "ezquiz/classes/ezquizanswer.php" );
+include_once( "ezquiz/classes/ezquizscore.php" );
+
+$ini =& INIFile::globalINI();
+
+$Limit = $ini->read_var( "eZQuizMain", "ScoreLimit" );
+$Language = $ini->read_var( "eZQuizMain", "Language" );
+$ScoreCurrent = $ini->read_var( "eZQuizMain", "ScoreCurrent" );
+
+$t = new eZTemplate( "ezquiz/user/" . $ini->read_var( "eZQuizMain", "TemplateDir" ),
+                     "ezquiz/user/intl/", $Language, "quiz.php" );
+
+$intl = new INIFIle( "ezquiz/user/intl/". $Language . "/quiz.php.ini" );
+
+$t->setAllStrings();
+
+$t->set_file( array(
+    "score_page_tpl" => "quizmyscores.tpl"
+    ) );
+
+$t->set_block( "score_page_tpl", "error_item_tpl", "error_item" );
+$t->set_block( "score_page_tpl", "logged_in_user_item_tpl", "logged_in_user_item" );
+$t->set_block( "score_page_tpl", "no_scores_item_tpl", "no_scores_item" );
+$t->set_block( "no_scores_item_tpl", "game_item_tpl", "game_item" );
+$t->set_block( "score_page_tpl", "score_list_item_tpl", "score_list_item" );
+$t->set_block( "score_list_item_tpl", "score_item_tpl", "score_item" );
+
+$t->set_var( "error_item", "" );
+$t->set_var( "no_scores_item", "" );
+$t->set_var( "logged_in_user_item", "" );
+$t->set_var( "score_list_item", "" );
+$t->set_var( "score_item", "" );
+$t->set_var( "game_item", "" );
+
+$printScores = false;
+
+$user =&  eZUser::currentUser();
+
+if( get_class( $user ) == "ezuser" )
+{
+    $UserID = $user->id();
+    
+    $t->set_var( "user_id", $UserID );
+    $t->set_var( "user_login", $user->login() );
+    $t->set_var( "user_first", $user->firstName() );
+    $t->set_var( "user_last", $user->lastName() );
+    
+    $t->parse( "logged_in_user_item", "logged_in_user_item_tpl" );
+    
+    $score = new eZQuizScore();
+    $scores = $score->getAllByUser( $user, $Offset, $Limit );
+
+    $count = count( $scores ); 
+    $scoreCount = $score->countAllByUser( $user );
+
+    $last = 0;
+    $lastColor = "bgdark";
+    $position = $Offset + 1;
+    $locale = new eZLocale( $Language );
+    if( $scoreCount > 0 )
+    {
+        $printScores = true;
+    }
+    else
+    {
+        // Is there a game?
+
+        $t->parse( "no_scores_item", "no_scores_item_tpl" );
+    }
+}
+else
+{
+    $error = "login";
+}
+
+if( $printScores == true )
+{
+    foreach( $scores as $score )
+    {
+
+        $currentScore = $score->totalScore();
+
+        if( $currentScore == $last && $position != ( $Offset + 1 ) )
+        {
+            $t->set_var( "score_position", "&nbsp;" );
+
+            if( $lastColor == "bglight" || $i = 0 )
+            {
+                $t->set_var( "td_class", "bglight" );
+            }
+            else
+            {
+                $t->set_var( "td_class", "bgdark" );
+            }
+        }
+        else
+        {
+            $t->set_var( "score_position", $position );
+            if( $lastColor == "bglight" )
+            {
+                $t->set_var( "td_class", "bgdark" );
+                $lastColor = "bgdark";
+            }
+            else
+            {
+                $t->set_var( "td_class", "bglight" );
+                $lastColor = "bglight";
+            }
+        }
+
+
+        $game = $score->game();
+
+        $t->set_var( "game_name", $game->name() );
+        $t->set_var( "game_id", $game->id() );
+        $t->set_var( "game_score", $currentScore );
+        $t->set_var( "game_questions", $game->numberOfQuestions() );
+        $t->set_var( "game_players", $game->numberOfPlayers() );
+
+        $t->parse( "score_item", "score_item_tpl", true );
+
+        $last = $currentScore;
+        $position++;
+        $i++;
+    }
+
+    $t->parse( "score_list_item", "score_list_item_tpl" );
+}
+
+eZList::drawNavigator( $t, $scoreCount, $Limit, $Offset, "score_page_tpl" );
+
+if( $error )
+{
+    switch( $error )
+    {
+        case "login":
+        {
+            $t->set_var( "error_message", $intl->read_var( "strings", "error_login" ) );
+            $t->parse( "error_item", "error_item_tpl" );
+        }
+        break;
+        
+        default:
+        {
+            $t->set_var( "error_message", $intl->read_var( "strings", "error_undefined" ) );
+            $t->parse( "error_item", "error_item_tpl" );
+        }
+        break;
+    }
+}
+
+$t->pparse( "output", "score_page_tpl" );
+
+?>
---
+++ contribution/versions/ezpublish2/trunk/projects/php/ezquiz/user/templates/standard/quizmyscores.tpl
@@ -0,0 +1,103 @@
+<!-- BEGIN logged_in_user_item_tpl -->
+<h1>{intl-my_score_list}</h1>
+
+<hr noshade="noshade" size="4" />
+
+<table width="100%" cellspacing="0" cellpadding="2" border="0">
+<tr>
+    <td>{intl-logged_in_as}</td>
+    <td>{user_login}</td>
+</tr>
+<tr>
+    <td>{intl-name}</td>
+    <td>{user_first} {user_last}</td>
+</tr>
+</table>
+
+<hr noshade="noshade" size="4" />
+<!-- END logged_in_user_item_tpl -->
+
+
+
+<!-- BEGIN error_item_tpl -->
+<h2 class="error">{intl-error}</h2>
+<hr noshade="noshade" size="4" />
+<p class="error">{error_message}</p>
+<!-- END error_item_tpl -->
+
+<!-- BEGIN score_list_item_tpl -->
+<table width="100%" cellspacing="0" cellpadding="2" border="0">
+<tr>
+	<th width="1%">{intl-game}</td>
+	<th>{intl-score}</td>
+	<th>{intl-questions}</td>
+	<th>{intl-players}</td>
+</tr>
+
+<!-- BEGIN score_item_tpl -->
+<tr class="{td_class}">
+	<td>{game_name} <!-- {game_id} --></td>
+	<td>{game_score}</td>
+	<td>{game_questions}</td>
+	<td>{game_players}</td>
+</tr>
+<!-- END score_item_tpl -->
+
+</table>
+<!-- END score_list_item_tpl -->
+
+<!-- BEGIN no_scores_item_tpl -->
+<h2>{intl-you_have_no_scores}</h2>
+{intl-you_must_play}. 
+<!-- BEGIN game_item_tpl -->
+<!-- {intl-click_on_game_in_menu} --> {intl-play_the_current}: <a href="/quiz/game/play/{game_id}/">{game_name}</a>
+<!-- END game_item_tpl -->
+<!-- END no_scores_item_tpl -->
+
+<!-- BEGIN type_list_tpl -->
+<br />
+<table cellpadding="0" cellspacing="0" border="0">
+<tr>
+	<!-- BEGIN type_list_previous_tpl -->
+	<td>
+	<a class="path" href="/quiz/game/score/{user_id}/{item_previous_index}">&lt;&lt;&nbsp;{intl-previous}</a>&nbsp;
+	</td>
+	<!-- END type_list_previous_tpl -->
+
+	<!-- BEGIN type_list_previous_inactive_tpl -->
+	<td>
+	&nbsp;
+	</td>
+	<!-- END type_list_previous_inactive_tpl -->
+
+	<!-- BEGIN type_list_item_list_tpl -->
+
+	<!-- BEGIN type_list_item_tpl -->
+	<td>
+	|&nbsp;<a class="path" href="/quiz/game/score/{user_id}/{item_index}">{type_item_name}</a>&nbsp;
+	</td>
+	<!-- END type_list_item_tpl -->
+
+	<!-- BEGIN type_list_inactive_item_tpl -->
+	<td>
+	|&nbsp;&lt;&nbsp;{type_item_name}&nbsp;&gt;&nbsp;
+	</td>
+	<!-- END type_list_inactive_item_tpl -->
+
+	<!-- END type_list_item_list_tpl -->
+
+	<!-- BEGIN type_list_next_tpl -->
+	<td>
+	|&nbsp;<a class="path" href="/quiz/game/score/{user_id}/{item_next_index}">{intl-next}&nbsp;&gt;&gt;</a>
+	</td>
+	<!-- END type_list_next_tpl -->
+
+	<!-- BEGIN type_list_next_inactive_tpl -->
+	<td>
+	|&nbsp;
+	</td>
+	<!-- END type_list_next_inactive_tpl -->
+
+</tr>
+</table>
+<!-- END type_list_tpl -->
