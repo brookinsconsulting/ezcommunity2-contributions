71f99a2b962a1a304064bddf600306d089c027dd
Bård Farstad <bf@ez.no>
2001-06-06 08:21:53 +0000

    Added site cache.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/index.php
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/index.php
@@ -55,8 +55,6 @@ include_once( "ezuser/classes/ezuser.php" );
 $session =& eZSession::globalSession();
 
 //
-// the section setting code below is obsolete and will
-// be removed in 2.1 final
 unset( $siteDesign );
 unset( $GlobalSiteDesign );
 if ( $session->fetch() == false )
@@ -72,6 +70,7 @@ else
         $siteDesign =& $ini->read_var( "site", "SiteDesign" );
     }
 }
+
 // Store the site design in a global variable
 $GlobalSiteDesign = $siteDesign;
 
@@ -135,105 +134,120 @@ if ( ( $requireUserLogin == "disabled" ) ||
             $REQUEST_URI = "/article/archive/0/";
             $url_array = explode( "/", $REQUEST_URI );
         }
-     }
-    
+    }
     
     // Load the main contents and store in a variable
     $content_page = "ez" . $url_array[1] . "/user/datasupplier.php";
 
-    $buffer =& ob_get_contents();
-    ob_end_clean();
-    ob_start();
+    // site cache check
 
-    // fetch the module printout
-    if ( file_exists( $content_page ) )
-    {
-        // the page with the real contents
-        include( $content_page );
+    $SiteCacheFile = "classes/cache/" . md5( $REQUEST_URI ) . ".php";
 
-        // The following variables can be set from the contents page:
-        // $PrintableVersion = "enabled | disabled";
-        // $GlobalSectionID = integer value, reference to the selected section.
-    }
-    else
+    if ( !file_exists( $SiteCacheFile ) )
     {
-        // the default page to load
-        if ( $ini->read_var( "site", "DefaultPage" ) != "disabled" )
+        $buffer =& ob_get_contents();
+        ob_end_clean();
+        ob_start();
+        
+        // fetch the module printout
+        if ( file_exists( $content_page ) )
         {
-            include( $ini->read_var( "site", "DefaultPage" ) );
-        }
-    }
-    
-    $MainContents =& ob_get_contents();
-    ob_end_clean();
-    
-    // fill the buffer with the old values
-    ob_start();
-    print( $buffer );
+            // the page with the real contents
+            include( $content_page );
 
-    // set the sitedesign from the section
-    if ( $ini->read_var( "site", "Sections" ) == "enabled" )
-    {
-        if ( !is_numeric( $GlobalSectionID ) )
-        {
-            $GlobalSectionID = $ini->read_var( "site", "DefaultSection" );
+            // The following variables can be set from the contents page:
+            // $PrintableVersion = "enabled | disabled";
+            // $GlobalSectionID = integer value, reference to the selected section.
         }
+        else
+        {
+            // the default page to load
+            if ( $ini->read_var( "site", "DefaultPage" ) != "disabled" )
+            {
+                include( $ini->read_var( "site", "DefaultPage" ) );
+            }
+        }
+    
+        $MainContents =& ob_get_contents();
+        ob_end_clean();
+    
+        // fill the buffer with the old values
+        ob_start();
+        print( $buffer );
+
+        // set the sitedesign from the section
+        if ( $ini->read_var( "site", "Sections" ) == "enabled" )
+        {
+            if ( !is_numeric( $GlobalSectionID ) )
+            {
+                $GlobalSectionID = $ini->read_var( "site", "DefaultSection" );
+            }
                  
-        include_once( "ezsitemanager/classes/ezsection.php" );
+            include_once( "ezsitemanager/classes/ezsection.php" );
 
-        if ( $DEBUG == true )
-        {
-            print( eZSection::siteDesign( $GlobalSectionID ) );
+            if ( $DEBUG == true )
+            {
+                print( eZSection::siteDesign( $GlobalSectionID ) );
+            }
+        
+            $siteDesign = eZSection::siteDesign( $GlobalSectionID );
+            $GlobalSiteDesign = $siteDesign;
         }
         
-        $siteDesign = eZSection::siteDesign( $GlobalSectionID );
-        $GlobalSiteDesign = $siteDesign;
-    }
+        $meta_page = "ez" . $url_array[1] . "/metasupplier.php";
         
-    $meta_page = "ez" . $url_array[1] . "/metasupplier.php";
+        // include some html
+        $Title = $ini->read_var( "site", "SiteTitle" );
+        include( "sitedesign/$siteDesign/preamble.php" );
         
-    // include some html
-    $Title = $ini->read_var( "site", "SiteTitle" );
-    include( "sitedesign/$siteDesign/preamble.php" );
+        // check if there is specific meta info, if not include the default
+        if ( file_exists( $meta_page ) )
+        {
+            include( $meta_page );
+        }
+        else
+        {
+            // Load the default meta info
+            include( "sitedesign/$siteDesign/defaultmetainfo.php" );
+        }    
 
-    // check if there is specific meta info, if not include the default
-    if ( file_exists( $meta_page ) )
-    {
-        include( $meta_page );
-    }
-    else
-    {
-        // Load the default meta info
-        include( "sitedesign/$siteDesign/defaultmetainfo.php" );
-    }
+        // include more html
+        if ( $PrintableVersion == "enabled" )
+        {
+            include( "sitedesign/$siteDesign/simpleheader.php" );
+        }
+        else
+        {
+            include( "sitedesign/$siteDesign/header.php" );    
+        }
 
+        // Main contents
+        print( $MainContents );
     
+        // and the html finish
+        // include more html
+        if ( $PrintableVersion == "enabled" )
+        {
+            include( "sitedesign/$siteDesign/simplefooter.php" );
+        }
+        else
+        {
+            include( "sitedesign/$siteDesign/footer.php" );
+        }
 
-    // include more html
-    if ( $PrintableVersion == "enabled" )
-    {
-        include( "sitedesign/$siteDesign/simpleheader.php" );
+        // store site cache
+        $fp = fopen ( $SiteCacheFile, "w+");
+
+        $SiteContents =& ob_get_contents();
+        fwrite ( $fp, $SiteContents );
+        fclose( $fp );
+        
     }
     else
     {
-        include( "sitedesign/$siteDesign/header.php" );    
+        // load site cache
+        include( $SiteCacheFile );
     }
-
-
-    // Main contents
-    print( $MainContents );
-    
-    // and the html finish
-    // include more html
-    if ( $PrintableVersion == "enabled" )
-    {
-        include( "sitedesign/$siteDesign/simplefooter.php" );
-    }
-    else
-    {
-        include( "sitedesign/$siteDesign/footer.php" );
-    }
-
 }
 else
 {
@@ -257,8 +271,9 @@ else
 eZDB::close();
 
 // Stop benchmark test and print the result.
- $bench->stop();
- $bench->printResults();
+$bench->stop();
+$bench->printResults();
 
 ob_end_flush();
+
 ?>
