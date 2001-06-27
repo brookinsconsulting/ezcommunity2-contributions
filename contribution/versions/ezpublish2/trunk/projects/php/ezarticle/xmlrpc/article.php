227db45cb8e07d9247c154c83c8ab4e7d521137f
eZ Systems <info@ez.no>
2001-06-27 13:52:48 +0000

    Fixed article data retrieval and delete. Commented out datastoring for now.

--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/xmlrpc/article.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/xmlrpc/article.php
@@ -6,97 +6,135 @@ include_once( "ezxmlrpc/classes/ezxmlrpcarray.php" );
 include_once( "ezxmlrpc/classes/ezxmlrpcbool.php" );
 include_once( "ezxmlrpc/classes/ezxmlrpcint.php" );
 
-if( $Action == "article" ) // return all the data in the category
+if( $Command == "data" ) // return all the data in the category
 {
-    $writeGroups = eZObjectPermission::getGroups( $ID, "article_article", 'w', false );
-    $readGroups = eZObjectPermission::getGroups( $ID, "article_article", 'r', false );
-    $article = new eZArticle( $ID );
-    
-    $ReturnData = new eZXMLRPCStruct( array( "Location" => createURLStruct( "ezarticle", "article", $article->id() ),
-                                             "AuthorID" => new eZXMLRPCInt( $article->author( false ) ),
-                                             "Name" => new eZXMLRPCString( $article->name( false ) ), // title
-                                             "Contents" => new eZXMLRPCString( $article->contents( false ) ),
-                                             "ContentsWriterID" => new eZXMLRPCString( $article->contentsWriter( true ) ),
-                                             "LinkText" => new eZXMLRPCString( $article->linkText( false ) ),
-                                             "ManualKeyWords" => new eZXMLRPCString( $article->manualKeywords() ),
-                                             "Discuss" => new eZXMLRPCBool( $article->discuss() ),
-                                             "IsPublished" => new eZXMLRPCBool( $article->isPublished() ),
-                                             "PageCount" => new eZXMLRPCInt( $article->pageCount() ),
-                                             "Images" => new eZXMLRPCArray( $imageList = $article->images( false ) ),
-                                             "ReadGroups" => new eZXMLRPCArray( $readGroups ),
-                                             "WriteGroups" => new eZXMLRPCArray( $writeGroups )
-//                                             "StartDate" => new eZXMLRPCStruct(),
-//                                             "StopDate" => new eZXMLRPCStruct(),
-//                                             "PublishedDate" => new eZXMLRPCStruct(),
-                                             )
-                                      );
+    $article = new eZArticle();
+    if ( !$article->get( $ID ) )
+    {
+        $Error = createErrorMessage( EZERROR_NONEXISTING_OBJECT );
+    }
+    else
+    {
+        $writeGroups = eZObjectPermission::getGroups( $ID, "article_article", 'w', false );
+        $readGroups = eZObjectPermission::getGroups( $ID, "article_article", 'r', false );
+        $contentsWriter =& $article->contentsWriter( true );
 
+        $type_arr = array();
+        $types =& $article->types();
+        foreach( $types as $type )
+        {
+            $attributes =& $type->attributes();
+            if ( count( $attributes ) > 0 )
+            {
+                $attr_arr = array();
+                foreach( $attributes as $attrib )
+                {
+                    $attr_arr[] = new eZXMLRPCStruct( array( "ID" => new eZXMLRPCInt( $attrib->id() ),
+                                                             "Name" => new eZXMLRPCString( $attrib->name() ),
+                                                             "Content" => new eZXMLRPCString( $attrib->value( $article ) ) ) );
+                }
+                $type_arr[] = new eZXMLRPCStruct( array( "ID" => new eZXMLRPCInt( $type->id() ),
+                                                         "Name" => new eZXMLRPCString( $type->name() ),
+                                                         "Attributes" => new eZXMLRPCArray( $attr_arr ) ) );
+            }
+        }
+
+        $ret = array( "Location" => createURLStruct( "ezarticle", "article", $article->id() ),
+                      "AuthorID" => new eZXMLRPCInt( $article->author( false ) ),
+                      "Name" => new eZXMLRPCString( $article->name( false ) ), // title
+                      "Contents" => new eZXMLRPCString( $article->contents( false ) ),
+                      "ContentsWriterID" => new eZXMLRPCInt( $contentsWriter->id() ),
+                      "LinkText" => new eZXMLRPCString( $article->linkText( false ) ),
+                      "ManualKeyWords" => new eZXMLRPCString( $article->manualKeywords() ),
+                      "Discuss" => new eZXMLRPCBool( $article->discuss() ),
+                      "IsPublished" => new eZXMLRPCBool( $article->isPublished() ),
+                      "PageCount" => new eZXMLRPCInt( $article->pageCount() ),
+                      "Images" => new eZXMLRPCArray( $article->images( false ), "integer" ),
+                      "Files" => new eZXMLRPCArray( $article->files( false ), "integer" ),
+                      "ReadGroups" => new eZXMLRPCArray( $readGroups, "integer" ),
+                      "WriteGroups" => new eZXMLRPCArray( $writeGroups, "integer" ),
+                      "Types" => new eZXMLRPCArray( $type_arr ),
+                      "Topic" => new eZXMLRPCInt( $article->topic( false ) )
+//                                             "PublishedDate" => new eZXMLRPCStruct(),
+                      );
+        $start_date = $article->startDate();
+        if ( $start_date->isValid() )
+            $ret["StartDate"] = createDateTimeStruct( $start_date );
+        $stop_date =& $article->stopDate();
+        if ( $stop_date->isValid() )
+            $ret["StopDate"] = createDateTimeStruct( $stop_date );
+        $published =& $article->published();
+        if ( $published->isValid() )
+            $ret["PublishDate"] = createDateTimeStruct( $published );
+        $ReturnData = new eZXMLRPCStruct( $ret );
+    }
 }
 // TODO, storearticle needs work..
-else if( $Action == "storearticle" )
-{
-    $ID = $Data["ID"]->value();
-    if( $ID == 0 )
-        $article = new eZArticle();
-    else
-        $article = new eZArticle( $ID );
+//  else if( $Command == "storedata" )
+//  {
+//      $ID = $Data["ID"]->value();
+//      if( $ID == 0 )
+//          $article = new eZArticle();
+//      else
+//          $article = new eZArticle( $ID );
 
-    $article->setAuthor( $Data["AuthorID"]->value() );
-    $article->setName( $Data["Name"]->value() ); // title
-    $article->setContents( $Data["Contents"]->value() );
-    $article->setLinkText( $Data["LinkText"]->value() );
-    $article->setManualKeywords( $Data["ManualKeywords"] );
-    $article->setDiscuss( $Data["Discuss"] );
-    $article->setIsPublished( $Data["IsPublished"] );
-    $article->setPageCount( $Data["PageCount"] );
-    $article->store();
-    $ID = $article->id();
+//      $article->setAuthor( $Data["AuthorID"]->value() );
+//      $article->setName( $Data["Name"]->value() ); // title
+//      $article->setContents( $Data["Contents"]->value() );
+//      $article->setLinkText( $Data["LinkText"]->value() );
+//      $article->setManualKeywords( $Data["ManualKeywords"] );
+//      $article->setDiscuss( $Data["Discuss"] );
+//      $article->setIsPublished( $Data["IsPublished"] );
+//      $article->setPageCount( $Data["PageCount"] );
+//      $article->store();
+//      $ID = $article->id();
 
-    // images
-    $images = $Data["Images"]->value();
-    foreach( $images as $image )
-        $article->addImage( $image );
-
-
-    // permissions....
-    eZObjectPermission::removePermissions( $ID, "article_article", 'r' );
-    $readGroups = $Data["ReadGroups"]->value();
-    foreach( $readGroups as $readGroup )
-        eZObjectPermission::setPermission( $readGroup->value(), $ID, "article_article", 'r' );
+//      // images
+//      $images = $Data["Images"]->value();
+//      foreach( $images as $image )
+//          $article->addImage( $image );
 
 
-    eZObjectPermission::removePermissions( $ID, "article_article", 'w' );
-    foreach( $Data["WriteGroups"]->value() as $writeGroup )
-        eZObjectPermission::setPermission( $writeGroup->value(), $ID, "article_article", 'w' );
+//      // permissions....
+//      eZObjectPermission::removePermissions( $ID, "article_article", 'r' );
+//      $readGroups = $Data["ReadGroups"]->value();
+//      foreach( $readGroups as $readGroup )
+//          eZObjectPermission::setPermission( $readGroup->value(), $ID, "article_article", 'r' );
+
+
+//      eZObjectPermission::removePermissions( $ID, "article_article", 'w' );
+//      foreach( $Data["WriteGroups"]->value() as $writeGroup )
+//          eZObjectPermission::setPermission( $writeGroup->value(), $ID, "article_article", 'w' );
     
-    $category = eZArticleCategory::categoryDefinitionStatic( $ID )
-    $path =& $category->path();
-    if ( $category->id() != 0 )
-    {
-        $par[] = createURLStruct( "ezarticle", "category", 0 );
-    }
-    else
-    {
-        $par[] = createURLStruct( "ezarticle", "" );
-    }
-    foreach( $path as $item )
-    {
-        if ( $item[0] != $category->id() )
-            $par[] = createURLStruct( "ezarticle", "category", $item[0] );
-    }
+//      $category = eZArticleCategory::categoryDefinitionStatic( $ID )
+//      $path =& $category->path();
+//      if ( $category->id() != 0 )
+//      {
+//          $par[] = createURLStruct( "ezarticle", "category", 0 );
+//      }
+//      else
+//      {
+//          $par[] = createURLStruct( "ezarticle", "" );
+//      }
+//      foreach( $path as $item )
+//      {
+//          if ( $item[0] != $category->id() )
+//              $par[] = createURLStruct( "ezarticle", "category", $item[0] );
+//      }
 
     
-    $ReturnData = new eZXMLRPCStruct( array( "Location" => createURLStruct( "ezarticle", "article", $ID ),
-                                             "Path" => new eZXMLRPCArray( $par ),
-                                             "UpdateType" => new eZXMLRPCString( $Command )
-                                             )
-                                      );
-    $Command = "update";
+//      $ReturnData = new eZXMLRPCStruct( array( "Location" => createURLStruct( "ezarticle", "article", $ID ),
+//                                               "Path" => new eZXMLRPCArray( $par ),
+//                                               "UpdateType" => new eZXMLRPCString( $Command )
+//                                               )
+//                                        );
+//      $Command = "update";
 
-}
+//  }
 else if( $Command == "delete" )
 {
-    $category = eZArticleCategory::categoryDefinitionStatic( $ID )
+    $category = eZArticle::categoryDefinitionStatic( $ID );
+    $category = new eZArticleCategory( $category );
     $path =& $category->path();
     if ( $category->id() != 0 )
     {
@@ -112,14 +150,13 @@ else if( $Command == "delete" )
             $par[] = createURLStruct( "ezarticle", "category", $item[0] );
     }
 
-    
     $ReturnData = new eZXMLRPCStruct( array( "Location" => createURLStruct( "ezarticle", "article", $ID ),
                                              "Path" => new eZXMLRPCArray( $par ),
                                              "UpdateType" => new eZXMLRPCString( $Command )
                                              )
                                       );
     $Command = "update";
-    
+
     $article = new eZArticle( $ID );
     $article->delete();
 }
