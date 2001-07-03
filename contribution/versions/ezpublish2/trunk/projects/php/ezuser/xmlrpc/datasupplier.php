b8804bf1e0e9f2c911954562b4695788af0fcef1
eZ Systems <info@ez.no>
2001-07-03 15:18:47 +0000

    Minor fix.

--- contribution/versions/ezpublish2/trunk/projects/php/ezuser/xmlrpc/datasupplier.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezuser/xmlrpc/datasupplier.php
@@ -1,32 +1,24 @@
 <?
-if ( $ReturnCatalogues == true )
+switch ( $RequestType )
 {
-//    $Catalogues[] = new eZXMLRPCString( "users" );
-//    $Catalogues[] = new eZXMLRPCString( "groups" );
-}
-else
-{
-    switch ( $RequestType )
+    case "user" :
     {
-        case "user" :
-        {
-            include( "ezuser/xmlrpc/user.php" );
-        } break;
+        include( "ezuser/xmlrpc/user.php" );
+    } break;
 
-        case "author" :
-        {
-            include( "ezuser/xmlrpc/author.php" );
-        } break;
+    case "author" :
+    {
+        include( "ezuser/xmlrpc/author.php" );
+    } break;
         
-        case "group" :
-        {
-            include( "ezuser/xmlrpc/group.php" );
-        } break;
+    case "group" :
+    {
+        include( "ezuser/xmlrpc/group.php" );
+    } break;
 
-        default :
-        {
-            $Error = true;
-        } break;
-    }
+    default :
+    {
+        $Error = true;
+    } break;
 }
 ?>
