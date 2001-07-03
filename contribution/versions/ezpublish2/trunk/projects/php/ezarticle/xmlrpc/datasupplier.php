21781ec96670e93820b82f1d3f7d117bf5f984f1
eZ Systems <info@ez.no>
2001-07-03 15:03:23 +0000

    *** empty log message ***

--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/xmlrpc/datasupplier.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/xmlrpc/datasupplier.php
@@ -1,89 +1,82 @@
 <?
-if ( $ReturnCatalogues == true )
+switch ( $RequestType )
 {
-    $Catalogues[] = new eZXMLRPCString( "categorylist" );
-}
-else
-{
-    switch ( $RequestType )
+    case "type" :
     {
-        case "type" :
+        switch( $Command )
         {
-            switch( $Command )
+            case "list":
             {
-                case "list":
-                {
-                    include( "ezarticle/xmlrpc/typelist.php" );
-                    break;
-                }
-                case "data":
-                case "storedata":
-                case "delete":
-                {
-                    include( "ezarticle/xmlrpc/type.php" );
-                    break;
-                }
+                include( "ezarticle/xmlrpc/typelist.php" );
+                break;
             }
-        } break;
-        case "topic" :
+            case "data":
+            case "storedata":
+            case "delete":
+            {
+                include( "ezarticle/xmlrpc/type.php" );
+                break;
+            }
+        }
+    } break;
+    case "topic" :
+    {
+        switch( $Command )
         {
-            switch( $Command )
+            case "list":
             {
-                case "list":
-                {
-                    include( "ezarticle/xmlrpc/topiclist.php" );
-                    break;
-                }
-                case "data":
-                case "storedata":
-                {
-                    include( "ezarticle/xmlrpc/topic.php" );
-                    break;
-                }
+                include( "ezarticle/xmlrpc/topiclist.php" );
+                break;
             }
-        } break;
+            case "data":
+            case "storedata":
+            {
+                include( "ezarticle/xmlrpc/topic.php" );
+                break;
+            }
+        }
+    } break;
 
-        case "category" :
+    case "category" :
+    {
+        switch( $Command )
         {
-            switch( $Command )
+            case "list":
             {
-                case "list":
-                {
-                    include( "ezarticle/xmlrpc/categorylist.php" );
-                    break;
-                }
-                case "data":
-                case "storedata":
-                case "delete":
-                {
-                    include( "ezarticle/xmlrpc/category.php" );
-                    break;
-                }
-                default:
-                    $Error = true;
+                include( "ezarticle/xmlrpc/categorylist.php" );
+                break;
             }
-        } break;
-        case "article" :
+            case "data":
+            case "storedata":
+            case "delete":
+            {
+                include( "ezarticle/xmlrpc/category.php" );
+                break;
+            }
+            default:
+                $Error = true;
+        }
+    } break;
+    case "article" :
+    {
+        switch( $Command )
         {
-            switch( $Command )
+            case "data":
+            case "storedata":
+            case "delete":
             {
-                case "data":
-                case "storedata":
-                case "delete":
-                {
-                    include( "ezarticle/xmlrpc/article.php" );
-                    break;
-                }
-                default:
-                    $Error = true;
+                include( "ezarticle/xmlrpc/article.php" );
+                break;
             }
-        } break;
+            default:
+                $Error = true;
+        }
+    } break;
         
-        default :
-        {
-            $Error = true;
-        } break;
-    }
+    default :
+    {
+        $Error = true;
+    } break;
 }
 
 ?>
--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/xmlrpc/type.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/xmlrpc/type.php
@@ -4,11 +4,15 @@ include_once( "ezarticle/classes/ezarticleattribute.php" );
 
 if( $Command == "data" ) // return all the data in the category
 {
+    eZLog::writeNotice( "type: $Module/$RequestType/$ID#1" );
     $type = new eZArticleType();
+    eZLog::writeNotice( "type: $Module/$RequestType/$ID#2" );
     if ( $type->get( $ID ) )
     {
+    eZLog::writeNotice( "type: $Module/$RequestType/$ID#3" );
         $attrs = $type->attributes();
         $attr_arr = array();
+    eZLog::writeNotice( "type: $Module/$RequestType/$ID#4" );
         foreach( $attrs as $attr )
         {
             $attr_arr[] = new eZXMLRPCStruct( array( "ID" => new eZXMLRPCInt( $attr->id() ),
@@ -16,9 +20,11 @@ if( $Command == "data" ) // return all the data in the category
         }
         $ReturnData = new eZXMLRPCStruct( array( "Name" => new eZXMLRPCString( $type->name() ),
                                                  "Attributes" => new eZXMLRPCArray( $attr_arr ) ) );
+    eZLog::writeNotice( "type: $Module/$RequestType/$ID#5" );
     }
     else
     {
+    eZLog::writeNotice( "type: $Module/$RequestType/$ID#2.2" );
         $Error = createErrorMessage( EZERROR_NONEXISTING_OBJECT );
     }
 }
