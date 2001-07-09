0e5562b167df4946f8494f2930ffa6b94c5350f4
Bård Farstad <bf@ez.no>
2001-07-09 05:24:49 +0000

    Added informix db schemas.

--- contribution/versions/ezpublish2/trunk/projects/php/ezstats/sql/informix/ezstats.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/ezstats/sql/informix/ezstats.sql
@@ -36,3 +36,58 @@ CREATE TABLE eZStats_RequestPage (
   URI varchar(250) default NULL,
   PRIMARY KEY (ID)
 );
+
+
+CREATE TABLE eZStats_Archive_RequestedPage (
+  ID int NOT NULL,
+  Month int,
+  URI varchar(250) default NULL,
+  Count int NOT NULL ,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZStats_Archive_PageView (
+  ID int NOT NULL,
+  Hour int NOT NULL,
+  Count int  NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZStats_Archive_UniqueVisits (
+  ID int NOT NULL,
+  Day int NOT NULL,
+  Count int NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZStats_Archive_BrowserType (
+  ID int NOT NULL,
+  Browser varchar(250) default NULL,
+  Count int NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZStats_Archive_RefererURL (
+  ID int NOT NULL,
+  Month int NOT NULL,
+  Domain varchar(100) default NULL,
+  URI varchar(200) default NULL,
+  Count int  NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZStats_Archive_Users (
+  ID int NOT NULL,
+  UserID int NOT NULL,
+  Month int NOT NULL,
+  Count int  NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZStats_Archive_RemoteHost (
+  ID int NOT NULL,
+  IP varchar(15) default NULL,
+  HostName varchar(150) default NULL,
+  Count int NOT NULL,
+  PRIMARY KEY (ID)
+);
