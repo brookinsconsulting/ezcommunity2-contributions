aac9cca0312800579f441df5a4a79a8e6037b382
eZ Systems <info@ez.no>
2001-07-10 13:25:02 +0000

    Implemented storing of articles. Added forms to article data retrieve.

--- contribution/versions/ezpublish2/trunk/projects/php/ezarticle/xmlrpc/article.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezarticle/xmlrpc/article.php
@@ -2,6 +2,7 @@
 include_once( "ezarticle/classes/ezarticlecategory.php" );
 include_once( "ezarticle/classes/ezarticleattribute.php" );
 include_once( "ezarticle/classes/ezarticle.php" );
+include_once( "ezform/classes/ezform.php" );
 include_once( "ezuser/classes/ezobjectpermission.php" );
 include_once( "ezxmlrpc/classes/ezxmlrpcarray.php" );
 include_once( "ezxmlrpc/classes/ezxmlrpcbool.php" );
@@ -53,6 +54,7 @@ if( $Command == "data" ) // return all the data in the category
                       "Thumbnail" => new eZXMLRPCInt( $article->thumbnailImage( false ) ),
                       "Images" => new eZXMLRPCArray( $article->images( false ), "integer" ),
                       "Files" => new eZXMLRPCArray( $article->files( false ), "integer" ),
+                      "Forms" => new eZXMLRPCArray( $article->forms( false ), "integer" ),
                       "ReadGroups" => new eZXMLRPCArray( $readGroups, "integer" ),
                       "WriteGroups" => new eZXMLRPCArray( $writeGroups, "integer" ),
                       "Types" => new eZXMLRPCArray( $type_arr ),
@@ -73,84 +75,68 @@ if( $Command == "data" ) // return all the data in the category
 }
 else if( $Command == "storedata" )
 {
-// TODO, storearticle needs work..
-    eZLog::writeNotice( "article: #1" );
-//    $ID = $Data["ID"]->value();
-    if( $ID == 0 )
-        $article = new eZArticle();
-    else
-        $article = new eZArticle( $ID );
-    eZLog::writeNotice( "article: #2" );
+    $article = new eZArticle();
+    if( $ID != 0 )
+        $article->get( $ID );
 
     $article->setAuthor( $Data["AuthorID"]->value() );
-    eZLog::writeNotice( "article: #2.1" );
     $article->setName( $Data["Name"]->value() ); // title
-    eZLog::writeNotice( "article: #2.2" );
     $article->setContents( $Data["Contents"]->value() );
-    eZLog::writeNotice( "article: #2.3" );
     $article->setContentsWriter( $Data["ContentsWriterID"]->value() );
-    eZLog::writeNotice( "article: #2.4" );
     $article->setLinkText( $Data["LinkText"]->value() );
-    eZLog::writeNotice( "article: #2.5" );
     $article->setManualKeywords( $Data["ManualKeyWords"]->value() );
-    eZLog::writeNotice( "article: #2.6" );
     $article->setDiscuss( $Data["Discuss"]->value() );
-    eZLog::writeNotice( "article: #2.7" );
-    eZLog::writeNotice( "article: " . $Data["IsPublished"] );
-    eZLog::writeNotice( "article: " . $Data["IsPublished"]->value() ? "true" : "false" );
     $article->setIsPublished( $Data["IsPublished"]->value() );
-//      eZLog::writeNotice( "article: #2.8" );
-//      $article->setPageCount( $Data["PageCount"]->value() );
-    eZLog::writeNotice( "article: #2.9" );
     $thumbImage = new eZImage( $Data["Thumbnail"]->value() );
-    eZLog::writeNotice( "article: #2.10" );
     $article->setThumbnailImage( $thumbImage );
-    eZLog::writeNotice( "article: #2.11" );
     $article->setTopic( $Data["Topic"]->value() );
 
-    eZLog::writeNotice( "article: #2.11.2" );
     if ( isset( $Data["StartDate"] ) )
     {
-        eZLog::writeNotice( "article: #2.11.3" );
         $startDate = createDateTime( $Data["StartDate"]->value() );
         $article->setStartDate( $startDate );
     }
     if ( isset( $Data["StopDate"] ) )
     {
-        eZLog::writeNotice( "article: #2.11.4" );
         $stopDate = createDateTime( $Data["StopDate"]->value() );
-        ob_start();
-        print_r( $stopDate );
-        eZLog::writeNotice( "article: #2.11.4: " . ob_get_contents() );
-        ob_end_flush();
         $article->setStopDate( $stopDate );
     }
 
-    eZLog::writeNotice( "article: #2.12" );
     $article->store();
     $ID = $article->id();
-    eZLog::writeNotice( "article: #3" );
 
-//      // images
-//      $images = $article->images( false );
-//      $new_images = $Data["Images"]->value();
-//      $old_images = array_diff( $images, $new_images );
-//      $added_images = array_diff( $new_images, $images );
-//      $changed_images = array_intersect( $new_images, $images );
-//      foreach( $old_images as $image )
-//          $article->deleteImage( $image );
-//      foreach( $added_images as $image )
-//          $article->addImage( $image );
+    // images
+    $images = $Data["Images"]->value();
+    $new_images = array();
+    foreach( $images as $img )
+    {
+        $new_images[] = $img->value();
+    }
+    $images = $article->images( false );
+    $old_images = array_diff( $images, $new_images );
+    $added_images = array_diff( $new_images, $images );
+    $changed_images = array_intersect( $new_images, $images );
 
-//      $files =& $article->files( false );
-//      $new_files = $Data["Files"]->value();
-//      $old_files = array_diff( $files, $new_files );
-//      $added_files = array_diff( $new_files, $files );
-//      $changed_files = array_intersect( $new_files, $files );
-//      foreach( $old_files as $file )
-//          $article->deleteFile( $file );
-//      foreach( $added_files as $file )
-//          $article->addFile( $file );
+    foreach( $old_images as $image )
+        $article->deleteImage( $image );
+    foreach( $added_images as $image )
+        $article->addImage( $image );
+
+    // files
+    $files = $Data["Files"]->value();
+    $new_files = array();
+    foreach( $files as $fl )
+    {
+        $new_files[] = $fl->value();
+    }
+    $files =& $article->files( false );
+    $old_files = array_diff( $files, $new_files );
+    $added_files = array_diff( $new_files, $files );
+    $changed_files = array_intersect( $new_files, $files );
+    foreach( $old_files as $file )
+        $article->deleteFile( $file );
+    foreach( $added_files as $file )
+        $article->addFile( $file );
 
     // permissions....
     eZObjectPermission::removePermissions( $ID, "article_article", 'r' );
@@ -163,33 +149,61 @@ else if( $Command == "storedata" )
     foreach( $writeGroups as $writeGroup )
         eZObjectPermission::setPermission( $writeGroup->value(), $ID, "article_article", 'w' );
 
-    eZLog::writeNotice( "article: #4" );
+    // types
+    $types = $Data["Types"]->value();
+    $old_types = $article->types( false );
+    $new_types = array();
+    foreach ( $types as $type )
+    {
+        $type = $type->value();
+        $typeID = $type["ID"]->value();
+        $new_types[] = $typeID;
+        $attrs = $type["Attributes"]->value();
+        $attr_map = array();
+        foreach( $attrs as $attr )
+        {
+            $attr = $attr->value();
+            $id = $attr["ID"]->value();
+            $name = $attr["Name"]->value();
+            $content = $attr["Content"]->value();
+            $attr_map[$id] = array( "ID" => $id,
+                                    "Name" => $name,
+                                    "Content" => $content );
+        }
+        $articleType = new eZArticleType( $typeID );
+        $attributes = $articleType->attributes();
 
-//      $types = $Data["Types"]->value();
-//      foreach ( $types as $type )
-//      {
-//          $typeID = $type["ID"]->value();
-//          $attrs = $type["Attributes"]->value();
-//          $articleType = new eZArticleType( $type );
-//          $attributes = $articleType->attributes( false );
+        foreach( $attributes as $attribute )
+        {
+            $id = $attribute->id();
+            if ( isset( $attr_map[$id] ) )
+            {
+                $attribute->setName( $attr_map[$id]["Name"] );
+                $attribute->store();
+                $attribute->setValue( $article, $attr_map[$id]["Content"] );
+            }
+        }
+    }
+    $removed_types = array_diff( $old_types, $new_types );
+    foreach( $removed_types as $typeID )
+    {
+        $type = new eZArticleType();
+        if ( $type->get( $typeID ) )
+            $type->delete();
+    }
 
-//          $attrArray = array();
-//          foreach( $attrs as $attr )
-//          {
-//              $attrArray[] = $attr["ID"]->value();
-//          }
-//          $old_attrs = array_diff( $attributes, $attrArray );
-        
+    // forms
+    $article->deleteForms();
+    $forms = $Data["Forms"]->value();
+    foreach( $forms as $form )
+    {
+        $form = new eZForm( $form->value() );
+        $article->addForm( $form );
+    }
 
-//          {
-//              $attrib->setValue( $ID, htmlspecialchars( "" ) );
-//          }
-//      }
-
+    // categories
     $category = new eZArticleCategory( eZArticle::categoryDefinitionStatic( $ID ) );
-    eZLog::writeNotice( "article: #4.2" );
     $path =& $category->path();
-    eZLog::writeNotice( "article: #4.3" );
     if ( $category->id() != 0 )
     {
         $par[] = createURLStruct( "ezarticle", "category", 0 );
@@ -198,14 +212,12 @@ else if( $Command == "storedata" )
     {
         $par[] = createURLStruct( "ezarticle", "" );
     }
-    eZLog::writeNotice( "article: #4.4" );
     foreach( $path as $item )
     {
         if ( $item[0] != $category->id() )
             $par[] = createURLStruct( "ezarticle", "category", $item[0] );
     }
 
-    eZLog::writeNotice( "article: #5" );
 //      eZArticleTool::deleteCache( $ID, $CategoryID, $CategoryArray );
 
     $ReturnData = new eZXMLRPCStruct( array( "Location" => createURLStruct( "ezarticle", "article", $ID ),
@@ -214,7 +226,6 @@ else if( $Command == "storedata" )
                                              )
                                       );
     $Command = "update";
-    eZLog::writeNotice( "article: #6" );
 
 }
 else if( $Command == "delete" )
