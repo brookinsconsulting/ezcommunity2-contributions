b57ad8c7c6a023024bcf64197d0328f0d4712312
eZ Systems <info@ez.no>
2001-07-20 12:04:13 +0000

    changed headers in main scripts

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/index_verify.php
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/index_verify.php
@@ -1,4 +1,28 @@
-<?
+<?php
+// 
+// $Id: index_verify.php,v 1.2 2001/07/20 12:04:13 jakobn Exp $
+//
+// Created on: <09-Nov-2000 14:52:40 ce>
+//
+// This source file is part of eZ publish, publishing software.
+//
+// Copyright (C) 1999-2001 eZ Systems.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
+
 ob_end_clean();
 // script to check if the site is alive
 // this script will return 42 if the server is alive
