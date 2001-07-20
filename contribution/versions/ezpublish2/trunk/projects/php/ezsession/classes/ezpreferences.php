342378050b55c9b60ce8d49726dbcdd8906aa2fe
eZ Systems <info@ez.no>
2001-07-20 11:25:45 +0000

    changed headers for ezsession

--- contribution/versions/ezpublish2/trunk/projects/php/ezsession/classes/ezpreferences.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezsession/classes/ezpreferences.php
@@ -1,14 +1,14 @@
 <?php
 // 
-// $Id: ezpreferences.php,v 1.12 2001/07/13 14:48:19 jhe Exp $
+// $Id: ezpreferences.php,v 1.13 2001/07/20 11:25:45 jakobn Exp $
 //
 // Definition of eZPreferences class
 //
-// Bård Farstad <bf@ez.no>
 // Created on: <18-Jan-2001 13:05:06 bf>
 //
 // This source file is part of eZ publish, publishing software.
-// Copyright (C) 1999-2001 eZ systems as
+//
+// Copyright (C) 1999-2001 eZ Systems.  All rights reserved.
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
--- contribution/versions/ezpublish2/trunk/projects/php/ezsession/classes/ezsession.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezsession/classes/ezsession.php
@@ -1,14 +1,14 @@
 <?php
 // 
-// $Id: ezsession.php,v 1.55 2001/07/16 13:50:30 bf Exp $
+// $Id: ezsession.php,v 1.56 2001/07/20 11:25:45 jakobn Exp $
 //
 // Definition of eZSession class
 //
-// Bård Farstad <bf@ez.no>
 // Created on: <25-Sep-2000 15:21:18 bf>
 //
 // This source file is part of eZ publish, publishing software.
-// Copyright (C) 1999-2001 eZ systems as
+//
+// Copyright (C) 1999-2001 eZ Systems.  All rights reserved.
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
