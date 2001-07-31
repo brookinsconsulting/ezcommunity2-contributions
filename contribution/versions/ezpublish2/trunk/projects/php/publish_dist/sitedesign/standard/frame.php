3a15ad19fb440204c989bc5d4ca62528db30b98a
eZ Systems <info@ez.no>
2001-07-31 15:25:56 +0000

    Added $GlobalSiteDesign so path was dynamic -cm

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/standard/frame.php
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/standard/frame.php
@@ -7,7 +7,7 @@
 
 $SiteTitle = $ini->read_var( "site", "SiteTitle" );
 
-if ( isset( $SiteTitleAppend ) )    
+if ( isset( $SiteTitleAppend ) )
     print( $SiteTitle . " - " . $SiteTitleAppend );
 else
     print( $SiteTitle );
@@ -25,39 +25,43 @@ if ( isset( $MetaRedirectLocation ) && isset( $MetaRedirectTimer ) )
 
 ?>
 
-<link rel="stylesheet" type="text/css" href="<? print $wwwDir; ?>/sitedesign/standard/style.css" />
+<<<<<<< frame.php
+<link rel="stylesheet" type="text/css" href="/sitedesign/<? print ($GlobalSiteDesign); ?>/style.css" />
+=======
+<link rel="stylesheet" type="text/css" href="<? print $wwwDir; ?>/sitedesign/<? print ($GlobalSiteDesign); ?>/style.css" />
+>>>>>>> 1.2
 
 <script language="JavaScript1.2">
 <!--//
 
-	function MM_swapImgRestore() 
-	{
-		var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-	}
+   function MM_swapImgRestore()
+   {
+      var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+   }
 
-	function MM_preloadImages() 
-	{
-		var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-		var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-		if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-	}
+   function MM_preloadImages()
+   {
+      var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+      var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+      if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+   }
 
-	function MM_findObj(n, d) 
-	{
-		var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-		d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-		if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-		for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document); return x;
-	}
+   function MM_findObj(n, d)
+   {
+      var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+      d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+      if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+      for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document); return x;
+   }
 
-	function MM_swapImage() 
-	{
-		var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-		if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-	}
-	
+   function MM_swapImage()
+   {
+      var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+      if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+   }
+
 //-->
-</script> 
+</script>
 
 <meta name="author" content="eZ systems" />
 <meta name="copyright" content="eZ systems &copy; 2001" />
@@ -83,86 +87,105 @@ else
 
 <body bgcolor="#b5b5b5" topmargin="6" marginheight="6" leftmargin="6" marginwidth="6"  onload="MM_preloadImages('<? print $wwwDir; ?>/images/redigerminimrk.gif','<? print $wwwDir; ?>/images/slettminimrk.gif','<? print $wwwDir; ?>/images/downloadminimrk.gif','<? print $wwwDir; ?>/images/addminimrk.gif')">
 
-<img src="<? print $wwwDir; ?>/sitedesign/standard/images/ezpublish-standard.gif" height="40" width="610" border="0" alt="" />
+<<<<<<< frame.php
+<img src="/sitedesign/<? print ($GlobalSiteDesign); ?>/images/ezpublish-standard.gif" height="40" width="610" border="0" alt="" />
+=======
+<img src="<? print $wwwDir; ?>/sitedesign/<? print ($GlobalSiteDesign); ?>/images/ezpublish-standard.gif" height="40" width="610" border="0" alt="" />
+>>>>>>> 1.2
 
 
 <table width="100%" border="0" cellspacing="0" cellpadding="4">
 <tr valign="top">
     <td width="1%" bgcolor="#f0f0f0">
-	<!-- Left menu start -->
+   <!-- Left menu start -->
 
-	<?
+   <?
     $CategoryID = 0;
-	include( "ezarticle/user/menubox.php" );
-	?>
-   
-	<?
-//	include( "eznewsfeed/user/menubox.php" );
-	?>      
+   include( "ezarticle/user/menubox.php" );
+   ?>
 
-	<?
+   <?
+// include( "eznewsfeed/user/menubox.php" );
+   ?>
+
+   <?
     $CategoryID = 1;
-//	include( "eznewsfeed/user/headlines.php" );
-	?>      
+// include( "eznewsfeed/user/headlines.php" );
+   ?>
 
-	<?
-//	include( "ezmail/user/menubox.php" );
-	?>
-	
-	<?
-	include( "ezforum/user/menubox.php" );
+   <?
+// include( "ezmail/user/menubox.php" );
+   ?>
+
+   <?
+   include( "ezforum/user/menubox.php" );
 
     include( "ezforum/user/latestmessages.php" );
-	?>
-	
-	<?
-	include( "ezlink/user/menubox.php" );
-	?>
+   ?>
+
+   <?
+   include( "ezlink/user/menubox.php" );
+   ?>
 
     <?
-	include( "ezfilemanager/user/menubox.php" );
-	?>
+   include( "ezfilemanager/user/menubox.php" );
+   ?>
 
-	
-	<?
+
+   <?
     // include the static pages for category 2
     $CategoryID = 2;
     include( "ezarticle/user/articlelinks.php" );
-	?>
+   ?>
 
-	<!-- Left menu end -->
-	
-	<img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="8" border="0"><br />
-	</td>
+<<<<<<< frame.php
+   <!-- Left menu end -->
+=======
+   <!-- Left menu end -->
 
-	<td width="1%" bgcolor="#ffffff"><img src="<? print $wwwDir; ?>/images/1x1.gif" width="2" height="1" border="0" alt="0" /></td>
+   <img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="8" border="0"><br />
+   </td>
+>>>>>>> 1.2
+
+<<<<<<< frame.php
+   <img src="/images/1x1.gif" width="130" height="8" border="0"><br />
+   </td>
+
+   <td width="1%" bgcolor="#ffffff"><img src="/images/1x1.gif" width="2" height="1" border="0" alt="0" /></td>
+=======
+   <td width="1%" bgcolor="#ffffff"><img src="<? print $wwwDir; ?>/images/1x1.gif" width="2" height="1" border="0" alt="0" /></td>
+>>>>>>> 1.2
     <td width="96%" bgcolor="#ffffff">
 
     <!-- Banner start -->
 
     <!-- Banner end-->
 
-	<!-- Main content view start -->
+   <!-- Main content view start -->
      <?
      print( $MainContents );
      ?>
- 
-	<!-- Main content view end -->
 
-	<br />
+   <!-- Main content view end -->
+
+   <br />
     </td>
-   	<td width="1%" bgcolor="#ffffff"><img src="<? print $wwwDir; ?>/images/1x1.gif" width="2" height="1" border="0" alt="" /></td>
+<<<<<<< frame.php
+      <td width="1%" bgcolor="#ffffff"><img src="/images/1x1.gif" width="2" height="1" border="0" alt="" /></td>
+=======
+      <td width="1%" bgcolor="#ffffff"><img src="<? print $wwwDir; ?>/images/1x1.gif" width="2" height="1" border="0" alt="" /></td>
+>>>>>>> 1.2
 
-	<td width="1%" bgcolor="#f0f0f0">
+   <td width="1%" bgcolor="#f0f0f0">
 
-   	<!-- Right menu start -->
-    
+      <!-- Right menu start -->
+
     <?
     include( "ezuser/user/userbox.php" );
-	?>
+   ?>
 
     <?
-  	$CategoryID = 1;
+   $CategoryID = 1;
     include( "ezarticle/user/smallarticlelist.php" );
     ?>
 
@@ -174,9 +197,9 @@ else
     <?
     include( "ezquiz/user/menubox.php" );
     ?>
-        
-	<hr noshade="noshade" size="4" />
-	
+
+   <hr noshade="noshade" size="4" />
+
     <?
     $session =& eZSession::globalSession();
 
@@ -184,7 +207,7 @@ else
 if ( $session->fetch() == false )
 {
     $session =& eZSession::globalSession();
-    $session->store();    
+    $session->store();
 }
 
 if ( $Design == 1 )
@@ -205,7 +228,7 @@ if ( $Design == 2 )
     {
         $redir = $REQUEST_URI;
     }
-        
+
     eZHTTPTool::header( "Location: $redir" );
     exit();
 }
@@ -220,30 +243,50 @@ if ( $Design == 3 )
     {
         $redir = $REQUEST_URI;
     }
-        
+
     eZHTTPTool::header( "Location: $redir" );
     exit();
 }
 
 
     ?>
-	
-	<h2>Alternative sitedesigns:</h2>
+<<<<<<< frame.php
+
+   <h2>Alternative sitedesigns:</h2>
+    <a href="<? print( $REQUEST_URI . "?Design=1"); ?>"><b>Intranet</b></a><br />
+    <a href="<? print( $REQUEST_URI . "?Design=2"); ?>"><b>Trade</b></a><br />
+    <a href="<? print( $REQUEST_URI . "?Design=3"); ?>"><b>News</b></a><br />
+
+      <!-- Right menu end -->
+
+   <img src="/images/1x1.gif" width="130" height="20" border="0" alt="" /><br />
+
+   <div align="center">
+   <a target="_blank" href="http://publish.ez.no"><img src="/images/powered-by-ezpublish-100x35-trans-lgrey.gif" width="100" height="35" border="0" alt="Powered by eZ publish" /></a>
+   </div>
+
+   <img src="/images/1x1.gif" width="130" height="8" border="0" alt="" /><br />
+
+   </td>
+=======
+
+   <h2>Alternative sitedesigns:</h2>
     <a href="<? print( $wwwDir . $index . $REQUEST_URI . "?Design=1"); ?>"><b>Intranet</b></a><br />
     <a href="<? print( $wwwDir . $index . $REQUEST_URI . "?Design=2"); ?>"><b>Trade</b></a><br />
     <a href="<? print( $wwwDir . $index . $REQUEST_URI . "?Design=3"); ?>"><b>News</b></a><br />
 
-   	<!-- Right menu end -->
-	
-	<img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="20" border="0" alt="" /><br />
-	
-	<div align="center">
-	<a target="_blank" href="http://publish.ez.no"><img src="<? print $wwwDir; ?>/images/powered-by-ezpublish-100x35-trans-lgrey.gif" width="100" height="35" border="0" alt="Powered by eZ publish" /></a>
-	</div>
-	
-	<img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="8" border="0" alt="" /><br />
-	 
-	</td>
+      <!-- Right menu end -->
+
+   <img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="20" border="0" alt="" /><br />
+
+   <div align="center">
+   <a target="_blank" href="http://publish.ez.no"><img src="<? print $wwwDir; ?>/images/powered-by-ezpublish-100x35-trans-lgrey.gif" width="100" height="35" border="0" alt="Powered by eZ publish" /></a>
+   </div>
+
+   <img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="8" border="0" alt="" /><br />
+
+   </td>
+>>>>>>> 1.2
   </tr>
 </table>
 
@@ -257,8 +300,13 @@ $StoreStats = $ini->read_var( "eZStatsMain", "StoreStats" );
 if ( $StoreStats == "enabled" )
 {
     // callback for storing the stats
+<<<<<<< frame.php
+    $imgSrc = "/stats/store" . $REQUEST_URI . "1x1.gif";
+    print( "<img src=\"$imgSrc\" height=\"1\" width=\"1\" border=\"0\" alt=\"\" />" );
+=======
     $imgSrc = $wwwDir . "/stats/store" . $REQUEST_URI . "1x1.gif";
-    print( "<img src=\"$imgSrc\" height=\"1\" width=\"1\" border=\"0\" alt=\"\" />" );    
+    print( "<img src=\"$imgSrc\" height=\"1\" width=\"1\" border=\"0\" alt=\"\" />" );
+>>>>>>> 1.2
 }
 
 ?>
