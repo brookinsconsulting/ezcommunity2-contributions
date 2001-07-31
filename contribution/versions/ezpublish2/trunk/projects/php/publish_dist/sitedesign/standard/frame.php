eec9b20fa129f0c30c103dac076c46a2b8fec511
eZ Systems <info@ez.no>
2001-07-31 14:51:12 +0000

    - made conform with non-virtualhosts install

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/standard/frame.php
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/standard/frame.php
@@ -25,7 +25,7 @@ if ( isset( $MetaRedirectLocation ) && isset( $MetaRedirectTimer ) )
 
 ?>
 
-<link rel="stylesheet" type="text/css" href="/sitedesign/standard/style.css" />
+<link rel="stylesheet" type="text/css" href="<? print $wwwDir; ?>/sitedesign/standard/style.css" />
 
 <script language="JavaScript1.2">
 <!--//
@@ -81,9 +81,9 @@ else
 
 </head>
 
-<body bgcolor="#b5b5b5" topmargin="6" marginheight="6" leftmargin="6" marginwidth="6"  onload="MM_preloadImages('/images/redigerminimrk.gif','/images/slettminimrk.gif','/images/downloadminimrk.gif','/images/addminimrk.gif')">
+<body bgcolor="#b5b5b5" topmargin="6" marginheight="6" leftmargin="6" marginwidth="6"  onload="MM_preloadImages('<? print $wwwDir; ?>/images/redigerminimrk.gif','<? print $wwwDir; ?>/images/slettminimrk.gif','<? print $wwwDir; ?>/images/downloadminimrk.gif','<? print $wwwDir; ?>/images/addminimrk.gif')">
 
-<img src="/sitedesign/standard/images/ezpublish-standard.gif" height="40" width="610" border="0" alt="" />
+<img src="<? print $wwwDir; ?>/sitedesign/standard/images/ezpublish-standard.gif" height="40" width="610" border="0" alt="" />
 
 
 <table width="100%" border="0" cellspacing="0" cellpadding="4">
@@ -132,10 +132,10 @@ else
 
 	<!-- Left menu end -->
 	
-	<img src="/images/1x1.gif" width="130" height="8" border="0"><br />
+	<img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="8" border="0"><br />
 	</td>
 
-	<td width="1%" bgcolor="#ffffff"><img src="/images/1x1.gif" width="2" height="1" border="0" alt="0" /></td>
+	<td width="1%" bgcolor="#ffffff"><img src="<? print $wwwDir; ?>/images/1x1.gif" width="2" height="1" border="0" alt="0" /></td>
     <td width="96%" bgcolor="#ffffff">
 
     <!-- Banner start -->
@@ -151,7 +151,7 @@ else
 
 	<br />
     </td>
-   	<td width="1%" bgcolor="#ffffff"><img src="/images/1x1.gif" width="2" height="1" border="0" alt="" /></td>
+   	<td width="1%" bgcolor="#ffffff"><img src="<? print $wwwDir; ?>/images/1x1.gif" width="2" height="1" border="0" alt="" /></td>
 
 	<td width="1%" bgcolor="#f0f0f0">
 
@@ -229,19 +229,19 @@ if ( $Design == 3 )
     ?>
 	
 	<h2>Alternative sitedesigns:</h2>
-    <a href="<? print( $REQUEST_URI . "?Design=1"); ?>"><b>Intranet</b></a><br />
-    <a href="<? print( $REQUEST_URI . "?Design=2"); ?>"><b>Trade</b></a><br />
-    <a href="<? print( $REQUEST_URI . "?Design=3"); ?>"><b>News</b></a><br />
+    <a href="<? print( $wwwDir . $index . $REQUEST_URI . "?Design=1"); ?>"><b>Intranet</b></a><br />
+    <a href="<? print( $wwwDir . $index . $REQUEST_URI . "?Design=2"); ?>"><b>Trade</b></a><br />
+    <a href="<? print( $wwwDir . $index . $REQUEST_URI . "?Design=3"); ?>"><b>News</b></a><br />
 
    	<!-- Right menu end -->
 	
-	<img src="/images/1x1.gif" width="130" height="20" border="0" alt="" /><br />
+	<img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="20" border="0" alt="" /><br />
 	
 	<div align="center">
-	<a target="_blank" href="http://publish.ez.no"><img src="/images/powered-by-ezpublish-100x35-trans-lgrey.gif" width="100" height="35" border="0" alt="Powered by eZ publish" /></a>
+	<a target="_blank" href="http://publish.ez.no"><img src="<? print $wwwDir; ?>/images/powered-by-ezpublish-100x35-trans-lgrey.gif" width="100" height="35" border="0" alt="Powered by eZ publish" /></a>
 	</div>
 	
-	<img src="/images/1x1.gif" width="130" height="8" border="0" alt="" /><br />
+	<img src="<? print $wwwDir; ?>/images/1x1.gif" width="130" height="8" border="0" alt="" /><br />
 	 
 	</td>
   </tr>
@@ -257,7 +257,7 @@ $StoreStats = $ini->read_var( "eZStatsMain", "StoreStats" );
 if ( $StoreStats == "enabled" )
 {
     // callback for storing the stats
-    $imgSrc = "/stats/store" . $REQUEST_URI . "1x1.gif";
+    $imgSrc = $wwwDir . "/stats/store" . $REQUEST_URI . "1x1.gif";
     print( "<img src=\"$imgSrc\" height=\"1\" width=\"1\" border=\"0\" alt=\"\" />" );    
 }
 
