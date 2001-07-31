0f4c5880f4900fca901f044d28c4a3e37ac2b9c3
Bård Farstad <bf@ez.no>
2001-07-31 11:45:41 +0000

    removed obsolete files.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sitedesign/news/defaultmetainfo.php
+++
@@ -1,4 +0,0 @@
-<meta name="author" content="eZ systems" />
-<meta name="copyright" content="eZ systems &copy; 2001" />
-<meta name="description" content="eZ publish - the web application suite" />
-<meta name="keywords" content="IT, data, computer, web, internet, PC, network, server, programming, publishing, portal, intranet, e-commerce, e-trade, software, database, open source, unix, linux, apache, PHP, HTML, XML, MySQL, Skien, Grenland, Telemark, Norway" />
