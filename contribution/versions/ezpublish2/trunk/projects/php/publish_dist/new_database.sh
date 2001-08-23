b50070cc2b9efe3d4c3eab4ec6d11fa6ac47a364
eZ Systems <info@ez.no>
2001-08-23 20:21:55 +0000

    Added default database name and fixed Blank password bug

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/new_database.sh
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/new_database.sh
@@ -1,35 +1,22 @@
 #!/bin/sh
 # This script will create the publish MySQL database with all the patches applied.
-echo -n 'Name of Database to create: '
+
+echo -n "Name of Database to create [publish]:"
 read DBNAME
+if [ -n "$DBNAME" ]; then
+   echo -n 'Database is' $DBNAME
+else
+   DBNAME="publish"
+fi
+echo "Database "$DBNAME" will be created"
+
 echo -n 'Mysql root password: '
 read PASS
-if $PASS
+if [ -n "$PASS" ]
 then
-   echo "Blank Password"
-   echo "Dropping database"
-   
-   if  mysqladmin -u root drop $DBNAME
-   then
-      echo "Dropping database"
-   else
-      echo "No database to drop"
-   fi
-   
-   echo "Creating database"
-   mysqladmin -u root create $DBNAME
-   echo "Adding Tables"
-   mysql -u root $DBNAME < sql/publish.sql 
-   echo "Adding Data"
-   mysql -u root $DBNAME < sql/data.sql 
-   echo "Upgrading"
-   mysql -u root $DBNAME < upgrade/2_1_to_2_1_1/2_1_to_2_1_1.sql
-   mysql -u root -e"grant all on $DBNAME.* to $DBNAME@localhost identified by '$DBNAME' "
-   
-else
    echo "Password is $PASS"
    echo "Dropping database"
-   
+
       if  mysqladmin -u root -p'$PASS' drop $DBNAME
       then
          echo "Dropping database"
@@ -46,6 +33,26 @@ else
    echo "Upgrading"
    mysql -u root -p'$PASS' $DBNAME < upgrade/2_1_to_2_1_1/2_1_to_2_1_1.sql
    mysql -u root -p'$PASS' -e"grant all on $DBNAME.* to $DBNAME@localhost identified by '$DBNAME' " 
+else
+   echo "Blank Password"
+   echo "Dropping database"
+
+   if  mysqladmin -u root drop $DBNAME
+   then
+      echo "Dropping database"
+   else
+      echo "No database to drop"
+   fi
+
+   echo "Creating database"
+   mysqladmin -u root create $DBNAME
+   echo "Adding Tables"
+   mysql -u root $DBNAME < sql/publish.sql 
+   echo "Adding Data"
+   mysql -u root $DBNAME < sql/data.sql 
+   echo "Upgrading"
+   mysql -u root $DBNAME < upgrade/2_1_to_2_1_1/2_1_to_2_1_1.sql
+   mysql -u root -e"grant all on $DBNAME.* to $DBNAME@localhost identified by '$DBNAME' "   
 fi
 
 
