a5c2dc07860b320284260ce63491eb65e2f305f1
eZ Systems <info@ez.no>
2001-08-24 12:26:12 +0000

    Added a for loop

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/make_sql.sh
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/make_sql.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# PostgreSQL
-cat ezsession/sql/postgresql/ezsession.sql > sql/publish_postgresql.sql      
-cat ezuser/sql/postgresql/ezuser.sql  >> sql/publish_postgresql.sql
-cat ezsitemanager/sql/postgresql/ezsitemanager.sql  >> sql/publish_postgresql.sql
-cat ezurltranslator/sql/postgresql/ezurltranslator.sql >> sql/publish_postgresql.sql
-cat ezlink/sql/postgresql/ezlink.sql >> sql/publish_postgresql.sql
-cat ezad/sql/postgresql/ezad.sql >> sql/publish_postgresql.sql
-cat ezimagecatalogue/sql/postgresql/ezimagecatalogue.sql >> sql/publish_postgresql.sql
-cat ezarticle/sql/postgresql/ezarticle.sql >> sql/publish_postgresql.sql
+modules=`ls -d ez*`
+
+for module in $modules
+do
+    if [ -f $module/sql/postgresql/$module.sql ]
+	then cat $module/sql/postgresql/$module.sql >> sql/publish_postgresql.sql
+    fi
+    if [ -f $module/sql/mysql/$module.sql ]
+	then cat $module/sql/mysql/$module.sql >> sql/publish_mysql.sql
+    fi
+done
