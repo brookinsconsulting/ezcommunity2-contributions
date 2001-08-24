6d866e1bcce27d6d80c6c784e7ab940fb6a88197
eZ Systems <info@ez.no>
2001-08-24 14:55:00 +0000

    fix

--- contribution/versions/ezpublish2/trunk/projects/php/eznewsfeed/sql/mysql/eznewsfeed.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/eznewsfeed/sql/mysql/eznewsfeed.sql
@@ -1,7 +1,3 @@
-#
-# Table structure for table 'eZNewsFeed_Category'
-#
-DROP TABLE IF EXISTS eZNewsFeed_Category;
 CREATE TABLE eZNewsFeed_Category (
   ID int(11) DEFAULT '0' NOT NULL auto_increment,
   Name varchar(150) DEFAULT '' NOT NULL,
@@ -10,16 +6,8 @@ CREATE TABLE eZNewsFeed_Category (
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZNewsFeed_Category'
-#
-
 INSERT INTO eZNewsFeed_Category VALUES (1,'News from freshmeat','',0);
 
-#
-# Table structure for table 'eZNewsFeed_News'
-#
-DROP TABLE IF EXISTS eZNewsFeed_News;
 CREATE TABLE eZNewsFeed_News (
   ID int(11) DEFAULT '0' NOT NULL auto_increment,
   IsPublished int NOT NULL DEFAULT '0',
@@ -33,17 +21,6 @@ CREATE TABLE eZNewsFeed_News (
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZNewsFeed_News'
-#
-
-
-
-
-#
-# Table structure for table 'eZNewsFeed_NewsCategoryLink'
-#
-DROP TABLE IF EXISTS eZNewsFeed_NewsCategoryLink;
 CREATE TABLE eZNewsFeed_NewsCategoryLink (
   ID int(11) DEFAULT '0' NOT NULL auto_increment,
   NewsID int(11) DEFAULT '0' NOT NULL,
@@ -51,17 +28,6 @@ CREATE TABLE eZNewsFeed_NewsCategoryLink (
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZNewsFeed_NewsCategoryLink'
-#
-
-
-
-
-#
-# Table structure for table 'eZNewsFeed_SourceSite'
-#
-DROP TABLE IF EXISTS eZNewsFeed_SourceSite;
 CREATE TABLE eZNewsFeed_SourceSite (
   ID int(11) DEFAULT '0' NOT NULL auto_increment,
   URL varchar(250),
@@ -74,10 +40,6 @@ CREATE TABLE eZNewsFeed_SourceSite (
   AutoPublish int(11) DEFAULT '0' NOT NULL,
   PRIMARY KEY (ID)
 );
-
-#
-# Dumping data for table 'eZNewsFeed_SourceSite'
-#
 
 INSERT INTO eZNewsFeed_SourceSite VALUES (1,'http://freshmeat.net/backend/fm.rdf','','',1,'Freshmeat','',0,0);
 
