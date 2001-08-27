2fe137343eafa036c9d9947e0ae06bf70d708275
Bård Farstad <bf@ez.no>
2001-08-27 12:22:06 +0000

    Updated SQL schema.

--- contribution/versions/ezpublish2/trunk/projects/php/ezfilemanager/sql/mysql/ezfilemanager.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/ezfilemanager/sql/mysql/ezfilemanager.sql
@@ -1,7 +1,3 @@
-#
-# Table structure for table 'eZFileManager_File'
-#
-DROP TABLE IF EXISTS eZFileManager_File;
 CREATE TABLE eZFileManager_File (
   ID int(11) DEFAULT '0' NOT NULL,
   Name char(200),
@@ -14,16 +10,6 @@ CREATE TABLE eZFileManager_File (
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZFileManager_File'
-#
-
-INSERT INTO eZFileManager_File VALUES (1,'CHANGELOG','The complete change log.','phpUuO7Ms','CHANGELOG',0,0,0);
-
-#
-# Table structure for table 'eZFileManager_FileFolderLink'
-#
-DROP TABLE IF EXISTS eZFileManager_FileFolderLink;
 CREATE TABLE eZFileManager_FileFolderLink (
   ID int(11) DEFAULT '0' NOT NULL,
   FolderID int(11) DEFAULT '0' NOT NULL,
@@ -31,15 +17,6 @@ CREATE TABLE eZFileManager_FileFolderLink (
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZFileManager_FileFolderLink'
-#
-
-
-#
-# Table structure for table 'eZFileManager_FilePageViewLink'
-#
-DROP TABLE IF EXISTS eZFileManager_FilePageViewLink;
 CREATE TABLE eZFileManager_FilePageViewLink (
   ID int(11) DEFAULT '0' NOT NULL,
   PageViewID int(11) DEFAULT '0' NOT NULL,
@@ -47,22 +24,6 @@ CREATE TABLE eZFileManager_FilePageViewLink (
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZFileManager_FilePageViewLink'
-#
-
-INSERT INTO eZFileManager_FilePageViewLink VALUES (1,121,1);
-INSERT INTO eZFileManager_FilePageViewLink VALUES (2,123,1);
-INSERT INTO eZFileManager_FilePageViewLink VALUES (3,216,1);
-INSERT INTO eZFileManager_FilePageViewLink VALUES (4,217,1);
-INSERT INTO eZFileManager_FilePageViewLink VALUES (5,219,1);
-INSERT INTO eZFileManager_FilePageViewLink VALUES (6,221,1);
-INSERT INTO eZFileManager_FilePageViewLink VALUES (7,223,1);
-
-#
-# Table structure for table 'eZFileManager_Folder'
-#
-DROP TABLE IF EXISTS eZFileManager_Folder;
 CREATE TABLE eZFileManager_Folder (
   ID int(11) DEFAULT '0' NOT NULL,
   Name varchar(100),
@@ -74,13 +35,6 @@ CREATE TABLE eZFileManager_Folder (
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZFileManager_Folder'
-#
-
-#
-# Table structure for table 'eZFileManager_FolderPermission'
-#
 CREATE TABLE eZFileManager_FolderPermission (
   ID int(11) NOT NULL,
   ObjectID int(11) default NULL,
@@ -90,9 +44,6 @@ CREATE TABLE eZFileManager_FolderPermission (
   PRIMARY KEY (ID)
 ) TYPE=MyISAM;
 
-#
-# Table structure for table 'eZFileManager_FilePermission'
-#
 CREATE TABLE eZFileManager_FilePermission (
   ID int(11) NOT NULL,
   ObjectID int(11) default NULL,
@@ -102,21 +53,7 @@ CREATE TABLE eZFileManager_FilePermission (
   PRIMARY KEY (ID)
 ) TYPE=MyISAM;
 
-#
-# Table structure for table 'eZFileManager_FileReadGroupLink'
-#
- 
-CREATE TABLE eZFileManager_FileReadGroupLink (
-  ID int(11) NOT NULL,
-  GroupID int(11) default NULL,
-  FileID int(11) default NULL,
-  PRIMARY KEY (ID)
-) TYPE=MyISAM;
 
-#
-# Table structure for table 'eZFileManager_FolderReadGroupLink'
-#
- 
 CREATE TABLE eZFileManager_FolderReadGroupLink (
   ID int(11) NOT NULL,
   GroupID int(11) default NULL,
@@ -125,10 +62,6 @@ CREATE TABLE eZFileManager_FolderReadGroupLink (
 ) TYPE=MyISAM;
 
 
-#
-# Table structure for table 'eZFileManager_FileReadGroupLink'
-#
- 
 CREATE TABLE eZFileManager_FileReadGroupLink (
   ID int(11) NOT NULL,
   GroupID int(11) default NULL,
@@ -136,10 +69,6 @@ CREATE TABLE eZFileManager_FileReadGroupLink (
   PRIMARY KEY (ID)
 ) TYPE=MyISAM;
 
-#
-# Table structure for table 'eZFileManager_FileWriteGroupLink'
-#
- 
 CREATE TABLE eZFileManager_FileWriteGroupLink (
   ID int(11) NOT NULL,
   GroupID int(11) default NULL,
