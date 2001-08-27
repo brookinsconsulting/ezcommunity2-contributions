25358ba8f84850f82811034b6798a0f86bb9028a
Bård Farstad <bf@ez.no>
2001-08-27 13:56:55 +0000

    Updated SQL schemas.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sql/publish_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sql/publish_mysql.sql
@@ -630,7 +630,7 @@ CREATE TABLE eZBug_Module (
   PRIMARY KEY (ID)
 );
 
-INSERT INTO eZBug_Module VALUES (1,0,'My program','');
+INSERT INTO eZBug_Module VALUES (1,0,'My program','', 1);
 
 CREATE TABLE eZBug_Priority (
   ID int NOT NULL,
