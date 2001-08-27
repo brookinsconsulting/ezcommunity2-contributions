accfede8cf1a8c361070f0c027374920bbb9d7ec
eZ Systems <info@ez.no>
2001-08-27 12:07:27 +0000

    Fixed the SQL`

--- contribution/versions/ezpublish2/trunk/projects/php/ezbug/sql/mysql/ezbug.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/ezbug/sql/mysql/ezbug.sql
@@ -1,9 +1,5 @@
-#
-# Table structure for table 'eZBug_Bug'
-#
-DROP TABLE IF EXISTS eZBug_Bug;
 CREATE TABLE eZBug_Bug (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL,
   Name varchar(150),
   Description text,
   UserID int(11) DEFAULT '0' NOT NULL,
@@ -17,70 +13,38 @@ CREATE TABLE eZBug_Bug (
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZBug_Bug'
-#
+INSERT INTO eZBug_Bug VALUES (1,'Help!','It dosent work!',33,997357856,0,0,0,'','','');
 
-INSERT INTO eZBug_Bug VALUES (1,'Help!','It dosent work!',33,997357856,0,0,0,'','');
-
-#
-# Table structure for table 'eZBug_BugCategoryLink'
-#
-DROP TABLE IF EXISTS eZBug_BugCategoryLink;
 CREATE TABLE eZBug_BugCategoryLink (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL,
   CategoryID int(11),
   BugID int(11),
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZBug_BugCategoryLink'
-#
-
 INSERT INTO eZBug_BugCategoryLink VALUES (1,2,1);
 
-#
-# Table structure for table 'eZBug_BugModuleLink'
-#
-DROP TABLE IF EXISTS eZBug_BugModuleLink;
 CREATE TABLE eZBug_BugModuleLink (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL,
   ModuleID int(11),
   BugID int(11),
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZBug_BugModuleLink'
-#
-
 INSERT INTO eZBug_BugModuleLink VALUES (1,1,1);
 
-#
-# Table structure for table 'eZBug_Category'
-#
-DROP TABLE IF EXISTS eZBug_Category;
 CREATE TABLE eZBug_Category (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL,
   Name varchar(150),
   Description text,
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZBug_Category'
-#
-
 INSERT INTO eZBug_Category VALUES (1,'GUI','');
 INSERT INTO eZBug_Category VALUES (2,'Feature request','');
 
-#
-# Table structure for table 'eZBug_Log'
-#
-DROP TABLE IF EXISTS eZBug_Log;
 CREATE TABLE eZBug_Log (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL,
   BugID int(11) DEFAULT '0' NOT NULL,
   UserID int(11) DEFAULT '0' NOT NULL,
   Description text,
@@ -89,56 +53,32 @@ CREATE TABLE eZBug_Log (
 );
 
 
-#
-# Table structure for table 'eZBug_Module'
-#
-DROP TABLE IF EXISTS eZBug_Module;
 CREATE TABLE eZBug_Module (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL,
   ParentID int(11),
   Name varchar(150),
   Description text,
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZBug_Module'
-#
-
 INSERT INTO eZBug_Module VALUES (1,0,'My program','');
 
-#
-# Table structure for table 'eZBug_Priority'
-#
-DROP TABLE IF EXISTS eZBug_Priority;
 CREATE TABLE eZBug_Priority (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL,
   Name char(150) DEFAULT '' NOT NULL,
   Value int(11),
   PRIMARY KEY (ID)
 );
 
-#
-# Dumping data for table 'eZBug_Priority'
-#
-
 INSERT INTO eZBug_Priority VALUES (1,'High',NULL);
 INSERT INTO eZBug_Priority VALUES (2,'Middels',NULL);
 INSERT INTO eZBug_Priority VALUES (3,'Low',NULL);
 
-#
-# Table structure for table 'eZBug_Status'
-#
-DROP TABLE IF EXISTS eZBug_Status;
 CREATE TABLE eZBug_Status (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL,
   Name char(150) DEFAULT '' NOT NULL,
   PRIMARY KEY (ID)
 );
-
-#
-# Dumping data for table 'eZBug_Status'
-#
 
 INSERT INTO eZBug_Status VALUES (1,'Fixed');
 
--- contribution/versions/ezpublish2/trunk/projects/php/ezbug/sql/postgresql/ezbug.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/ezbug/sql/postgresql/ezbug.sql
@@ -1,5 +1,5 @@
 CREATE TABLE eZBug_Bug (
-  ID int DEFAULT '0' NOT NULL,
+  ID int NOT NULL,
   Name varchar(150),
   Description text,
   UserID int DEFAULT '0' NOT NULL,
@@ -13,29 +13,38 @@ CREATE TABLE eZBug_Bug (
   PRIMARY KEY (ID)
 );
 
+INSERT INTO eZBug_Bug VALUES (1,'Help!','It dosent work!',33,997357856,0,0,0,'','','');
+
 CREATE TABLE eZBug_BugCategoryLink (
-  ID int DEFAULT '0' NOT NULL,
+  ID int NOT NULL,
   CategoryID int,
   BugID int,
   PRIMARY KEY (ID)
 );
 
+INSERT INTO eZBug_BugCategoryLink VALUES (1,2,1);
+
 CREATE TABLE eZBug_BugModuleLink (
-  ID int DEFAULT '0' NOT NULL,
+  ID int NOT NULL,
   ModuleID int,
   BugID int,
   PRIMARY KEY (ID)
 );
 
+INSERT INTO eZBug_BugModuleLink VALUES (1,1,1);
+
 CREATE TABLE eZBug_Category (
-  ID int DEFAULT '0' NOT NULL,
+  ID int NOT NULL,
   Name varchar(150),
   Description text,
   PRIMARY KEY (ID)
 );
 
+INSERT INTO eZBug_Category VALUES (1,'GUI','');
+INSERT INTO eZBug_Category VALUES (2,'Feature request','');
+
 CREATE TABLE eZBug_Log (
-  ID int DEFAULT '0' NOT NULL,
+  ID int NOT NULL,
   BugID int DEFAULT '0' NOT NULL,
   UserID int DEFAULT '0' NOT NULL,
   Description text,
@@ -43,23 +52,33 @@ CREATE TABLE eZBug_Log (
   PRIMARY KEY (ID)
 );
 
+
 CREATE TABLE eZBug_Module (
-  ID int DEFAULT '0' NOT NULL,
+  ID int NOT NULL,
   ParentID int,
   Name varchar(150),
   Description text,
   PRIMARY KEY (ID)
 );
 
+INSERT INTO eZBug_Module VALUES (1,0,'My program','');
+
 CREATE TABLE eZBug_Priority (
-  ID int DEFAULT '0' NOT NULL,
+  ID int NOT NULL,
   Name varchar(150) DEFAULT '' NOT NULL,
   Value int,
   PRIMARY KEY (ID)
 );
 
+INSERT INTO eZBug_Priority VALUES (1,'High',NULL);
+INSERT INTO eZBug_Priority VALUES (2,'Middels',NULL);
+INSERT INTO eZBug_Priority VALUES (3,'Low',NULL);
+
 CREATE TABLE eZBug_Status (
-  ID int DEFAULT '0' NOT NULL,
+  ID int NOT NULL,
   Name varchar(150) DEFAULT '' NOT NULL,
   PRIMARY KEY (ID)
 );
+
+INSERT INTO eZBug_Status VALUES (1,'Fixed');
+
