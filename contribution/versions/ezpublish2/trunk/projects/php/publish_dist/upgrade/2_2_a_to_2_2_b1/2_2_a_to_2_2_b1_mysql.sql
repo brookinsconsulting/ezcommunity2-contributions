8f39633791ff05d29b630d35a7049d94474462e0
Bård Farstad <bf@ez.no>
2001-08-29 14:31:25 +0000

    Added upgrade for XML tags in trade.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_a_to_2_2_b1/2_2_a_to_2_2_b1_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_a_to_2_2_b1/2_2_a_to_2_2_b1_mysql.sql
@@ -1,1 +1,7 @@
 alter table eZContact_Person change BirthDate BirthDate int;
+
+# XML contents in trade
+alter table eZTrade_Product add Contents text not null;
+Update eZTrade_Product set Contents=Concat( "<?xml version=\"1.0\"?><article><generator>qdom</generator><intro>", Brief, "</intro><body><page>", Description, "</page></body></article>" );  
+
+alter table eZTrade_ProductImageLink add Placement int not null default 0; 
