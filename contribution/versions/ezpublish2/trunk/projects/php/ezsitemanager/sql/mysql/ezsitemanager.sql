135433dbb0518be0628bcaeccdb0c47f39152a01
eZ Systems <info@ez.no>
2001-08-30 06:21:08 +0000

    Languages in sections

--- contribution/versions/ezpublish2/trunk/projects/php/ezsitemanager/sql/mysql/ezsitemanager.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/ezsitemanager/sql/mysql/ezsitemanager.sql
@@ -9,11 +9,12 @@ CREATE TABLE eZSiteManager_Section (
   SiteDesign varchar(30) default NULL,
   Created int(11) default NULL,
   TemplateStyle varchar(255) default NULL,
+  Language varchar(5) default NULL,
   PRIMARY KEY (ID)
 ) TYPE=MyISAM;
 
 
-INSERT INTO eZSiteManager_Section   ( ID,  Name, Created, Description,  SiteDesign, TemplateStyle) VALUES ( 1, 'Standard Section', 1, NULL, 'standard', NULL);
+INSERT INTO eZSiteManager_Section   ( ID,  Name, Created, Description,  SiteDesign, TemplateStyle, Language) VALUES ( 1, 'Standard Section', 1, NULL, 'standard', NULL, NULL);
 
 
 
