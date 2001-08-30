cdde3f53fb22987f05ee10146bab5da3f7578d1c
eZ Systems <info@ez.no>
2001-08-30 06:34:50 +0000

    Languages in sections

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_a_to_2_2_b1/2_2_a_to_2_2_b1_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_a_to_2_2_b1/2_2_a_to_2_2_b1_mysql.sql
@@ -4,4 +4,8 @@ alter table eZContact_Person change BirthDate BirthDate int;
 alter table eZTrade_Product add Contents text not null;
 Update eZTrade_Product set Contents=Concat( "<?xml version=\"1.0\"?><article><generator>qdom</generator><intro>", Brief, "</intro><body><page>", Description, "</page></body></article>" );  
 
-alter table eZTrade_ProductImageLink add Placement int not null default 0; 
+alter table eZTrade_ProductImageLink add Placement int not null default 0;
+
+#Languages in sections
+alter table eZSiteManager_Section add Language varchar(5) default NULL;
+ 
