af196498d206892daf7dbe0e0c15dc7225921e24
Bård Farstad <bf@ez.no>
2001-09-04 14:39:19 +0000

    SQL update.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_b1_to_2_2_b2/2_2_b1_to_2_2_b2_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_b1_to_2_2_b2/2_2_b1_to_2_2_b2_mysql.sql
@@ -1,2 +1,7 @@
 alter table eZTrade_Voucher add MailMethod int default 1;
 alter table eZTrade_VoucherEMail change Email OnlineID int default 0; 
+
+
+# Article search
+alter table eZArticle_ArticleWordLink add Frequency float default 0.2;
+alter table eZArticle_Word add Frequency float default 0.2;
