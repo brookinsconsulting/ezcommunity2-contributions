fc2be049d17a90ae62fb3280c0f63344c2a221dc
eZ Systems <info@ez.no>
2001-09-04 11:57:28 +0000

    Speed up of keywords added.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_a_to_2_2_b1/2_2_a_to_2_2_b1_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_a_to_2_2_b1/2_2_a_to_2_2_b1_mysql.sql
@@ -16,3 +16,7 @@ alter table eZTrade_Option add RemoteID varchar(100);
 alter table eZTrade_CartOptionValue add Count int default 1;
 
 alter table eZTrade_Order add Comment text;
+
+/* speeding up the keywords */
+alter table eZArticle_ArticleKeyword add index (Keyword);
+alter table eZArticle_ArticleKeyword add index (ArticleID);
