57275c9d31f8d30d8ba46f523f478703aaa5e1c2
Bård Farstad <bf@ez.no>
2001-09-04 07:35:38 +0000

    SQL updates.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sql/publish_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sql/publish_mysql.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE eZAd_Ad(
   ID int NOT NULL,
   Name varchar(150) default NULL,
@@ -1095,7 +1094,7 @@ CREATE TABLE eZForum_Message (
   ForumID int NOT NULL default '0',
   Topic varchar(60) default NULL,
   Body text,
-  UserName varchar default NULL,
+  UserName varchar(60) default NULL,
   UserID int default NULL,
   Parent int default NULL,
   EmailNotice int NOT NULL default '0',
