74897997c6499cfc2ca2ede8dd994869fc11e679
eZ Systems <info@ez.no>
2001-09-26 10:17:36 +0000

    SQL Admin working sample

--- contribution/versions/ezpublish2/trunk/projects/php/ezsitemanager/admin/sqlquery.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezsitemanager/admin/sqlquery.php
@@ -2,24 +2,59 @@
 
 include_once( "classes/ezdb.php" );
 
-$Language = $ini->read_var( "eZSiteManager", "Language" );
+$Language = $ini->read_var( "eZSiteManagerMain", "Language" );
 
-$t = new eZTemplate( "ezsitemanager/admin/" . $ini->read_var( "eZSiteManager", "AdminTemplateDir" ),
+$t = new eZTemplate( "ezsitemanager/admin/" . $ini->read_var( "eZSiteManagerMain", "AdminTemplateDir" ),
                      "ezsitemanager/admin/intl", $Language, "sqlquery.php" );
 
 $t->set_file( "sql_query_tpl", "sqlquery.tpl" );
 
 $t->setAllStrings();
 
-
 $db =& eZDB::globalDatabase();
 
-$res = $db->array_query( $ret_array, $QueryText );
+$QueryRes   = '';
+$QueryError = '';
 
-if ( $res == false )
-    print( "query error" );
+if ( ! $QueryText )
+{
+    $QueryError = 'No query specified';        
+}
+else
+{
+    $db->array_query( $return_array, $QueryText );
 
-$t->set_var( "query_text", $QueryText );
+    if ($return_array)
+    {
+	$QueryRes .= "<table width=\"100%\" border=\"1\"";
+	
+	for ( $i = 0; $i < count( $return_array ); $i++ )
+        {
+	    $QueryRes .= "<tr>";
+	    
+	    for ( $j = 0; $j <  count( $return_array[$i]) / 2 ; $j++ )
+    	    {
+		$QueryRes .= "<td>".$return_array[$i][$j]."</td>";
+	    }
+	    $QueryRes .= "</tr>";
+
+	}
+	
+	$QueryRes .= "</table>";
+	
+    }
+    else
+    {
+	$QueryError = "SQL error occured";
+    }
+
+    
+
+}
+
+$t->set_var( "query_text",   $QueryText  );
+$t->set_var( "query_result", $QueryRes   );
+$t->set_var( "error", 	     $QueryError );
 
 $t->pparse( "output", "sql_query_tpl" );
 
