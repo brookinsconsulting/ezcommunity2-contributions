5b313e7b84d27b6d8a60a3c4fee5e1f98e83dab4
Bård Farstad <bf@ez.no>
2001-09-28 13:02:35 +0000

    SQL updates.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sql/publish_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/sql/publish_mysql.sql
@@ -1217,6 +1217,7 @@ CREATE TABLE eZImageCatalogue_CategoryPermission (
   GroupID int default NULL,
   ReadPermission int default '0',
   WritePermission int default '0',
+  UploadPermission int default '0',
   PRIMARY KEY (ID)
 );
 
