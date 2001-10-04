c9a9700fde918e535e624a2e5a21cf78dadec104
Vidar Langseid <vl@ez.no>
2001-10-04 09:29:02 +0000

    sql upgrade : lagt in SectionID i eZFileManager_Folder

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_b2_to_2_2_pre/2_2_b2_to_2_2_pre_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_b2_to_2_2_pre/2_2_b2_to_2_2_pre_mysql.sql
@@ -1,2 +1,5 @@
 alter table eZTrade_VoucherInformation add FromAddressID int default 0; 
 alter table eZTrade_VoucherInformation change AddressID ToAddressID int default 0;        
+
+alter table eZFileManager_Folder add SectionID int(11);
+
