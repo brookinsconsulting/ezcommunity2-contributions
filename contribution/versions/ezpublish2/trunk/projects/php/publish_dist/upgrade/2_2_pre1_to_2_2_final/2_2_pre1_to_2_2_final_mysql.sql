9bbb173fddf79e6ff66c67482a250b667d790e63
eZ Systems <info@ez.no>
2001-10-15 10:26:37 +0000

    Added sql tables for menu

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_pre1_to_2_2_final/2_2_pre1_to_2_2_final_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_pre1_to_2_2_final/2_2_pre1_to_2_2_final_mysql.sql
@@ -1,1 +1,14 @@
 alter table eZBulkMail_UserCategoryLink drop ID;
+
+CREATE TABLE eZSiteManager_Menu (
+  ID int(11) NOT NULL default '0',
+  Name varchar(40) default NULL,
+  Link varchar(40) default NULL,
+  Type int(11) default '1',
+  ParentID int(11) default '0'
+) TYPE=MyISAM;
+
+CREATE TABLE eZSiteManager_MenuType (
+  ID int(11) NOT NULL default '0',
+  Name varchar(30) default NULL
+) TYPE=MyISAM;
