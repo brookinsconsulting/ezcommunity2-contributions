25fd231ffa4ca05b84811820bbd09e218ec9ecdc
eZ Systems <info@ez.no>
2001-10-23 10:01:45 +0000

    Added Verified in eZPreOrder.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_2_2_1/2_2_to_2_2_1_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_2_2_1/2_2_to_2_2_1_mysql.sql
@@ -1,1 +1,2 @@
-alter table eZTrade_VoucherInformation change OnlineID ToOnlineID int default '0';           
+alter table eZTrade_VoucherInformation change OnlineID ToOnlineID int default '0';
+alter table eZTrade_PreOrder add Verified Verified int default '0';
