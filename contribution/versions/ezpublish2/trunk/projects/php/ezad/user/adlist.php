4c2ce8d6f5603045a5c42b84189b306a9259dff3
eZ Systems <info@ez.no>
2001-10-29 16:35:30 +0000

    process external/internal ads

--- contribution/versions/ezpublish2/trunk/projects/php/ezad/user/adlist.php
+++ contribution/versions/ezpublish2/trunk/projects/php/ezad/user/adlist.php
@@ -1,6 +1,6 @@
 <?php
 // 
-// $Id: adlist.php,v 1.21 2001/09/29 12:35:14 kaid Exp $
+// $Id: adlist.php,v 1.22 2001/10/29 16:35:30 master Exp $
 //
 // Created on: <25-Nov-2000 15:44:37 bf>
 //
@@ -67,7 +67,16 @@ foreach ( $adList as $ad )
     }
     else
     {
-        print( "<a target=\"_blank\" href=\"".$GlobalSiteIni->WWWDir.$GlobalSiteIni->Index."/ad/goto/$adID/\"><img src=\"".$GlobalSiteIni->WWWDir."$imgSRC\" width=\"$imgWidth\" height=\"$imgHeight\" border=\"0\" alt=\"\" /></a><br />" );
+	if ( strpos( $ad->URL, "http://" ) === 0 )
+	{
+	    print( "<a target=\"_blank\" href=\"".$GlobalSiteIni->WWWDir.$GlobalSiteIni->Index."/ad/goto/$adID/\">" );
+	}
+	else
+	{
+	    print( "<a href=\"".$GlobalSiteIni->WWWDir.$GlobalSiteIni->Index."/ad/goto/$adID/\">" );
+	}
+
+	print ("<img src=\"".$GlobalSiteIni->WWWDir."$imgSRC\" width=\"$imgWidth\" height=\"$imgHeight\" border=\"0\" alt=\"\" /></a><br />" );
     }
 }
 
