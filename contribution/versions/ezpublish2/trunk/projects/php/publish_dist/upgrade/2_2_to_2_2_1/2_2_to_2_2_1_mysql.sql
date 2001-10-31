def8e70f195ade2fcdd17ac2af154928ac480abe
Bård Farstad <bf@ez.no>
2001-10-31 12:25:49 +0000

    SQL updates.

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_2_2_1/2_2_to_2_2_1_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_2_2_1/2_2_to_2_2_1_mysql.sql
@@ -1,3 +1,4 @@
+## NOTE this file should not be used in head branch..
 alter table eZTrade_VoucherInformation change OnlineID ToOnlineID int default '0';
 alter table eZTrade_PreOrder add Verified int default '0';
 
--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_3_0/2_2_to_3_0.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_3_0/2_2_to_3_0.sql
@@ -1,7 +1,27 @@
+# This SQL file is for 3.0 features in the upcomming version ( head branch )
 CREATE TABLE eZBug_Support (
   ID int(11) NOT NULL default '0',
   Name varchar(150),
   Email varchar(100) NOT NULL,
   ExpiryDate int(11),
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZArticle_ArticleRate( ID int not null primary key, ArticleID int not null, Rate int not null, IP char(15) not null );
+
+alter table eZTrade_VoucherInformation change OnlineID ToOnlineID int default '0';
+alter table eZTrade_PreOrder add Verified int default '0';
+
+CREATE TABLE eZUser_Title (
+  ID int NOT NULL,
+  Name varchar(255) default NULL,
+  PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE eZUser_UserTitleLink (
+  ID int NOT NULL,
+  UserID int NOT NULL,
+  TitleID int NOT NULL,
   PRIMARY KEY (ID)
 );
