95063a03abccbc005bf5953f933a0525e69f91ea
eZ Systems <info@ez.no>
2001-11-01 17:48:34 +0000

    Fixed some media bugs

--- contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/ezmediacatalogue/sql/mysql/ezmediacatalogue.sql
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/ezmediacatalogue/sql/mysql/ezmediacatalogue.sql
@@ -25,6 +25,10 @@ INSERT INTO eZMediaCatalogue_Attribute VALUES (13,3,'type',996137896,12,'','appl
 INSERT INTO eZMediaCatalogue_Attribute VALUES (14,3,'width',996137906,13,'','');
 INSERT INTO eZMediaCatalogue_Attribute VALUES (15,3,'height',996137917,14,'','');
 INSERT INTO eZMediaCatalogue_Attribute VALUES (16,2,'type',996139826,15,'','application/x-mplayer2');
+INSERT INTO eZMediaCatalogue_Attribute VALUES (17,4,'width',1004640070,16,'','');
+INSERT INTO eZMediaCatalogue_Attribute VALUES (18,4,'height',1004640076,17,'','');
+INSERT INTO eZMediaCatalogue_Attribute VALUES (19,4,'controls',1004640087,18,'','ImageWindow');
+INSERT INTO eZMediaCatalogue_Attribute VALUES (20,4,'autostart',1004640100,19,'','true');
 
 CREATE TABLE eZMediaCatalogue_Category (
   ID int(11) NOT NULL,
@@ -100,6 +104,7 @@ CREATE TABLE eZMediaCatalogue_TypeLink (
 INSERT INTO eZMediaCatalogue_Type VALUES (1,'QuickTime');
 INSERT INTO eZMediaCatalogue_Type VALUES (2,'Windows Media Player');
 INSERT INTO eZMediaCatalogue_Type VALUES (3,'ShockWave Flash');
+INSERT INTO eZMediaCatalogue_Type VALUES (4,'Real Player');
 
 CREATE TABLE eZMediaCatalogue_AttributeValue (
   ID int(11) NOT NULL auto_increment,
@@ -108,31 +113,3 @@ CREATE TABLE eZMediaCatalogue_AttributeValue (
   Value varchar(200) default NULL,
   PRIMARY KEY (ID)
 ) TYPE=MyISAM;
-
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (33,1,16,'application/x-mplayer2');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (32,1,10,'true');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (31,1,9,'false');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (30,1,8,'false');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (29,1,7,'300');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (28,1,6,'300');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (7,3,11,'high');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (8,3,12,'http://www.macromedia.com/shockwave/download/index.cgi?P1_P rod_Version=ShockwaveFlash');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (9,3,13,'application/x-shockwave-flash');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (10,3,14,'500');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (11,3,15,'400');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (38,2,15,'400');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (37,2,14,'500');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (36,2,13,'application/x-shockwave-flash');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (35,2,12,'http://www.macromedia.com/shockwave/download/index.cgi?P1_P rod_Version=ShockwaveFlash');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (34,2,11,'high');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (17,4,1,'500');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (18,4,2,'400');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (19,4,3,'video/quicktime');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (20,4,4,'true');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (21,4,5,'true');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (44,5,16,'application/x-mplayer2');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (43,5,10,'true');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (42,5,9,'false');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (41,5,8,'false');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (40,5,7,'300');
-INSERT INTO eZMediaCatalogue_AttributeValue VALUES (39,5,6,'300');
