49d9b49f4d451a838698028f8d40c2d01ffe0f11
Unknown Contributor <info@example.com>
2001-11-01 12:14:47 +0000

    This commit was manufactured by cvs2svn to create branch 'eZPublish_2_2_final_branch'.

---
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/publish_dist/upgrade/2_2_to_2_2_1/2_2_to_2_2_1_mysql.sql
@@ -0,0 +1,18 @@
+## NOTE this file should not be used in head branch..
+alter table eZTrade_VoucherInformation change OnlineID ToOnlineID int default '0';
+alter table eZTrade_PreOrder add Verified int default '0';
+
+
+CREATE TABLE eZUser_Title (
+  ID int NOT NULL,
+  Name varchar(255) default NULL,
+  PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE eZUser_UserTitleLink (
+  ID int NOT NULL,
+  UserID int NOT NULL,
+  TitleID int NOT NULL,
+  PRIMARY KEY (ID)
+);
