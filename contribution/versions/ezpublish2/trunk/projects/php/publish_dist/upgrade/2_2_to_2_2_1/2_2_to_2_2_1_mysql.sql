25d973d568d03a23110a21f38ddf8de15480ce84
eZ Systems <info@ez.no>
2001-11-12 08:05:33 +0000

    Added SQL changes

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_2_2_1/2_2_to_2_2_1_mysql.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_2_2_1/2_2_to_2_2_1_mysql.sql
@@ -1,7 +1,9 @@
 ## NOTE this file should not be used in head branch..
 alter table eZTrade_VoucherInformation change OnlineID ToOnlineID int default '0';
+alter table eZTrade_PreOrder add Verified Verified int default '0';
+alter table eZArticle_Article add LinkURL varchar(100) default '';
 alter table eZTrade_PreOrder add Verified int default '0';
-
+alter table eZTrade_VoucherInformation add UserID int default 0; 
 
 CREATE TABLE eZUser_Title (
   ID int NOT NULL,
@@ -9,10 +11,11 @@ CREATE TABLE eZUser_Title (
   PRIMARY KEY (ID)
 );
 
-
 CREATE TABLE eZUser_UserTitleLink (
   ID int NOT NULL,
   UserID int NOT NULL,
   TitleID int NOT NULL,
   PRIMARY KEY (ID)
 );
+
+alter table eZTrade_ProductSectionDict add ID int NOT NULL default 0; 
