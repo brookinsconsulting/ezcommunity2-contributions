740a233a7b4718f482349b625dc3db99a2b53ad9
Bård Farstad <bf@ez.no>
2001-11-19 11:37:48 +0000

    Fixed convert.

--- contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/ezuser/admin/welcome.php
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/ezuser/admin/welcome.php
@@ -1,6 +1,6 @@
 <?php
 // 
-// $Id: welcome.php,v 1.11.2.2 2001/11/08 09:56:54 bf Exp $
+// $Id: welcome.php,v 1.11.2.3 2001/11/19 11:37:48 bf Exp $
 //
 // Created on: <13-Nov-2000 10:57:15 bf>
 //
@@ -74,7 +74,11 @@ if ( $ini->read_var( "site", "CheckDependence" ) == "enabled" )
         $error = true;
     }
 
-    $check = system( "convert > /dev/null", $ret );
+    $image_prog = "convert";    
+    if ( $ini->has_var( "classes", "ImageConversionProgram" ) )
+        $image_prog = $ini->read_var( "classes", "ImageConversionProgram" );
+    
+    $check = system( "$image_prog > /dev/null", $ret );
 
     if ( $ret == "127" )
     {
