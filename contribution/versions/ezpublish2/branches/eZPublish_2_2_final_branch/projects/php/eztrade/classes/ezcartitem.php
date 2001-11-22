14c7de7dab9bd4e07b24e4c986ad295709442eb2
eZ Systems <info@ez.no>
2001-11-22 12:31:59 +0000

    Added functions for calculation of savings.

--- contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/eztrade/classes/ezcartitem.php
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/eztrade/classes/ezcartitem.php
@@ -1,6 +1,6 @@
 <?php
 // 
-// $Id: ezcartitem.php,v 1.27.2.1 2001/11/01 15:39:06 ce Exp $
+// $Id: ezcartitem.php,v 1.27.2.2 2001/11/22 12:31:59 pkej Exp $
 //
 // Definition of eZCartItem class
 //
@@ -282,6 +282,61 @@ class eZCartItem
             {
                 $price = ( $voucherInfo->correctPrice( $calcVAT, $product ) + $optionPrice );
             }
+        }
+
+        return $price;        
+    }
+    
+    /*!
+      Returns the correct localized price of the product.
+    */
+    function localeSavings( $calcCount=true, $withOptions=true, $calcVAT )
+    {
+        $ini =& INIFile::globalINI();
+        $inLanguage = $ini->read_var( "eZTradeMain", "Language" );
+        
+        $locale = new eZLocale( $inLanguage );
+        $currency = new eZCurrency();
+        
+        $price = $this->correctSavings( $calcCount, $withOptions, $calcVAT );
+        
+        $currency->setValue( $price );
+        return $locale->format( $currency );
+    }    
+    
+    /*!
+      Returns the correct savings of the product based on the logged in user, and the
+      VAT status and use.
+    */
+    function correctSavings( $calcCount=true, $withOptions=true, $calcVAT )
+    {
+        $optionValues =& $this->optionValues();
+        $product =& $this->product();
+            
+        $optionPrice = 0.0;
+        if ( $withOptions )
+        {
+            foreach ( $optionValues as $optionValue )
+            {
+                $option =& $optionValue->option();
+                $value =& $optionValue->optionValue();
+                    
+                $price = $value->correctSavings( $calcVAT, $product );
+                        
+                if ( $calcCount == true )
+                    $price = $price * $optionValue->count();
+                    
+                $optionPrice+=$price;
+            }
+        }
+            
+        if ( $calcCount == true )
+        {
+            $price = ( $product->correctSavings( $calcVAT ) * $this->count() ) + $optionPrice;
+        }
+        else
+        {
+            $price = ( $product->correctSavings( $calcVAT ) + $optionPrice );
         }
 
         return $price;        
