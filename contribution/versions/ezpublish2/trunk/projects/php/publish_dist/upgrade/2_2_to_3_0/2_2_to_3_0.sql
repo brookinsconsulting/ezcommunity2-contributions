e51f8a631858cb8024fd4c355789276fb48d1e97
eZ Systems <info@ez.no>
2001-12-04 14:19:14 +0000

    added tables for expiry date

--- contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_3_0/2_2_to_3_0.sql
+++ contribution/versions/ezpublish2/trunk/projects/php/publish_dist/upgrade/2_2_to_3_0/2_2_to_3_0.sql
@@ -12,6 +12,7 @@ CREATE TABLE eZBug_SupportCategory (
   Name varchar(150),
   BugModuleID int(11),
   Email varchar(100) NOT NULL,
+  ReplyTo varchar(100) NOT NULL,
   Password varchar(100) NOT NULL,
   MailServer varchar(100) NOT NULL,
   MailServerPort int default '110',
@@ -41,3 +42,43 @@ CREATE TABLE eZUser_UserTitleLink (
 ALTER TABLE eZLink_Category ADD SortMode int default '1';
 ALTER TABLE eZLink_Category ADD Placement int default '0';
 ALTER TABLE eZLink_LinkCategoryLink ADD Placement int default '0';
+
+alter table eZUser_User add ExpiryDate int default '0';
+alter table eZUser_User add IsActive int default 1;
+
+CREATE TABLE eZUser_Title (
+  ID int NOT NULL,
+  Name varchar(255) default NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZUser_UserTitleLink (
+  ID int NOT NULL,
+  UserID int NOT NULL,
+  TitleID int NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE eZUser_Additional (
+  ID int(11) NOT NULL default '0',
+  Name varchar(50) default NULL,
+  Placement int(11) default '0',
+  Type int(11) default '1',
+  PRIMARY KEY  (ID)
+) TYPE=MyISAM;
+
+CREATE TABLE eZUser_AdditionalFixedValue (
+  ID int(11) NOT NULL default '0',
+  Value varchar(70) default NULL,
+  AdditionalID int(11) default '0',
+  PRIMARY KEY  (ID)
+) TYPE=MyISAM;
+
+CREATE TABLE eZUser_AdditionalValue (
+  ID int(11) NOT NULL default '0',
+  AdditionalID int(11) NOT NULL default '0',
+  UserID int(11) NOT NULL default '0',
+  Value varchar(100) default NULL,
+  PRIMARY KEY  (ID)
+) TYPE=MyISAM;
+
