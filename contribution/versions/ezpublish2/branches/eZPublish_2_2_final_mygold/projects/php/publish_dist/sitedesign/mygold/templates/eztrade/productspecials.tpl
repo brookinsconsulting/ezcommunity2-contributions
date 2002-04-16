7a148e79e257878a8179365f9ec1c42e47f6e649
eZ Systems <info@ez.no>
2002-04-16 10:39:49 +0000

    Added files

---
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_mygold/projects/php/publish_dist/sitedesign/mygold/templates/eztrade/productspecials.tpl
@@ -0,0 +1,57 @@
+<h1>{special_name}</h1>
+
+<hr noshade="noshade" size="1" />
+
+<p>{special_description}</p>
+
+<hr noshade="noshade" size="1" />
+<table width="100%" cellspacing="0" cellpadding="0" border="0">
+  <!-- BEGIN product_tpl -->
+  <tr>
+    <td colspan="2">
+      <a href="{www_dir}{index}/trade/productview/{product_id}/{category_id}/"><h2>{product_name}</h2></a><br />
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2" class="spacer2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="99%" valign="top">
+      {product_intro_text}
+    </td>
+    <td>
+      <!-- BEGIN image_tpl -->
+      <table align="right">
+        <tr>
+          <td>
+            <table border="0" cellspacing="1" cellpadding="0" bgcolor="#003366">
+              <tr>
+	        <td><a href="{www_dir}{index}/trade/productview/{product_id}/{category_id}/"><img src="{www_dir}{thumbnail_image_uri}" border="0" width="{thumbnail_image_width}" height="{thumbnail_image_height}" /></a></td>
+	      </tr>
+	    </table>  
+	  </td>
+	</tr>
+	<tr>
+          <td>
+	    {thumbnail_image_caption}
+          </td>
+	</tr>
+      </table>
+      <!-- END image_tpl -->
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2">
+      <!-- BEGIN price_tpl -->
+      <p class "pris">
+	{product_price}
+      </p>
+      <!-- END price_tpl -->
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2"><hr noshade="noshade" size="1" /></td>
+  </tr>
+  <!-- END product_tpl -->
+</table>
+
---
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_mygold/projects/php/publish_dist/sitedesign/mygold/uptodate.php
@@ -0,0 +1,46 @@
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+  <tr> 
+    <th>Liefergarantie</th>
+  </tr>
+  <tr> 
+    <td class="spacer2">&nbsp;</td>
+  </tr>
+  <tr> 
+    <td valign="top"> 
+      <table width="100%" border="0" cellspacing="0" cellpadding="5">
+        <tr> 
+          <td valign="top" class="small">
+	    <table border="0" width="75" cellspacing="0" cellpadding="0" align="left">
+	      <tr>
+	        <td width="1">
+		  <table border="0" cellspacing="0" cellpadding="1" bgcolor="#003366">
+	            <tr>
+	              <td><img src="/sitedesign/mygold/images/ostern.jpg" align="left" width="68" height="56" alt="Ostern bei MyGold.com" /></td>
+	            </tr>
+	          </table>
+		</td>
+	      </tr>
+	    </table>
+            <b>Am 31. M&auml;rz ist Ostern!<br />
+	    Schenken Sie doch etwas Besonderes, denn Schmuck ist unverg&auml;nglich!<br />
+	    Alle Bestellungen die bis Mittwoch, den 27.3. 12:00 Uhr bei uns eingehen, werden Ihnen
+	    rechtzeitig zu Ostern geliefert. Lesen Sie bitte unsere <a href="/article/articleview/49/">Liefergarantie</a>
+	    f&uuml;r weitere Informationen.<br />
+	    F&uuml;r die Unentschlossenen haben wir ein <a href="/trade/productspecials/1/">Osterangebot</a> zusammengestellt.	    
+            </b>
+	  </td>
+	</tr>
+      </table>
+    </td>
+  </tr>
+  <tr> 
+    <td class="spacer2">&nbsp;</td>
+  </tr>
+  <tr> 
+    <td class="bgspacer" ><img src="/sitedesign/mygold/images/shim.gif" width="1" height="2" alt="" /></td>
+  </tr>
+  <tr> 
+    <td class="spacer5">&nbsp;</td>
+  </tr>
+</table>
+																								      
