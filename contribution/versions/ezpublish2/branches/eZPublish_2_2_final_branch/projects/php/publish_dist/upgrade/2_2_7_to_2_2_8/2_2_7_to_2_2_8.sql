03922ad353e8423383f2c449d6b2d89b67ba46d9
Vidar Langseid <vl@ez.no>
2003-01-24 13:19:05 +0000

    removed duplicate index in previous fix

--- contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/publish_dist/upgrade/2_2_7_to_2_2_8/2_2_7_to_2_2_8.sql
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/publish_dist/upgrade/2_2_7_to_2_2_8/2_2_7_to_2_2_8.sql
@@ -4,8 +4,6 @@ create index eZStats_Archive_RequestedPageMonth on eZStats_Archive_RequestedPage
 create index eZStats_Archive_RequestedPageURI on eZStats_Archive_RequestedPage (URI);
 
 create index eZStats_Archive_RefererURLMonth on eZStats_Archive_RefererURL (Month);
-create index eZStats_Archive_RequestedPageURI on eZStats_Archive_RequestedPage (URI);
-create index eZStats_Archive_RequestedPageURI on eZStats_Archive_RequestedPage (URI);
 
 create index eZStats_Archive_RemoteHostIP on eZStats_Archive_RemoteHost (IP);
 create index eZStats_Archive_PageViewHour on eZStats_Archive_PageView (Hour);
