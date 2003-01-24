9536fb74b49374421a45389dd125e19ea328aa5e
Vidar Langseid <vl@ez.no>
2003-01-24 13:14:56 +0000

    Fixed performance problems (cron.php) by adding db indexes in eZStat

---
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/publish_dist/upgrade/2_2_7_to_2_2_8/2_2_7_to_2_2_8.sql
@@ -0,0 +1,12 @@
+
+
+create index eZStats_Archive_RequestedPageMonth on eZStats_Archive_RequestedPage (Month);
+create index eZStats_Archive_RequestedPageURI on eZStats_Archive_RequestedPage (URI);
+
+create index eZStats_Archive_RefererURLMonth on eZStats_Archive_RefererURL (Month);
+create index eZStats_Archive_RequestedPageURI on eZStats_Archive_RequestedPage (URI);
+create index eZStats_Archive_RequestedPageURI on eZStats_Archive_RequestedPage (URI);
+
+create index eZStats_Archive_RemoteHostIP on eZStats_Archive_RemoteHost (IP);
+create index eZStats_Archive_PageViewHour on eZStats_Archive_PageView (Hour);
+
