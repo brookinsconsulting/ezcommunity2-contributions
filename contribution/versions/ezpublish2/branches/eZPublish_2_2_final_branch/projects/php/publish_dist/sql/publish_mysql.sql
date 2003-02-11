fcd02c2f19088bebce341b71d34b3364b04e151d
Vidar Langseid <vl@ez.no>
2003-02-11 15:31:35 +0000

    Added db indexes to fix performanceproblems in eZImageCatalogue

--- contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/publish_dist/sql/publish_mysql.sql
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/publish_dist/sql/publish_mysql.sql
@@ -2738,3 +2738,15 @@ CREATE INDEX eZStats_Archive_RefererURLMonth ON eZStats_Archive_RefererURL (Mont
 CREATE INDEX eZStats_Archive_RemoteHostIP ON eZStats_Archive_RemoteHost (IP);
 CREATE INDEX eZStats_Archive_PageViewHour ON eZStats_Archive_PageView (Hour);
 
+
+
+create index eZImageCatalogue_Image_OriginalFileName on eZImageCatalogue_Image(OriginalFileName);
+create index eZImageCatalogue_ImagePermission_GroupID on eZImageCatalogue_ImagePermission(GroupID);
+create index eZImageCatalogue_ImagePermission_ObjectID on eZImageCatalogue_ImagePermission(ObjectID);
+create index eZImageCatalogue_ImagePermission_ReadPermission on eZImageCatalogue_ImagePermission(ReadPermission);
+create index eZImageCatalogue_CategoryPermission_GroupID on eZImageCatalogue_CategoryPermission(GroupID);
+create index eZImageCatalogue_CategoryPermission_ObjectID on eZImageCatalogue_CategoryPermission(ObjectID);
+create index eZImageCatalogue_CategoryPermission_ReadPermission on eZImageCatalogue_CategoryPermission(ReadPermission);
+
+
+
