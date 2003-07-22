81b4de183f3c317ab220c83597e5fa621fe147f9
Vidar Langseid <vl@ez.no>
2003-07-22 09:55:52 +0000

    - Fixed bug with invalid content type header sendt with RSS article list

--- contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/ezarticle/user/articlelistrss.php
+++ contribution/versions/ezpublish2/branches/eZPublish_2_2_final_branch/projects/php/ezarticle/user/articlelistrss.php
@@ -1,6 +1,6 @@
 <?php
 // 
-// $Id: articlelistrss.php,v 1.6.2.1 2002/02/27 08:40:19 bf Exp $
+// $Id: articlelistrss.php,v 1.6.2.2 2003/07/22 09:55:52 vl Exp $
 //
 // Created on: <11-Dec-2000 09:44:51 bf>
 //
@@ -40,7 +40,7 @@ $Language = $ini->read_var( "eZArticleRSS", "Language" );
 ob_end_clean();
 
 // xml header
-header( "Content-type text/xml" );
+header( "Content-type: text/xml" );
 print( "<?xml version=\"1.0\"?>\n\n" );
 
 // rss header
