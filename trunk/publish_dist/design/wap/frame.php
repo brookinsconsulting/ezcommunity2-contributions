274d01a76cb48a085dec75b923fd4db52fd33d93
kracker <info@kracker.org>
2004-09-22 00:41:53 +0000

    Updated: Removed excess blank lines

--- trunk/publish_dist/design/wap/frame.php
+++ trunk/publish_dist/design/wap/frame.php
@@ -1,10 +1,8 @@
 <?php
-
     header ("Content-Type: text/vnd.wap.wml");
     echo  "<?xml version=\"1.0\" ?>\n"; 
 
     $SiteTitle = $ini->read_var( "site", "SiteTitle" );
-   
 ?>
 
 <!DOCTYPE wml PUBLIC "-//WAPFORUM//DTD WML 1.1//EN" "http://www.wapforum.org/DTD/wml_1.1.xml">
