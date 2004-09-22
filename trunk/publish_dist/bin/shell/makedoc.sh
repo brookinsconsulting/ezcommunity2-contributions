a891e0e6aadb295495f09175ed0aa247e32dc2a6
kracker <info@kracker.org>
2004-09-22 00:34:44 +0000

    Updated: Changed echo statement to 'Generating eZ publish 2 documentation'

--- trunk/publish_dist/bin/shell/makedoc.sh
+++ trunk/publish_dist/bin/shell/makedoc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "Generatin eZ publish documentation"
+echo "Generating eZ publish 2 documentation"
 
 ezphpdoc.pl --disable_todo classes/ ezarticle/classes/ ezad/classes/ ezbug/classes/ ezcalendar/classes/ ezcontact/classes/ ezfilemanager/classes/ ezforum/classes/ ezimagecatalogue/classes/ ezlink/classes/ eznewsfeed/classes/ ezpoll/classes/ ezsession/classes/ ezstats/classes/ eztodo/classes/ eztrade/classes/ ezuser/classes/ ezaddress/classes/
