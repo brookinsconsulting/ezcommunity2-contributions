2403c9c7a623e56a92eb35c5b1902b00ee0f80c8
kracker <info@kracker.org>
2004-09-22 00:29:02 +0000

    Added entry for ezxml in script modules.sh

--- trunk/publish_dist/bin/shell/modules.sh
+++ trunk/publish_dist/bin/shell/modules.sh
@@ -2,4 +2,4 @@
 # Prints out the modules used in this site.
 # This output can be used with a cvs command to checkout the correct modules
 # Example: cvs co `./modules.sh`
-echo " ezsession ezuser ezarticle classes ezimagecatalogue ezpoll ezcontact ezforum ezlink admin eztodo ezad eznewsfeed ezbug ezstats eztrade ezcalendar ezaddress ezfilemanager ezerror ezmail ezbulkmail ezmodule ezsysinfo ezurltranslator ezform ezsitemanager ezquiz  ezmessage ezsearch ezmediacatalogue ezxmlrpc"
+echo " ezsession ezuser ezarticle classes ezimagecatalogue ezpoll ezcontact ezforum ezlink admin eztodo ezad eznewsfeed ezbug ezstats eztrade ezcalendar ezaddress ezfilemanager ezerror ezmail ezbulkmail ezmodule ezsysinfo ezurltranslator ezform ezsitemanager ezquiz  ezmessage ezsearch ezmediacatalogue ezxml ezxmlrpc"
