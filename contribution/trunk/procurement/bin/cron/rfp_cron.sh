ac934553e56737fe9b1107b86960e8d2ec7290db
kracker <info@kracker.org>
2004-10-17 20:09:44 +0000

    Added: Moved root components into bin/ dirs

---
+++ contribution/trunk/procurement/bin/cron/rfp_cron.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#/home/www/web/rfp_cron.sh
+#echo "rfp,cron,more"
+
+#//change directory
+cd /home/www/web/;
+
+#// run ez publish php rfp cron n script
+/usr/bin/php -a /home/www/web/rfp_cron.php
+
+
+
+
+
