60b4ab219eab16588b72e4848cf5b12018f17844
kracker <info@kracker.org>
2004-11-24 23:16:45 +0000

    Added: sitedesign/admin/ not primary (yet)

---
+++ contribution/trunk/procurement/sitedesign/admin/footer.php
@@ -0,0 +1,50 @@
+<?php
+// 
+// $Id: footer.php,v 1.17 2001/09/29 12:35:14 kaid Exp $
+//
+// Created on: <23-Jan-2001 16:06:07 bf>
+//
+// This source file is part of eZ publish, publishing software.
+//
+// Copyright (C) 1999-2001 eZ Systems.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
+
+include_once( "classes/INIFile.php" );
+$ini =& INIFile::globalINI();
+
+$Language = $ini->read_var( "eZArticleMain", "Language" );
+
+// include_once( "classes/template.inc" );
+// $t = new Template( $siteDir . "admin/templates/" . $SiteStyle );
+// $t->set_var( "www_dir", $GlobalSiteIni->WWWDir );
+// $t->set_var( "index", $GlobalSiteIni->Index );
+
+include_once( "classes/eztemplate.php" );
+$t = new eZTemplate( "admin/templates/" . $SiteStyle,
+                     "admin/intl/", $Language, "footer.php" );
+
+$t->set_file( array(
+    "footer_tpl" => "footer.tpl"
+    ) );
+
+$t->set_var( "site_style", $SiteStyle );
+$t->set_var( "module_dir", $moduleName );
+
+$t->pparse( "output", "footer_tpl" );
+
+?>
+
---
+++ contribution/trunk/procurement/sitedesign/admin/loginheader.php
@@ -0,0 +1,58 @@
+<?php
+//
+// $Id: loginheader.php,v 1.8.2.1 2002/03/04 13:15:21 ce Exp $
+//
+// Created on: <23-Jan-2001 16:06:07 bf>
+//
+// This source file is part of eZ publish, publishing software.
+//
+// Copyright (C) 1999-2001 eZ Systems.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+//
+
+include_once( "classes/eztemplate.php" );
+include_once( "classes/ezlocale.php" );
+include_once( "classes/ezpublish.php" );
+
+$ini =& INIFile::globalINI();
+$Language =& $ini->read_var( "eZUserMain", "Language" );
+$Locale = new eZLocale( $Language );
+$iso = $Locale->languageISO();
+
+
+$t = new eZTemplate( "admin/templates/" . $SiteStyle,
+                     "ezuser/admin/intl/", $Language, "menubox.php" );
+$t->set_file( array(
+    "header_tpl" => "loginheader.tpl"
+    ) );
+
+$SiteURL =& $ini->read_var( "site", "SiteURL" );
+
+$t->set_var( "site_url", $SiteURL );
+$t->set_var( "site_style", $SiteStyle );
+
+$moduleName = "user";
+$t->set_var( "module_name", $moduleName );
+
+$t->set_var( "charset", $iso );
+
+$t->setAllStrings();
+$t->set_var( "ezpublish_version", eZPublish::version() );
+
+$t->pparse( "output", "header_tpl" );
+
+
+?>
