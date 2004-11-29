154e8714c518b58afff5e633169733c94d5da124
kracker <info@kracker.org>
2004-11-29 23:45:16 +0000

    Updated: Inital Addition of report_cron.php (stats)

--- contribution/trunk/procurement/ezprocurement/cron/report_cron.php
+++ contribution/trunk/procurement/ezprocurement/cron/report_cron.php
@@ -1,1 +1,23 @@
+<?php
 
+################################################################################
+# Includes - INIFile.php to read FlagExpiredStats setting
+#            class:DateTime to get today's timestamp
+#            class:ezpageview for stats info
+# Section 1 - Summary
+#
+################################################################################
+include_once("classes/INIFile.php");
+$ini = &INIFile::globalINI();
+$Flagged = $ini->read_var("FlagExpiredStats", "eZProcurementMain");
+include_once("classes/ezdatetime.php");
+include_once("ezstats/classes/ezpageview.php");
+include_once("ezstats/classes/ezpageviewquery.php");
+
+$today = new eZDateTime();
+$todayStamp = $today->mysqlTimeStamp();
+die('today is '.$todayStamp);
+
+
+
+php?>
