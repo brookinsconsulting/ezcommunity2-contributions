efd5bb1f85146d47121a59caa001ba20973ba2a8
kracker <info@kracker.org>
2004-12-23 12:59:51 +0000

    Updated: Properly clear cache directories and Major speed increase on operation

--- contribution/trunk/extention/ezgroupeventcalendar/doc/examples/clearcache2.sh
+++ contribution/trunk/extention/ezgroupeventcalendar/doc/examples/clearcache2.sh
@@ -44,26 +44,38 @@ ezmediacatalogue
 ezsysinfo
 ezvote
 "
+root="$PWD";
 
 for dir in $clear_base_dirs
 do
     if [ -d $dir ]; then
-        echo "Clearing $dir"
-        #rm -f $dir/cache/*.cache
-        find . -name '$dir/cache/*.cache' -print0 | xargs -0 rm -vf
-	#rm -f $dir/cache/*.php
-        find . -name '$dir/cache/*.php' -print0 | xargs -0 rm -vf
+            echo "Clearing $dir"
+        if [ -d $dir/cache ]; then 
+            #rm -f $dir/cache/*.cache
+            cd "$dir/cache/";
+	    find . -name '*.cache' -print0 | xargs -0 rm -vf
+	    #rm -f $dir/cache/*.php
+            #find . -name '*.php' -print0 ; 
+            find . -name '*.php' -print0 | xargs -0 rm -vf
+	    cd "$root";
+	fi
 	if [ -d $dir/admin/cache/ ]; then
+	    cd "$dir/admin/cache/";
 	    #rm -f $dir/admin/cache/*.cache
-            find . -name '$dir/admin/cache/*.cache' -print0 | xargs -0 rm -vf
-	fi
+            find . -name '*.cache' -print0 | xargs -0 rm -vf
+	    cd "$root";
+	fi 
 	if [ -d $dir/user/cache/ ]; then
+	    cd "$dir/user/cache/";
 	    #rm -f $dir/user/cache/*.cache
-            find . -name '$dir/user/cache/*.cache' -print0 | xargs -0 rm -vf
+            find . -name '*.cache' -print0 | xargs -0 rm -vf
+	    cd "$root";
 	fi
+	    echo "";
     else
         echo "Creating $dir"
-	    # mkdir -p $dir
+	    #echo "problem with $root | $PWD | $dir \n"
+	    mkdir -p $dir
     fi
-    chmod 777 $dir   
+    #chmod 777 $dir   
 done
