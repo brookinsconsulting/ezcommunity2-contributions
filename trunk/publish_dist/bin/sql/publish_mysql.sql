c4d3701b919fa986af1b9a43ad260affd9c6b777
kracker <info@kracker.org>
2005-12-20 22:18:30 +0000

    Updated: Fixed eZ newsfeed db table creation sql syntax errors

--- trunk/publish_dist/bin/sql/publish_mysql.sql
+++ trunk/publish_dist/bin/sql/publish_mysql.sql
@@ -1658,7 +1658,7 @@ CREATE TABLE eZModule_LinkModuleType (
   PRIMARY KEY (ID,Module,Type)
 );
 CREATE TABLE eZNewsFeed_Category (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL auto_increment,
   Name varchar(150) DEFAULT '' NOT NULL,
   Description text,
   ParentID int(11) DEFAULT '0' NOT NULL,
@@ -1668,7 +1668,7 @@ CREATE TABLE eZNewsFeed_Category (
 INSERT INTO eZNewsFeed_Category VALUES (1,'News from freshmeat','',0);
 
 CREATE TABLE eZNewsFeed_News (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL auto_increment,
   IsPublished int NOT NULL DEFAULT '0',
   PublishingDate int NOT NULL,
   OriginalPublishingDate int NOT NULL,
@@ -1681,14 +1681,14 @@ CREATE TABLE eZNewsFeed_News (
 );
 
 CREATE TABLE eZNewsFeed_NewsCategoryLink (
-  ID int(11) DEFAULT '0' NOT NULL auto_increment,
+  ID int(11) NOT NULL auto_increment,
   NewsID int(11) DEFAULT '0' NOT NULL,
   CategoryID int(11) DEFAULT '0' NOT NULL,
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE eZNewsFeed_SourceSite (
-  ID int(11) DEFAULT '0' NOT NULL,
+  ID int(11) NOT NULL auto_increment,
   URL varchar(250),
   Login varchar(30),
   Password varchar(30),
